e3cc3c24c10f1b8da0baa7b705a849ca9ef2b3b4
pradeep <pradeep@48088309-b40f-0410-b149-b14953253675>
2006-05-15 20:12:29 +0000

    Sample code for maintaining sessions and for passing CAS token to the annotation web-service.
    git-svn-id: http://svn.topazproject.org/svn/head@80 48088309-b40f-0410-b149-b14953253675

--- topazproject/webservices/annotation-admin/ws-client-impl/src/test/java/org/topazproject/ws/admin/service/AnnotationServiceTest.java
+++ topazproject/webservices/annotation-admin/ws-client-impl/src/test/java/org/topazproject/ws/admin/service/AnnotationServiceTest.java
@@ -1,16 +1,14 @@
-
 package org.topazproject.ws.admin.service;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+
 import java.rmi.RemoteException;
+
 import javax.xml.rpc.ServiceException;
 
 import junit.framework.TestCase;
 
-/**
- *
- */
 public class AnnotationServiceTest extends TestCase {
   private Annotation service;
 
@@ -19,9 +17,11 @@ public class AnnotationServiceTest extends TestCase {
   }
 
   protected void setUp() throws MalformedURLException, ServiceException, RemoteException {
-    URL url =
-        new URL("http://localhost:9998/ws-annotation-admin-webapp-0.1/services/AnnotationServicePort");
+   // CAS Integration hint: append a ?token=xxx to the url
+    URL                      url =
+      new URL("http://localhost:9998/ws-annotation-admin-webapp-0.1/services/AnnotationServicePort?token=xxx");
     AnnotationServiceLocator locator = new AnnotationServiceLocator();
+    locator.setMaintainSession(true);
     service = locator.getAnnotationServicePort(url);
   }
 
@@ -30,59 +30,68 @@ public class AnnotationServiceTest extends TestCase {
   }
 
   private void basicAnnotationTest() throws RemoteException {
-    String subject = "foo";
-    String annotation = "a1";
+    String   subject     = "foo";
+    String   annotation  = "a1";
     String[] annotations = service.listAnnotations(subject);
-    assertTrue("Expected empty list of annotations, got " + annotations.length, annotations.length == 0);
+    assertTrue("Expected empty list of annotations, got " + annotations.length,
+               annotations.length == 0);
 
     boolean gotExc = false;
+
     try {
-      String info = service.getAnnotationInfo(subject,annotation);
+      String info = service.getAnnotationInfo(subject, annotation);
     } catch (NoSuchIdException nsie) {
       gotExc = true;
     }
+
     assertTrue("Failed to get expected NoSuchIdException", gotExc);
 
     gotExc = false;
+
     try {
-      service.setAnnotationInfo(subject,annotation,"hello");
+      service.setAnnotationInfo(subject, annotation, "hello");
     } catch (NoSuchIdException nsie) {
       gotExc = true;
     }
+
     assertTrue("Failed to get expected NoSuchIdException", gotExc);
 
     gotExc = false;
+
     try {
-      service.deleteAnnotation(subject,annotation);
+      service.deleteAnnotation(subject, annotation);
     } catch (NoSuchIdException nsie) {
       gotExc = true;
     }
+
     assertTrue("Failed to get expected NoSuchIdException", gotExc);
 
-    service.createAnnotation(subject,annotation);
+    service.createAnnotation(subject, annotation);
 
     annotations = service.listAnnotations(subject);
     assertTrue("Expected one annotation, got " + annotations.length, annotations.length == 1);
-    assertEquals("Expected annotation-id '" + annotation +"', got '" + annotations[0] + "'", annotations[0], annotation);
+    assertEquals("Expected annotation-id '" + annotation + "', got '" + annotations[0] + "'",
+                 annotations[0], annotation);
 
-    String info = service.getAnnotationInfo(subject,annotation);
+    String info = service.getAnnotationInfo(subject, annotation);
     assertNull("Expected no info, got '" + info + "'", info);
 
-    service.setAnnotationInfo(subject,annotation, "hello");
-    info = service.getAnnotationInfo(subject,annotation);
+    service.setAnnotationInfo(subject, annotation, "hello");
+    info = service.getAnnotationInfo(subject, annotation);
     assertEquals("Info mismatch, got '" + info + "'", info, "hello");
 
-    service.deleteAnnotation(subject,annotation);
+    service.deleteAnnotation(subject, annotation);
     gotExc = false;
+
     try {
-      service.deleteAnnotation(subject,annotation);
+      service.deleteAnnotation(subject, annotation);
     } catch (NoSuchIdException nsie) {
       gotExc = true;
     }
+
     assertTrue("Failed to get expected NoSuchIdException", gotExc);
 
     annotations = service.listAnnotations(subject);
     assertTrue("Expected no annotations, got " + annotations.length, annotations.length == 0);
-    
   }
 }
