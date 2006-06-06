c7cad8d8311e6fc7ed7946363420ff25266752bd
ronald <ronald@48088309-b40f-0410-b149-b14953253675>
2006-06-06 11:58:45 +0000

    First simple ingest and delete test.
    The test is commented out so it won't break continous-integration builds, as the test
    requires a running Fedora.
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@99 48088309-b40f-0410-b149-b14953253675

--- topazproject/webservices/articles/ws-client-impl/src/test/java/org/topazproject/ws/article/service/ArticleServiceTest.java
+++ topazproject/webservices/articles/ws-client-impl/src/test/java/org/topazproject/ws/article/service/ArticleServiceTest.java
@@ -1,8 +1,11 @@
 
 package org.topazproject.ws.article.service;
 
+import java.io.InputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.rmi.RemoteException;
 import javax.xml.rpc.ServiceException;
 
@@ -20,15 +23,30 @@ public class ArticleServiceTest extends TestCase {
 
   protected void setUp() throws MalformedURLException, ServiceException, RemoteException {
     URL url =
-        new URL("http://localhost:9999/ws-articles-webapp-0.1/services/ArticleServicePort");
+        new URL("http://localhost:9997/ws-articles-webapp-0.1/services/ArticleServicePort");
     ArticleServiceLocator locator = new ArticleServiceLocator();
     service = locator.getArticleServicePort(url);
   }
 
-  public void testAll() throws RemoteException {
-    basicArticleTest();
+  public void testAll() throws RemoteException, IOException {
+    //basicArticleTest();
   }
 
-  private void basicArticleTest() throws RemoteException {
+  private void basicArticleTest() throws RemoteException, IOException {
+    byte[] zip = loadURL(getClass().getResource("/test_article.zip"));
+    service.ingestNew(zip);
+    service.delete("10.1371/journal.pbio.0020294", -1, true);
+  }
+
+  private static byte[] loadURL(URL url) throws IOException {
+    URLConnection con = url.openConnection();
+    con.connect();
+    byte[] res = new byte[con.getContentLength()];
+
+    InputStream is = con.getInputStream();
+    is.read(res);
+    is.close();
+
+    return res;
   }
 }
