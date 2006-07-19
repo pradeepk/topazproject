e097d6c85f87b94b3770d9ea427480611ca624a9
viru <viru@48088309-b40f-0410-b149-b14953253675>
2006-07-19 16:54:10 +0000

    Changed the signature so as to work with the checked exceptions thrown from the Registration service
    git-svn-id: http://svn.topazproject.org/svn/head@217 48088309-b40f-0410-b149-b14953253675

--- plosone-registration/src/test/java/org/plos/service/TestRegistrationService.java
+++ plosone-registration/src/test/java/org/plos/service/TestRegistrationService.java
@@ -9,21 +9,21 @@ import org.plos.registration.User;
  */
 public class TestRegistrationService extends BasePlosoneRegistrationTest {
 
-  public void testNewUser() {
+  public void testNewUser() throws UserAlreadyExistsException {
     User user = getRegistrationService().createUser("dave@home.com", "david");
     assertNotNull(user.getId());
     assertFalse(user.isVerified());
     assertFalse(user.isActive());
   }
 
-  public void testVerifiedUser() {
+  public void testVerifiedUser() throws UserAlreadyExistsException {
     User user = getRegistrationService().createUser("viru@home.com", "virender");
     getRegistrationService().setVerified(user);
     assertTrue(user.isVerified());
     assertTrue(user.isActive());
   }
 
-  public void testDeactivatedUser() {
+  public void testDeactivatedUser() throws UserAlreadyExistsException {
     final String email = "susie@home.com";
     getRegistrationService().createUser(email, "susan");
     final User user = getRegistrationService().getUserWithLoginName(email);
