aa069ca9e868a1b02bcf175253c9410b5378f736
dcotton <dcotton@48088309-b40f-0410-b149-b14953253675>
2006-07-21 17:35:29 +0000

    update JavaDocs
    git-svn-id: http://svn.topazproject.org/svn/head@245 48088309-b40f-0410-b149-b14953253675

--- plos-registration/src/main/java/org/plos/registration/UserImpl.java
+++ plos-registration/src/main/java/org/plos/registration/UserImpl.java
@@ -3,14 +3,15 @@ package org.plos.registration;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Date;
 
+/**
+ * $HeadURL: $
+ * @version: $Id: $
+ *
+ * Implementation for Plos Reistration User
+ */
 
 @Entity
 @Table (name = "plos_user")
@@ -43,99 +44,159 @@ public class UserImpl implements User {
   public UserImpl() {
   }
 
+  /**
+    * Creates a new UserImpl object.
+    *
+    * @param loginName The user login name
+    * @param password The user password
+    */
   public UserImpl(final String loginName, final String password) {
     this.loginName = loginName;
     this.password = password;
   }
 
+  /**
+   * @see org.plos.registration.User#getLoginName()
+   */
   @Transactional(readOnly=true)
   public String getLoginName() {
     return loginName;
   }
 
+  /**
+   * @see User#setLoginName(String)
+   */
   @Transactional(propagation= Propagation.MANDATORY)
   public void setLoginName(final String loginName) {
     this.loginName = loginName;
   }
 
+  /**
+   * @see org.plos.registration.User#getPassword()
+   */
   @Transactional(readOnly=true)
   public String getPassword() {
     return password;
   }
 
+  /**
+   * @see User#setPassword(String)
+   */
   @Transactional(propagation= Propagation.MANDATORY)
   public void setPassword(final String password) {
     this.password = password;
   }
 
+  /**
+   * @see User#setVerified(boolean)
+   */
   @Transactional(propagation= Propagation.MANDATORY)
   public void setVerified(final boolean verified) {
     this.verified = verified;
   }
 
+  /**
+   * @see org.plos.registration.User#isVerified()
+   */
   @Transactional(readOnly=true)
   public boolean isVerified() {
     return verified;
   }
 
+  /**
+   * @see User#setActive(boolean)
+   */
   @Transactional(propagation= Propagation.MANDATORY)
   public void setActive(final boolean active) {
     this.active = active;
 
   }
 
+  /**
+   * @see org.plos.registration.User#isActive()
+   */
   @Transactional(readOnly=true)
   public boolean isActive() {
     return active;
   }
 
+  /**
+   * @see org.plos.registration.User#getId()
+   */
   @Transactional(readOnly=true)
   public String getId() {
     return id;
   }
 
+  /**
+   * @see User#setId(String)
+   */
   @Transactional(propagation= Propagation.MANDATORY)
   public void setId(final String id) {
     this.id = id;
   }
 
+  /**
+   * @see org.plos.registration.User#getEmailVerificationToken()
+   */
   @Transactional(readOnly=true)
   public String getEmailVerificationToken() {
     return emailVerificationToken;
   }
 
+  /**
+   * @see User#setEmailVerificationToken(String)
+   */
   @Transactional(propagation= Propagation.MANDATORY)
   public void setEmailVerificationToken(final String emailVerificationToken) {
     this.emailVerificationToken = emailVerificationToken;
   }
 
+  /**
+   * @see org.plos.registration.User#getCreatedOn()
+   */
   @Transactional(readOnly=true)
   public Date getCreatedOn() {
     return createdOn;
   }
 
+  /**
+   * @see User#setCreatedOn(java.sql.Date)
+   */
   // TODO: set the date
   @Transactional(propagation= Propagation.MANDATORY)
   public void setCreatedOn(Date createdOn) {
     this.createdOn = createdOn;
   }
 
+  /**
+   * @see org.plos.registration.User#getUpdatedOn()
+   */
   @Transactional(readOnly=true)
   public Date getUpdatedOn() {
     return updatedOn;
   }
 
+  /**
+   * @see User#setUpdatedOn(java.sql.Date)
+   */
   // TODO: set the date
   @Transactional(propagation= Propagation.MANDATORY)
   public void setUpdatedOn(Date updatedOn) {
     this.updatedOn = updatedOn;
   }
 
+  /**
+   * @see org.plos.registration.User#getResetPasswordToken()
+   */
   @Transactional(readOnly=true)
   public String getResetPasswordToken() {
     return resetPasswordToken;
   }
 
+  /**
+   * @see User#setResetPasswordToken(String)
+   */
   @Transactional(propagation= Propagation.MANDATORY)
   public void setResetPasswordToken(String resetPasswordToken) {
     this.resetPasswordToken = resetPasswordToken;
