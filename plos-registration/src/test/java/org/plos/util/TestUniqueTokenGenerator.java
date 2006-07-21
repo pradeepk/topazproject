2a6fc07ca1610ebe99d588c87d2d438b04341a72
viru <viru@48088309-b40f-0410-b149-b14953253675>
2006-07-21 20:35:30 +0000

    Reset the version control headers
    git-svn-id: http://svn.topazproject.org/svn/head@258 48088309-b40f-0410-b149-b14953253675

--- plos-registration/src/test/java/org/plos/util/TestUniqueTokenGenerator.java
+++ plos-registration/src/test/java/org/plos/util/TestUniqueTokenGenerator.java
@@ -3,8 +3,8 @@ package org.plos.util;
 import junit.framework.TestCase;
 
 /**
- * $HeadURL: $
- * @version: $Id: $
+ * $HeadURL$
+ * @version: $Id$
  */
 public class TestUniqueTokenGenerator extends TestCase {
   public void testShouldGenerateUniqueTokens() {
