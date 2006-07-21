27a958ba4a0dbcb9bdca81bf9912870be0ea6237
ebrown <ebrown@48088309-b40f-0410-b149-b14953253675>
2006-07-21 23:26:34 +0000

    re #63 Re-enable integration tests through the it-helper profile
    The it-helper profile is defined in head/pom.xml and topazproject/webservices/
    */ws-client-impl/pom.xml. If "mvn -Pit-helper integration-tests" is run in
    any of the ws-client-impl modules, integration tests will be run. However, if
    the profile is off (default), integration tests are not run and everything
    builds normally.
    
    Unfortunately, running the it-helper profile outside of the specific sub-
    modules will fail due to the annoying way maven decises when and how to 
    compute the pom to run. So don't try it! This is phase 1. Phase 2 of the
    integration test stuff will address that.
    
    Integration test code does assume that fedora and CAS are running.
    
    Integration test code has been moved from src/test to src/it. This is a
    pattern, not necessarily the best pattern given difficulties getting maven
    to use this directory as intended. Time will tell.
    
    The it-helper profile defines the following:
     * testResources = src/it/resources AND src/test/resources
       (both needed or unit-tests may break - it is a compromise)
     * src/it/java added to test sources via build-helper-maven-plugin
     * surefire unit-tests turned off
     * surefire integration-tests turned on
     * A default configuration for cargo/jetty is defined such that all
       a sub-module needs to do is define that it wants to use the plugin
       and all defaults will be pulled in. (This is so that running with
       this profile in non ws-client-impl modules may still work somewhat.)
    
    === How to test ===
    for dir in topazproject/webservices/*/ws-client-impl; \
      do (cd $dir; mvn -Pit-helper integration-test); done
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@261 48088309-b40f-0410-b149-b14953253675

---
+++ topazproject/webservices/annotations/ws-client-impl/src/it/java/org/topazproject/ws/annotation/service/AnnotationServiceTest.java
@@ -0,0 +1,221 @@
+/* $HeadURL::                                                                            $
+ * $Id$
+ *
+ * Copyright (c) 2006 by Topaz, Inc.
+ * http://topazproject.org
+ *
+ * Licensed under the Educational Community License version 1.0
+ * http://opensource.org/licenses/ecl1.php
+ */
+package org.topazproject.ws.annotation.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the Annotation web service.
+ *
+ * @author Pradeep Krishnan
+ */
+public class AnnotationServiceTest extends TestCase {
+  private Annotation service;
+
+  /**
+   * Creates a new AnnotationServiceTest object.
+   *
+   * @param testName name of this test
+   */
+  public AnnotationServiceTest(String testName) {
+    super(testName);
+  }
+
+  /**
+   * Sets up the test. Gets the client stub for making calls to the service.
+   *
+   * @throws ServiceException indicates an error in setting up the client stub
+   * @throws RemoteException indicates an error in setting up the client stub
+   * @throws Error DOCUMENT ME!
+   */
+  protected void setUp() throws ServiceException, RemoteException {
+    try {
+      service =
+        AnnotationClientFactory.create("http://localhost:9997/ws-annotation-webapp-0.1/services/AnnotationServicePort");
+    } catch (MalformedURLException e) {
+      throw new Error(e);
+    }
+  }
+
+  /**
+   * Runs all tests
+   *
+   * @throws RemoteException on an error from the service
+   */
+  public void testAll() throws RemoteException {
+    basicAnnotationTest();
+  }
+
+  private void basicAnnotationTest() throws RemoteException {
+    String   subject     = "foo:bar";
+    String   context     = "foo:bar##xpointer(id(\"Main\")/p[2])";
+    String   hackContext = "$user/$annotates/$s/$created/\\'\"\'";
+    String   annotation  = "annotation:id#42";
+    String   bodyUrl     = "http://gandalf.topazproject.org";
+    String   bodyContent = "This is a comment on foo:bar";
+    String[] annotations = service.listAnnotations(subject, null, true);
+
+    try {
+      for (int i = 0; i < annotations.length; i++)
+        service.deleteAnnotation(annotations[i], true);
+    } catch (NoSuchIdException nsie) {
+      fail("Unexpected NoSuchIdException");
+    }
+
+    annotations = service.listAnnotations(subject, null, true);
+    assertTrue("Expected empty list of annotations, got " + annotations.length,
+               annotations.length == 0);
+
+    boolean gotExc = false;
+
+    try {
+      String info = service.getAnnotation(annotation);
+    } catch (NoSuchIdException nsie) {
+      gotExc = true;
+    }
+
+    assertTrue("Failed to get expected NoSuchIdException", gotExc);
+
+    gotExc = false;
+
+    try {
+      service.deleteAnnotation(annotation, false);
+    } catch (NoSuchIdException nsie) {
+      gotExc = true;
+    }
+
+    assertTrue("Failed to get expected NoSuchIdException", gotExc);
+
+    gotExc = false;
+
+    try {
+      annotation = service.createAnnotation(null, subject, null, null, "bad:url/{context}");
+    } catch (Exception e) {
+      gotExc = true;
+    }
+
+    assertTrue("Failed to get expected IllegalArgumentException", gotExc);
+
+    annotation   = service.createAnnotation(null, subject, hackContext, null, bodyUrl);
+
+    annotations = service.listAnnotations(subject, null, true);
+    assertTrue("Expected one annotation, got " + annotations.length, annotations.length == 1);
+    assertEquals("Expected annotation-id '" + annotation + "', got '" + annotations[0] + "'",
+                 annotations[0], annotation);
+
+    String info = service.getAnnotation(annotation);
+
+    //System.out.println(info);
+    int    i1 = info.indexOf("<a:body>") + 8;
+    int    i2 = info.indexOf("</a:body>");
+    String s  = info.substring(i1, i2);
+    assertEquals("<a:body> mismatch, got '" + s + "'", s, bodyUrl);
+
+    i1   = info.indexOf("<a:context>") + 11;
+    i2   = info.indexOf("</a:context>");
+    s    = info.substring(i1, i2);
+    assertEquals("<a:context> mismatch, got '" + s + "'", s, hackContext);
+
+    String superseded = annotation;
+
+    try {
+      annotation =
+        service.createAnnotation(null, subject, context, annotation, "text/plain;charset=utf-8",
+                                 bodyContent.getBytes("utf-8"));
+    } catch (java.io.UnsupportedEncodingException e) {
+      throw new Error(e);
+    }
+
+    info   = service.getAnnotation(annotation);
+
+    //System.out.println(info);
+    i1   = info.indexOf("<a:body>") + 8;
+    i2   = info.indexOf("</a:body>");
+    s    = info.substring(i1, i2);
+
+    try {
+      //System.out.println(info);
+      s = (new BufferedReader(new InputStreamReader((new URL(s)).openStream()))).readLine();
+
+      //System.out.println(info);
+    } catch (IOException e) {
+      throw new RemoteException("failed to read annotation body", e);
+    }
+
+    assertEquals("<a:body> mismatch, got '" + s + "'", s, bodyContent);
+
+    i1   = info.indexOf("<a:context>") + 11;
+    i2   = info.indexOf("</a:context>");
+    s    = info.substring(i1, i2);
+    assertEquals("<a:context> mismatch, got '" + s + "'", s, context);
+
+    annotations = service.listAnnotations(subject, null, true);
+    assertTrue("Expected one annotation, got " + annotations.length, annotations.length == 1);
+    assertEquals("Expected annotation-id '" + annotation + "', got '" + annotations[0] + "'",
+                 annotations[0], annotation);
+
+    annotations = service.getPrecedingAnnotations(annotation, true);
+    assertTrue("Expected one annotation, got " + annotations.length, annotations.length == 1);
+    assertEquals("Expected annotation-id '" + superseded + "', got '" + annotations[0] + "'",
+                 annotations[0], superseded);
+
+    annotations = service.getPrecedingAnnotations(superseded, true);
+    assertTrue("Expected zero annotation, got " + annotations.length, annotations.length == 0);
+
+    annotations = service.getLatestAnnotations(annotation, true);
+    assertTrue("Expected one annotation, got " + annotations.length, annotations.length == 1);
+    assertEquals("Expected annotation-id '" + annotation + "', got '" + annotations[0] + "'",
+                 annotations[0], annotation);
+
+    annotations = service.getLatestAnnotations(superseded, true);
+    assertTrue("Expected one annotation, got " + annotations.length, annotations.length == 1);
+    assertEquals("Expected annotation-id '" + annotation + "', got '" + annotations[0] + "'",
+                 annotations[0], annotation);
+
+    service.setAnnotationState(annotation, 42);
+    annotations = service.listAnnotations(42);
+    assertTrue("Expected one annotation, got " + annotations.length, annotations.length == 1);
+    assertEquals("Expected annotation-id '" + annotation + "', got '" + annotations[0] + "'",
+                 annotations[0], annotation);
+
+    service.deleteAnnotation(annotation, false);
+
+    annotations = service.listAnnotations(subject, null, true);
+    assertTrue("Expected one annotation, got " + annotations.length, annotations.length == 1);
+    assertEquals("Expected annotation-id '" + superseded + "', got '" + annotations[0] + "'",
+                 annotations[0], superseded);
+
+    service.deleteAnnotation(superseded, true);
+
+    gotExc = false;
+
+    try {
+      service.deleteAnnotation(annotation, true);
+    } catch (NoSuchIdException nsie) {
+      gotExc = true;
+    }
+
+    assertTrue("Failed to get expected NoSuchIdException", gotExc);
+
+    annotations = service.listAnnotations(subject, null, true);
+    assertTrue("Expected zero annotations, got " + annotations.length, annotations.length == 0);
+  }
+}
