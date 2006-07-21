a3ba9aa0b6b8d601b216ab863b134861ec0d9ccb
dcotton <dcotton@48088309-b40f-0410-b149-b14953253675>
2006-07-21 17:58:49 +0000

    update JavaDocs
    git-svn-id: http://svn.topazproject.org/svn/head@249 48088309-b40f-0410-b149-b14953253675

--- plos-registration/src/main/java/org/plos/registration/UserImpl.java
+++ plos-registration/src/main/java/org/plos/registration/UserImpl.java
@@ -3,7 +3,12 @@ package org.plos.registration;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.sql.Date;
 
 /**
