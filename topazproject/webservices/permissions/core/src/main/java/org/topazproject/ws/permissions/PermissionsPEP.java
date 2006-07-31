20cbdcc93446eae06ebc2704a7b8bfa84cad9e4b
pradeep <pradeep@48088309-b40f-0410-b149-b14953253675>
2006-07-31 20:55:13 +0000

    XACML policy for permissions service:   * looks up grants/revokes list as part of policy eval   * includes some common stuff like     * "admin" users have all permissions     * dc:creator has all permissions
    Also to pass integration tests, anonymous users have all permissions.
    
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@349 48088309-b40f-0410-b149-b14953253675

--- topazproject/webservices/permissions/core/src/main/java/org/topazproject/ws/permissions/PermissionsPEP.java
+++ topazproject/webservices/permissions/core/src/main/java/org/topazproject/ws/permissions/PermissionsPEP.java
@@ -28,32 +28,32 @@ public abstract class PermissionsPEP extends AbstractSimplePEP {
   /**
    * The action that represents a permission grant operation in XACML policies.
    */
-  public static final String GRANT = "grant";
+  public static final String GRANT = "permissions:grant";
 
   /**
    * The action that represents a permission revoke operation in XACML policies.
    */
-  public static final String REVOKE = "revoke";
+  public static final String REVOKE = "permissions:revoke";
 
   /**
    * The action that represents canceling a permission grant operation in XACML policies.
    */
-  public static final String CANCEL_GRANTS = "cancelGrants";
+  public static final String CANCEL_GRANTS = "permissions:cancelGrants";
 
   /**
    * The action that represents cancelling a permission revoke operation in XACML policies.
    */
-  public static final String CANCEL_REVOKES = "cancelRevokes";
+  public static final String CANCEL_REVOKES = "permissions:cancelRevokes";
 
   /**
    * The action that represents a list permission grants operation in XACML policies.
    */
-  public static final String LIST_GRANTS = "listGrants";
+  public static final String LIST_GRANTS = "permissions:listGrants";
 
   /**
    * The action that represents a list permission revokes operation in XACML policies.
    */
-  public static final String LIST_REVOKES = "listRevokes";
+  public static final String LIST_REVOKES = "permissions:listRevokes";
 
   /**
    * The list of all supported actions
