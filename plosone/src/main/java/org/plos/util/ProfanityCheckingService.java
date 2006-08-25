4c567002ded55658ed5738a56b2d4c2599c989e1
viru <viru@48088309-b40f-0410-b149-b14953253675>
2006-08-25 23:18:32 +0000

    Enhanced profanity checking service. More work needed with the regular expression as it catches even when it should not (test case provided)
    git-svn-id: http://svn.topazproject.org/svn/head@521 48088309-b40f-0410-b149-b14953253675

--- plosone/src/main/java/org/plos/util/ProfanityCheckingService.java
+++ plosone/src/main/java/org/plos/util/ProfanityCheckingService.java
@@ -11,21 +11,42 @@ package org.plos.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collection;
 
 /**
  * Checks that content is not profane. It could be used to check that the user's posts don't contain profane words like F***, GEORGE, BUSH, etc.
  */
 public class ProfanityCheckingService {
-  /** Validate that the content is profane or not
+  private Collection<String> profaneWords;
+
+  /**
+   * Validate that the content is profane or not
    * @param content content to check for profanity
    * @return true if content is not profane
    */
   public List<String> validate(final String content) {
-    //TODO: look for a stronger regular expression or algorithm to help with this
     final List<String> messages = new ArrayList<String>();
-    if (content.contains("BUSH")) {
-      messages.add("Found obscene word: BUSH");
+
+    final String contentLowerCase = content.toLowerCase();
+    for (final String word : profaneWords) {
+      //TODO:  More work needed with the regular expression as it catches even when it should not.
+      final String wordLowerCase = word.toLowerCase();
+      if (contentLowerCase.matches(".*\\s*\\p{Punct}*" + wordLowerCase + ".*")) {
+        if (contentLowerCase.matches(".*[a-zA-Z]+" + wordLowerCase)) {
+          continue;
+        }
+        if (contentLowerCase.matches("^[[a-zA-Z]*\\s+\\p{Punct}*]*" + wordLowerCase + ".*")) {
+          messages.add("Found obscene word:" + word);
+          break;
+        }
+      }
     }
+    
+
     return messages;
+  }
+
+  public void setWords(final Collection<String> profaneWords) {
+    this.profaneWords = profaneWords;
   }
 }
---
+++ plosone/src/test/java/org/plos/util/ProfanityCheckingServiceTest.java
@@ -0,0 +1,58 @@
+/* $HeadURL::                                                                            $
+ * $Id$
+ *
+ * Copyright (c) 2006 by Topaz, Inc.
+ * http://topazproject.org
+ *
+ * Licensed under the Educational Community License version 1.0
+ * http://opensource.org/licenses/ecl1.php
+ */
+package org.plos.util;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ProfanityCheckingServiceTest extends TestCase {
+  public void testShouldCatchProfaneText() {
+    final ProfanityCheckingService service = new ProfanityCheckingService();
+    final Collection<String> profaneWordList = new ArrayList<String>();
+    profaneWordList.add("BUSH");
+    service.setWords(profaneWordList);
+    found("bush", service);
+    found(" bush", service);
+    found("  bush", service);
+    found("  \nbush", service);
+    found("bUSH", service);
+    found("am bush", service);
+    found(".bush", service);
+    found(" some bush", service);
+    found("+bush", service);
+    found("-bush", service);
+    found(" bush", service);
+    found(" some before Bush and some after", service);
+    found("[Bushe ", service);
+    found(" (Bush ", service);
+    found("[Bush]", service);
+  }
+
+  public void testShouldAllowTextWhichIsNotProfane() {
+    final ProfanityCheckingService service = new ProfanityCheckingService();
+    final Collection<String> profaneWordList = new ArrayList<String>();
+    profaneWordList.add("BUSH");
+    service.setWords(profaneWordList);
+    notFound("ambush", service);
+    notFound(" some ambush", service);
+    notFound(" amBush ", service);
+    notFound(" some before amBush and some after", service);
+  }
+
+  private void notFound(final String content, final ProfanityCheckingService service) {
+    assertEquals("["+content+"]", 0, service.validate(content).size());
+  }
+
+  private void found(final String content, final ProfanityCheckingService service) {
+    assertEquals("["+content+"]", 1, service.validate(content).size());
+  }
+}
