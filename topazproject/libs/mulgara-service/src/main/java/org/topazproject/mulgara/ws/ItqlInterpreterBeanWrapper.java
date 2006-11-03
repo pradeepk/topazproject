481370acf75e5ff38d54bdf80d567d1aa94e79dc
pradeep <pradeep@48088309-b40f-0410-b149-b14953253675>
2006-11-03 01:07:18 +0000

    Implement the HttpSession to ItqlInterpreterBean 1-to-1 correspondance assumption correctly as per jax-rpc spec.
    There is still a problem though. If a client is multi-threaded and
    uses the same HttpSession cookie, then this can't support it.
    
    Also added debug logs before every use of ItqlInterpreterBean and
    for every checked exception.
    
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@930 48088309-b40f-0410-b149-b14953253675

--- topazproject/libs/mulgara-service/src/main/java/org/topazproject/mulgara/ws/ItqlInterpreterBeanWrapper.java
+++ topazproject/libs/mulgara-service/src/main/java/org/topazproject/mulgara/ws/ItqlInterpreterBeanWrapper.java
@@ -7,18 +7,22 @@
  * Licensed under the Educational Community License version 1.0
  * http://opensource.org/licenses/ecl1.php
  */
-
 package org.topazproject.mulgara.ws;
+
+import javax.servlet.http.HttpSession;
 
 import javax.xml.rpc.ServiceException;
 import javax.xml.rpc.server.ServiceLifecycle;
+import javax.xml.rpc.server.ServletEndpointContext;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.kowari.itql.ItqlInterpreterBean;
 import org.kowari.itql.ItqlInterpreterException;
+
 import org.kowari.query.QueryException;
+
 import org.kowari.server.SessionFactory;
 
 /**
@@ -27,81 +31,179 @@ import org.kowari.server.SessionFactory;
  * @author Ronald Tschalär
  */
 public class ItqlInterpreterBeanWrapper implements ServiceLifecycle {
-  private static final Log  log = LogFactory.getLog(ItqlInterpreterBeanWrapper.class);
-  private static final long MIN_FREE_MEM = 10 * 1024 * 1024;    // TODO: make this configurable
+  private static final Log    log             = LogFactory.getLog(ItqlInterpreterBeanWrapper.class);
+  private static final long   MIN_FREE_MEM    = 10 * 1024 * 1024; // TODO: make this configurable
+  private static final String INTERPRETER_KEY =
+    ItqlInterpreterBeanWrapper.class.getName() + ".interpreter";
 
-  private ItqlInterpreterBean interpreter;
+  //
+  private ServletEndpointContext context;
 
-  public void init(Object context) throws ServiceException {
-    try {
-      /* Note: this check won't work properly on JVM's that return Long.MAX_VALUE for maxMemory().
-       * But on those it's hard to know ahead of time if you're getting low on memory...
-       */
-      Runtime rt = Runtime.getRuntime();
-      if (rt.maxMemory() - rt.totalMemory() + rt.freeMemory() < MIN_FREE_MEM)
-        throw new ServiceException("Memory too low (probably too many sessions)");
-
-      SessionFactory sf = WebAppListenerInitMulgara.getSessionFactory();
-      interpreter = new ItqlInterpreterBean(sf.newSession(), sf.getSecurityDomain());
-
-      if (log.isDebugEnabled())
-        log.debug("Created ItqlInterpreterBean instance " + interpreter);
-    } catch (QueryException qe) {
-      log.error("Error initializing ItqlInterpreterBeanWrapper", qe);
-      throw new ServiceException(qe);
-    }
-  }
-
-  public void destroy() {
-    close();
-  }
-
-  /** 
-   * @see ItqlInterpreterBean#close 
+  /*
+   * @see javax.xml.rpc.server.ServletEndpointContext;
    */
-  public synchronized void close() {
+  public void init(Object context) throws ServiceException {
+    this.context = (ServletEndpointContext) context;
+  }
+
+  private ItqlInterpreterBean getInterpreter() throws QueryException {
+    HttpSession         session     = context.getHttpSession();
+    ItqlInterpreterBean interpreter = (ItqlInterpreterBean) session.getAttribute(INTERPRETER_KEY);
+
+    if (interpreter != null)
+      return interpreter;
+
+    /* Note: this check won't work properly on JVM's that return Long.MAX_VALUE for maxMemory().
+     * But on those it's hard to know ahead of time if you're getting low on memory...
+     */
+    Runtime rt = Runtime.getRuntime();
+
+    if ((rt.maxMemory() - rt.totalMemory() + rt.freeMemory()) < MIN_FREE_MEM)
+      throw new QueryException("Memory too low (probably too many sessions)");
+
+    SessionFactory sf = WebAppListenerInitMulgara.getSessionFactory();
+    interpreter = new ItqlInterpreterBean(sf.newSession(), sf.getSecurityDomain());
+
+    session.setAttribute(INTERPRETER_KEY, interpreter);
+
+    if (log.isDebugEnabled())
+      log.debug("Created ItqlInterpreterBean instance " + interpreter);
+
+    return interpreter;
+  }
+
+  /*
+   * @see javax.xml.rpc.server.ServletEndpointContext;
+   */
+  public void destroy() {
+    context = null;
+  }
+
+  /**
+   * @see ItqlInterpreterBean#close
+   */
+  public void close() {
+    HttpSession         session     = context.getHttpSession();
+    ItqlInterpreterBean interpreter;
+
+    interpreter = (ItqlInterpreterBean) session.getAttribute(INTERPRETER_KEY);
+
     if (interpreter == null)
       return;
+
+    session.removeAttribute(INTERPRETER_KEY);
 
     if (log.isDebugEnabled())
       log.debug("Closing ItqlInterpreterBean instance " + interpreter);
 
     interpreter.close();
-    interpreter = null;
   }
 
-  /** 
-   * @see ItqlInterpreterBean#beginTransaction 
+  /**
+   * @see ItqlInterpreterBean#beginTransaction
    */
   public void beginTransaction(String name) throws QueryException {
-    interpreter.beginTransaction(name);
+    ItqlInterpreterBean interpreter = getInterpreter();
+
+    if (log.isDebugEnabled())
+      log.debug("beginTransaction(" + name + ") on " + interpreter);
+
+    try {
+      interpreter.beginTransaction(name);
+    } catch (QueryException e) {
+      if (log.isDebugEnabled())
+        log.debug("beginTransaction on " + interpreter + " failed.", e);
+
+      throw e;
+    }
   }
 
-  /** 
-   * @see ItqlInterpreterBean#commit 
+  /**
+   * @see ItqlInterpreterBean#commit
    */
   public void commit(String name) throws QueryException {
-    interpreter.commit(name);
+    HttpSession         session     = context.getHttpSession();
+    ItqlInterpreterBean interpreter = (ItqlInterpreterBean) session.getAttribute(INTERPRETER_KEY);
+
+    if (interpreter != null) {
+      if (log.isDebugEnabled())
+        log.debug("commit(" + name + ") on " + interpreter);
+
+      try {
+        interpreter.commit(name);
+      } catch (QueryException e) {
+        if (log.isDebugEnabled())
+          log.debug("commit on " + interpreter + " failed.", e);
+
+        throw e;
+      }
+    }
   }
 
-  /** 
-   * @see ItqlInterpreterBean#rollback 
+  /**
+   * @see ItqlInterpreterBean#rollback
    */
   public void rollback(String name) throws QueryException {
-    interpreter.rollback(name);
+    HttpSession         session     = context.getHttpSession();
+    ItqlInterpreterBean interpreter = (ItqlInterpreterBean) session.getAttribute(INTERPRETER_KEY);
+
+    if (interpreter != null) {
+      if (log.isDebugEnabled())
+        log.debug("rollback(" + name + ") on " + interpreter);
+
+      try {
+        interpreter.rollback(name);
+      } catch (QueryException e) {
+        if (log.isDebugEnabled())
+          log.debug("rollback on " + interpreter + " failed.", e);
+
+        throw e;
+      }
+    }
   }
 
-  /** 
-   * @see ItqlInterpreterBean#executeQueryToString 
+  /**
+   * @see ItqlInterpreterBean#executeQueryToString
    */
-  public String executeQueryToString(String queryString) throws Exception {
-    return interpreter.executeQueryToString(queryString);
+  public String executeQueryToString(String queryString)
+                              throws Exception {
+    ItqlInterpreterBean interpreter = getInterpreter();
+
+    if (log.isDebugEnabled())
+      log.debug("executeQueryToString(" + queryString + ") on " + interpreter);
+
+    try {
+      return interpreter.executeQueryToString(queryString);
+    } catch (Exception e) {
+      if (log.isDebugEnabled())
+        log.debug("executeQueryToString on " + interpreter + " failed.", e);
+
+      throw e;
+    }
   }
 
-  /** 
-   * @see ItqlInterpreterBean#executeUpdate 
+  /**
+   * @see ItqlInterpreterBean#executeUpdate
    */
   public void executeUpdate(String itql) throws ItqlInterpreterException {
-    interpreter.executeUpdate(itql);
+    ItqlInterpreterBean interpreter;
+
+    try {
+      interpreter = getInterpreter();
+    } catch (QueryException e) {
+      throw new ItqlInterpreterException(e);
+    }
+
+    if (log.isDebugEnabled())
+      log.debug("executeUpdate(" + itql + ") on " + interpreter);
+
+    try {
+      interpreter.executeUpdate(itql);
+    } catch (ItqlInterpreterException e) {
+      if (log.isDebugEnabled())
+        log.debug("executeUpdate on " + interpreter + " failed.", e);
+
+      throw e;
+    }
   }
 }
