ca1e44cb8c573247136638456ceda77d88917fbf
viru <viru@48088309-b40f-0410-b149-b14953253675>
2006-11-17 17:23:48 +0000

    More work on unit tests for saving/fetching the user alerts
    git-svn-id: http://svn.topazproject.org/svn/head@1145 48088309-b40f-0410-b149-b14953253675

--- plosone/webapp/src/test/java/org/plos/user/action/UserAlertsTest.java
+++ plosone/webapp/src/test/java/org/plos/user/action/UserAlertsTest.java
@@ -11,11 +11,20 @@ package org.plos.user.action;
 
 import static com.opensymphony.xwork.Action.SUCCESS;
 import org.plos.BasePlosoneTestCase;
+import org.plos.Constants;
+import static org.plos.Constants.PLOS_ONE_USER_KEY;
+import org.plos.user.PlosOneUser;
 import org.apache.commons.lang.ArrayUtils;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class UserAlertsTest extends BasePlosoneTestCase {
+  final String AUTH_ID = UserAlertsTest.class.getName();
+
   public void testCreateAlerts() throws Exception {
-    final UserAlertsAction alertsAction = getUserAlertsAction();
+    final String topazId = createUser(AUTH_ID);
+    final UserAlertsAction alertsAction = getMockUserAlertsAction(AUTH_ID, topazId);
     final String[] weeklyAlertCategories = new String[]{
                 "biology",
                 "clinical_trials",
@@ -44,5 +53,60 @@ public class UserAlertsTest extends BasePlosoneTestCase {
     for (final String weeklyAlert : alertsAction.getWeeklyAlerts()) {
       ArrayUtils.contains(monthlyAlertCategories, weeklyAlert);
     }
+
+    getUserWebService().deleteUser(topazId);
   }
+
+  protected UserAlertsAction getMockUserAlertsAction(final String authId, final String topazId) {
+    final UserAlertsAction userAlertsAction = super.getUserAlertsAction();
+    final UserAlertsAction newUserAlertsAction = new UserAlertsAction() {
+      protected Map<String, Object> getSessionMap() {
+        return createMockSessionMap(authId, topazId);
+      }
+    };
+
+    newUserAlertsAction.setUserService(userAlertsAction.getUserService());
+
+    return newUserAlertsAction;
+  }
+
+  private String createUser(final String authId) throws Exception {
+    final CreateUserAction createUserAction = getMockCreateUserAction(authId);
+    createUserAction.setEmail("UserAlertsTest@test.com");
+    createUserAction.setRealName("UserAlertsTest test com");
+    createUserAction.setAuthId(authId);
+    createUserAction.setUsername("UserAlertsTest");
+    assertEquals(SUCCESS, createUserAction.execute());
+    final String topazId = createUserAction.getInternalId();
+    assertNotNull(topazId);
+
+    return topazId;
+  }
+
+  protected CreateUserAction getMockCreateUserAction(final String authId) {
+    final CreateUserAction createUserAction = super.getCreateUserAction();
+    final CreateUserAction newCreateUserAction = new CreateUserAction() {
+      protected Map<String, Object> getSessionMap() {
+        return createMockSessionMap(authId, null);
+      }
+    };
+
+    newCreateUserAction.setUserService(createUserAction.getUserService());
+
+    return newCreateUserAction;
+  }
+
+  private Map<String, Object> createMockSessionMap(final String authId, final String topazId) {
+    final PlosOneUser plosOneUser = new PlosOneUser(authId);
+    if (null != topazId) {
+      plosOneUser.setUserId(topazId);
+    }
+
+    final Map<String, Object> sessionMap = new HashMap<String, Object>();
+    sessionMap.put(PLOS_ONE_USER_KEY, plosOneUser);
+    sessionMap.put(Constants.SINGLE_SIGNON_USER_KEY, authId);
+
+    return sessionMap;
+  }
+
 }
