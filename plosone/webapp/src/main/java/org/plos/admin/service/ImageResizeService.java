41276d647d1292b08be607dfb2986f895451323a
alan <alan@48088309-b40f-0410-b149-b14953253675>
2006-11-26 03:30:01 +0000

    * always resize even on large 8 note these algs take a lot (400 meg) of heap
    git-svn-id: http://svn.topazproject.org/svn/head@1327 48088309-b40f-0410-b149-b14953253675

--- plosone/webapp/src/main/java/org/plos/admin/service/ImageResizeService.java
+++ plosone/webapp/src/main/java/org/plos/admin/service/ImageResizeService.java
@@ -1,57 +1,45 @@
 package org.plos.admin.service;
 
-import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Iterator;
 
-import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.ImageWriter;
-import javax.imageio.stream.*;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 
 public class ImageResizeService {
 	
 	private static final Log log = LogFactory.getLog(ImageResizeService.class);	
 	private BufferedImage image;
-	ImageWriter writer;
 	
-	public ImageResizeService() throws IOException {
-		Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("png");
-		if (writers.hasNext()) {
-			
-		} else {
-			throw new IOException("No image writers for PNG files");
-		}
-		writer = writers.next();
+	public ImageResizeService() {
+		ImageIO.setUseCache(true);
+		ImageIO.setCacheDirectory(null);
 	}
 	
-	private BufferedImage createResizedCopy(Image originalImage, 
-            int scaledWidth, int scaledHeight, 
-            boolean preserveAlpha)
+	private BufferedImage createResizedCopy(BufferedImage originalImage, 
+            int scaledWidth, int scaledHeight)
 	{
-		int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
-		BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, imageType);
-		Graphics2D g = scaledBI.createGraphics();
-		
-		if (preserveAlpha) {
-			g.setComposite(AlphaComposite.Src);
+		boolean noscale = false;
+		if (0 == scaledWidth && 0 == scaledHeight) {
+			scaledWidth = originalImage.getWidth(null);
+			scaledHeight = originalImage.getHeight(null);
+			noscale = true;
 		}
-		g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null); 
+		BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = scaledBI.createGraphics();
+		if (noscale) {
+			g.drawImage(originalImage, 0, 0, null); 
+		} else
+			g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null); 
 		g.dispose();
 		return scaledBI;
 	}
@@ -61,37 +49,16 @@ public class ImageResizeService {
 		HttpClient httpclient = new HttpClient();
 		GetMethod getmethod = new GetMethod(url);
 		if (200 == httpclient.executeMethod(getmethod)) {
-			InputStream is = getmethod.getResponseBodyAsStream();
-			String mimetype = getmethod.getResponseHeader("content-type").getValue().trim();
-			bi = readImage(is, mimetype);
+			bi = ImageIO.read(getmethod.getResponseBodyAsStream());
 		} else {
 			log.error("Cannot read image from: " + url);
 		}
 		return bi;
 	}
 
-	private BufferedImage readImage(InputStream is, String mimetype) throws IOException {
-		Iterator<ImageReader> readers = ImageIO.getImageReadersByMIMEType(mimetype);
-		ImageReader reader;
-		ImageInputStream iis = new FileCacheImageInputStream(is, null);
-		BufferedImage bi = null;
-		
-		if (! readers.hasNext()) {
-      log.error("No image readers for type: " + mimetype);
-			return null;
-		}
-    log.debug("found image reader for type: " + mimetype);
-		reader = readers.next();
-		reader.setInput(iis);
-		bi = reader.read(0);
-		is.close();
-		iis.close();
-    log.debug("BufferdImage.height = " + bi.getHeight() + " image.width = " + bi.getWidth());
-		return bi;
-	}
-	
 	public void captureImage(String url) throws HttpException, IOException {
 		image = readImage(url);		
+		// log.info("Captured image width = " + image.getWidth() + " height= " + image.getHeight());
 	}
 	
 	private byte[] scaleImage(float fixWidth, float fixHeight) throws IOException {
@@ -104,26 +71,20 @@ public class ImageResizeService {
 		} else if (fixHeight > 0) {
 			scale = fixHeight / height;
 		} else 
-			return null;
+			scale = 0;
 		width = (int)(width * scale);
 		height = (int)(height * scale);
-		return imageToByteArray(createResizedCopy(image, width, height, true));
+		return imageToByteArray(createResizedCopy(image, width, height));
 	}
 	
 	private byte[] imageToByteArray(BufferedImage image) throws IOException {
-    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-    ImageIO.write(image, "png", bos);
-    return bos.toByteArray();
-    /* log.debug("imageToByteArray: width= " + image.getWidth() + " height: " + image.getHeight());
-    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		writer.setOutput(new FileCacheImageOutputStream(bos, null));
-    //writer.setOutput(new MemoryCacheImageOutputStream(bos));
-		writer.write(image);
-		bos.flush();
-    log.debug("imagetobytearray size: " + bos.toString());
-    log.debug("imagetobytearray size: " + bos.size());
-    bos.close();
-		return bos.toByteArray();*/
+		ByteArrayOutputStream bos = new ByteArrayOutputStream(1000);
+		byte[] array;
+	    ImageIO.write(image, "png", bos);
+	    bos.flush();
+		array = bos.toByteArray();
+		bos.close();
+		return array;
 	}
 	
 	public byte[] getSmallImage() throws FileNotFoundException, IOException {
@@ -135,7 +96,14 @@ public class ImageResizeService {
 	}
 	
 	public byte[] getLargeImage() throws FileNotFoundException, IOException {
-		return imageToByteArray(image);
+		return scaleImage(0.0f, 0.0f);
 	}
-		
+	
+	public BufferedImage getImage() {
+		return image;
+	}
+	
+	public void setImage(BufferedImage bi) {
+		image = bi;
+	}
 }
