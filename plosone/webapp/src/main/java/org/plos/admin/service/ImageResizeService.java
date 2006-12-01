28b658094694155da9f15d889688d8ffe82af4d8
stevec <stevec@48088309-b40f-0410-b149-b14953253675>
2006-12-01 13:49:59 +0000

    Change the way image resizing is performed to make it both faster and better quality. Use a SubsampleAverage algorithm for all things except CMYK images which seem to expose a bug in the that class.  For these images, we first pass them through a low pass filter then through a scaler.  Speeds tend to be on the order of a few seconds depending on the image complexity and size.
    git-svn-id: http://svn.topazproject.org/svn/head@1507 48088309-b40f-0410-b149-b14953253675

--- plosone/webapp/src/main/java/org/plos/admin/service/ImageResizeService.java
+++ plosone/webapp/src/main/java/org/plos/admin/service/ImageResizeService.java
@@ -1,12 +1,30 @@
 package org.plos.admin.service;
 
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Transparency;
+import java.awt.color.ColorSpace;
+import java.awt.color.ICC_ColorSpace;
+import java.awt.color.ICC_Profile;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.renderable.ParameterBlock;
+
+import javax.media.jai.ImageLayout;
+import javax.media.jai.ParameterBlockJAI;
+import javax.media.jai.RasterFactory;
+import javax.media.jai.RenderedOp;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.media.jai.JAI;
+import javax.media.jai.operator.SubsampleAverageDescriptor;
+import javax.media.jai.InterpolationBicubic2;
+
+import java.net.URL;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
@@ -15,99 +33,190 @@ import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.sun.media.jai.codec.PNGEncodeParam;
+
 public class ImageResizeService {
 	
 	private static final Log log = LogFactory.getLog(ImageResizeService.class);	
-	private BufferedImage image;
-	
+	//private BufferedImage image;
+	private RenderedOp srcImage;
+	private PNGEncodeParam encodeParam;
+  private boolean isCMYK = false;
+  private RenderingHints hints;
+  
 	public ImageResizeService() {
-		ImageIO.setUseCache(true);
-		ImageIO.setCacheDirectory(null);
-	}
+		//ImageIO.setUseCache(true);
+		//ImageIO.setCacheDirectory(null);
 	
-	private BufferedImage createResizedCopy(BufferedImage originalImage, 
-            int scaledWidth, int scaledHeight)
-	{
-		boolean noscale = false;
-		if (0 == scaledWidth && 0 == scaledHeight) {
-			scaledWidth = originalImage.getWidth(null);
-			scaledHeight = originalImage.getHeight(null);
-			noscale = true;
-		}
-		BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g = scaledBI.createGraphics();
-		if (noscale) {
-			g.drawImage(originalImage, 0, 0, null); 
-		} else
-			g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null); 
-		g.dispose();
-		return scaledBI;
-	}
-	
-	private BufferedImage readImage(String url) throws HttpException, IOException {
-		BufferedImage bi = null;		
-		HttpClient httpclient = new HttpClient();
-		GetMethod getmethod = new GetMethod(url);
-		if (200 == httpclient.executeMethod(getmethod)) {
-			bi = ImageIO.read(getmethod.getResponseBodyAsStream());
-		} else {
-			log.error("Cannot read image from: " + url);
-		}
-		return bi;
-	}
+	  hints = new RenderingHints (RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+    hints.put(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+    hints.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    hints.put(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+    hints.put(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+    hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+    hints.put(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+//    hints.put(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+  }
 
 	public void captureImage(String url) throws HttpException, IOException {
-		image = readImage(url);		
-		// log.info("Captured image width = " + image.getWidth() + " height= " + image.getHeight());
+		srcImage = JAI.create("URL", new ParameterBlock().add(new URL (url)));
+		if (log.isDebugEnabled()) {
+		  log.debug("retrieved image from URL: " + url.toString());
+    }
+    if (srcImage.getColorModel().getColorSpace().getType() == ColorSpace.TYPE_CMYK ) {
+      isCMYK = true;
+      srcImage = convertCMYKtoRGB(srcImage);
+      if (log.isDebugEnabled()) {
+        log.debug("Image is of type CMYK");
+      }
+    }
 	}
 	
-	private byte[] scaleImage(float fixWidth, float fixHeight) throws IOException {
-		int width = image.getWidth();
-		int height = image.getHeight();		
-		float scale;
-		
-		if (fixWidth > 0) {
-			scale = fixWidth / width;
-		} else if (fixHeight > 0) {
-			scale = fixHeight / height;
-		} else 
-			scale = 0;
-		width = (int)(width * scale);
-		height = (int)(height * scale);
-		return imageToByteArray(createResizedCopy(image, width, height));
-	}
-	
-	private byte[] imageToByteArray(BufferedImage image) throws IOException {
-		ByteArrayOutputStream bos = new ByteArrayOutputStream(1000);
-		byte[] array;
-	    ImageIO.write(image, "png", bos);
-	    bos.flush();
-		array = bos.toByteArray();
-		bos.close();
-		return array;
-	}
-	
+  
+  private RenderedOp createScaledImage(float widthPercent, float heightPercent) {
+    RenderedOp transformedImage;
+    
+//  Scale the image to half its size in each direction
+    ParameterBlock pb;
+
+    if ((isCMYK) && ((srcImage.getWidth() * widthPercent > 512) || 
+        (srcImage.getHeight() * heightPercent > 512))) {
+      pb = new ParameterBlock();
+      pb.addSource(srcImage);
+      pb.add(5);
+      pb.add(5);
+      if (log.isDebugEnabled()) {
+        log.debug("Applying BoxFilter");
+      }
+      transformedImage = JAI.create("BoxFilter", pb);
+
+      pb = new ParameterBlock();
+      pb.addSource(transformedImage);
+      pb.add(widthPercent);
+      pb.add(heightPercent);
+      pb.add(0.0f);
+      pb.add(0.0f);
+      pb.add(new InterpolationBicubic2(256));
+      pb.add(hints);
+      if (log.isDebugEnabled()) {
+        log.debug("Applying Scaling");
+      }
+      transformedImage = JAI.create("Scale", pb);
+    } else {
+      pb = new ParameterBlock();
+      pb.addSource(srcImage);
+      pb.add((double)widthPercent);
+      pb.add((double)heightPercent);
+      pb.add(hints);
+      if (log.isDebugEnabled()) {
+        log.debug("SubsampleAverage");
+      }
+      transformedImage = JAI.create("SubsampleAverage", pb);
+    }
+    return transformedImage;
+  }
+  
+  private byte[] renderedOpToPNGByteArray (RenderedOp inImage) {
+    byte[] array = new byte[inImage.getHeight() * inImage.getWidth()];
+    ByteArrayOutputStream stream = new ByteArrayOutputStream(array.length);
+    JAI.create("encode", inImage, stream, "PNG");
+    return stream.toByteArray();
+    
+  }
+  private byte[] scaleFixWidth (float fixWidth) {
+    int width = srcImage.getWidth ();
+    float scale;
+    if (width > fixWidth){
+      scale = fixWidth/width;
+    } else {
+      scale = 1;
+    }
+    RenderedOp newImage; 
+    if (scale == 1) {
+      newImage = srcImage;
+    } else {
+      newImage = createScaledImage(scale, scale);
+    }
+    return renderedOpToPNGByteArray (newImage);
+  }
+
+  
+  
+  private byte[] scaleLargestDim (float oneSide){
+    int height = srcImage.getHeight();
+    int width = srcImage.getWidth ();
+    float scale;
+    if ((height > width) && (height > oneSide)){
+      scale = oneSide/height;
+    } else if (width > oneSide){
+      scale = oneSide/width;
+    } else {
+      scale = 1;
+    }
+    RenderedOp newImage; 
+    if (scale == 1) {
+      newImage = srcImage;
+    } else {
+      newImage = createScaledImage(scale, scale);
+    }
+    return renderedOpToPNGByteArray (newImage);
+  }
+ 
 	public byte[] getSmallImage() throws FileNotFoundException, IOException {
-		return scaleImage(70.0f, 0.0f);
+		return scaleFixWidth(70.0f);
 	}
 	
 	public byte[] getMediumImage() throws FileNotFoundException, IOException {
-		return scaleImage(600.0f, 0.0f);
+		return scaleLargestDim(600.0f);
 	}
 	
 	public byte[] getLargeImage() throws FileNotFoundException, IOException {
-		return scaleImage(0.0f, 0.0f);
+		return renderedOpToPNGByteArray(srcImage);
 	}
 	
   public byte[] getPageWidthImage() throws FileNotFoundException, IOException {
-    return scaleImage(400.0f, 0.0f);
+    return scaleFixWidth(400.0f);
   }  
-  
-	public BufferedImage getImage() {
-		return image;
-	}
-	
-	public void setImage(BufferedImage bi) {
-		image = bi;
-	}
+
+  private RenderedOp convertCMYKtoRGB(RenderedOp op) {
+    try {
+        ParameterBlockJAI pb;
+        ICC_Profile cmyk_profile = ICC_Profile.getInstance("CMYK.pf"); //CMYK.pf
+
+        ICC_ColorSpace cmyk_icp = new ICC_ColorSpace(cmyk_profile);
+        ColorModel cmyk_cm =
+            RasterFactory.createComponentColorModel(op.getSampleModel().getDataType(),
+                                                    cmyk_icp,
+                                                    false,
+                                                    false,
+                                                    Transparency.OPAQUE);
+
+        ImageLayout cmyk_il = new ImageLayout();
+        cmyk_il.setColorModel(cmyk_cm);
+        RenderingHints cmyk_hints = new RenderingHints(JAI.KEY_IMAGE_LAYOUT, cmyk_il);
+        pb = new ParameterBlockJAI("format");
+        pb.addSource(op);
+        pb.setParameter("datatype", op.getSampleModel().getDataType());
+        op = JAI.create("format", pb, cmyk_hints);
+        ColorSpace rgb_icp = ColorSpace.getInstance(ColorSpace.CS_sRGB);
+        ColorModel rgb_cm =
+            RasterFactory.createComponentColorModel(op.getSampleModel().getDataType(),
+                                                    rgb_icp,
+                                                    false,
+                                                    false,
+                                                    Transparency.OPAQUE);
+        ImageLayout rgb_il = new ImageLayout();
+        rgb_il.setSampleModel(rgb_cm.createCompatibleSampleModel(op.getWidth(),
+                                                                 op.getHeight()));
+        RenderingHints rgb_hints = new RenderingHints(JAI.KEY_IMAGE_LAYOUT, rgb_il);
+        pb = new ParameterBlockJAI("colorconvert");
+        pb.addSource(op);
+        pb.setParameter("colormodel", rgb_cm);
+        op = JAI.create("colorconvert", pb, rgb_hints);
+    } catch(Exception ex) {
+        ex.printStackTrace();
+    }
+    return op;
+
+  }
 }
