f9c2fc8455a40632ab98376ff0079ff183e6a102
joycelyn <joycelyn@48088309-b40f-0410-b149-b14953253675>
2006-12-14 01:05:23 +0000

    Per Margaret, apply the javascript resizing only to IE.
    git-svn-id: http://svn.topazproject.org/svn/head@1955 48088309-b40f-0410-b149-b14953253675

--- plosone/webapp/src/main/webapp/javascript/init_global.js
+++ plosone/webapp/src/main/webapp/javascript/init_global.js
@@ -1,34 +1,28 @@
+var container;
+var topBanner;
+
 function globalInit() {
-  var container = dojo.byId("container");
-  var footer = dojo.byId("ftr");
-  var topBanner = dojo.byId("topBanner");
-  var viewport = dojo.html.getViewport();
-  
-  if (container) {
-    topaz.domUtil.setContainerWidth(container, 675, 940);
+  if (BrowserDetect.browser == "Explorer") {
+    container = dojo.byId("container");
+    topBanner = dojo.byId("topBanner");
     
-    dojo.event.connect(window, "onresize", function() {
-        topaz.domUtil.setContainerWidth(container, 675, 940);
-      }
-    );
-  }
-  
-  //if (footer) {
-  //  topaz.domUtil.setContainerWidth(footer, 675, 940);
+    if (container) {
+      topaz.domUtil.setContainerWidth(container, 675, 940);
+      
+      dojo.event.connect(window, "onresize", function() {
+          setTimeout("topaz.domUtil.setContainerWidth(container, 675, 940)", 100);
+        }
+      );
+    }
     
-  //  dojo.event.connect(window, "onresize", function() {
-  //      topaz.domUtil.setContainerWidth(footer, 675, 940);
-  //    }
-  //  );
-  //}
-  
-  if (topBanner) {
-    topaz.domUtil.setContainerWidth(topBanner, 942, 944);
-    
-    dojo.event.connect(window, "onresize", function() {
-        topaz.domUtil.setContainerWidth(topBanner, 942, 944);
-      }
-    );
+    if (topBanner) {
+      topaz.domUtil.setContainerWidth(topBanner, 942, 944);
+      
+      dojo.event.connect(window, "onresize", function() {
+          setTimeout("topaz.domUtil.setContainerWidth(topBanner, 942, 944)", 100);
+        }
+      );
+    }
   }
 }
 
