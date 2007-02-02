debd4edbb13c082f056cd03f4befb78fe1f150a8
ebrown <ebrown@48088309-b40f-0410-b149-b14953253675>
2007-02-02 01:36:50 +0000

    re #170 List author email addresses when listing annotated articles
    There may be more than one author email address. Thus, the author(s) field
    in the CSV output will always be quoted and may contain a comma separated
    list of 0 or more email addresses. These must presumably be parsed out.
    
    Also changed command line args slightly. See --help. Briefly, specify
    -FfedoraHost:port and -MmulgaraHost:port.
    
    FYI: How to test (mostly to remind myself)
     * Setup tunnel to a real system (i.e. merlin):
       ssh -L9000:localhost:9091 -L9009:localhost:9090
     * Run from runscripts tool:
    {{{
    cd head/topazproject/tools/runscripts
    mvn -o -Prungroovy exec:java -Dargs="/Users/ebrown/topaz/head/packages/tools/src/maain/resources/scripts/annotated.groovy -Mlocalhost:9000 -Flocalhost:9009"
    }}}
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@2313 48088309-b40f-0410-b149-b14953253675

--- packages/tools/src/main/resources/scripts/annotated.groovy
+++ packages/tools/src/main/resources/scripts/annotated.groovy
@@ -9,13 +9,14 @@
  */
 import org.topazproject.mulgara.itql.ItqlHelper;
 import org.topazproject.mulgara.itql.Answer;
+import org.topazproject.xml.transform.cache.CachedSource;
 import org.apache.commons.lang.text.StrMatcher;
 import org.apache.commons.lang.text.StrTokenizer;
 
 // Constants
-DEFAULT_HOST = "localhost"
-DEFAULT_PORT = 9091
-DEFAULT_LOC = "/mulgara-service/services/ItqlBeanService"
+MULGARA_BASE = "localhost:9090"
+MULGARA_LOC = "/mulgara-service/services/ItqlBeanService"
+FEDORA_BASE = "localhost:9090"
 PREFS       = "<local:///topazproject#filter:model=preferences>"
 LIMIT       = 50000
 MAX         = 10000000
@@ -28,9 +29,8 @@ cli.h(longOpt:'help', 'usage information')
 cli.v(longOpt:'verbose', 'turn on verbose mode')
 cli.s(args:1, 'Date to start with')
 cli.e(args:1, 'Date to end with')
-cli.U(args:1, 'mulgara uri')
-cli.H(args:1, 'mulgara host')
-cli.P(args:1, 'mulgara port')
+cli.F(args:1, 'Fedora host:port')
+cli.M(args:1, 'Mulgara host:port')
 
 if (args.length == 1 && args[0].indexOf(' ') > 0)
   args = new StrTokenizer(args[0], StrMatcher.trimMatcher(), StrMatcher.quoteMatcher())
@@ -38,18 +38,21 @@ if (args.length == 1 && args[0].indexOf(' ') > 0)
 def opt = cli.parse(args)
 if (!opt) return
 if (opt.h) { cli.usage(); return }
-def host = (opt.H) ? opt.H : DEFAULT_HOST
-def port = (opt.P) ? opt.P : DEFAULT_PORT
-def uri  = (opt.U) ? opt.U : "http://${host}:${port}${DEFAULT_LOC}"
+def mulgaraBase = (opt.M) ? opt.M : MULGARA_BASE
+def fedoraBase  = (opt.F) ? opt.F : FEDORA_BASE
+def mulgaraUri  = "http://${mulgaraBase}${MULGARA_LOC}"
+def fedoraUri   = "http://${fedoraBase}/fedora/get"
 def verbose = opt.v
-if (verbose)
-  println "Mulgara URI: $uri"
+if (verbose) {
+  println "Mulgara URI: $mulgaraUri"
+  println "Fedora URI: $fedoraUri"
+}
 
 // Globals
 RI_MODEL='<local:///topazproject#ri>'
 STR_MODEL='<local:///topazproject#str>'
 
-itql = new ItqlHelper(new URI(uri))
+itql = new ItqlHelper(new URI(mulgaraUri))
 def aliases = ItqlHelper.getDefaultAliases()
 aliases['a'] = 'http://www.w3.org/2000/10/annotation-ns#'
 
@@ -82,8 +85,23 @@ def results = itql.doQuery(query + ";", aliases);
 if (verbose)
   println "Results: $results"
 
-println "article, title, count"
+// Get an XmlSlurper object and use our resolver (without resolver, parsing takes forever)
+def slurper = new XmlSlurper()
+slurper.setEntityResolver(CachedSource.getResolver())
+
+println "article, title, author(s), count"
 def ans = new XmlSlurper().parseText(results)
 ans.query[0].solution.each() {
-  println """\"${it.article.'@resource'}","${it.k0.solution.title.text()}",${it.k1}"""
+  // Get article DOI from mulgara answer and convert to fedora URL
+  String doi = it.article.'@resource'
+  def articleUrl = "$fedoraUri/doi:${URLEncoder.encode(doi.substring(9))}/XML"
+
+  // Parse article and get list of authors into a string
+  def authors = "" // An empty string
+  def article = slurper.parse(new URL(articleUrl).getContent())
+  def authorList = article.front.'article-meta'.'author-notes'.corresp.email.list()
+  if (authorList) authors = authorList.toString()[1..-2] // String of comma-separated emails
+
+  // Dump out one of the articles we found as a line of CSV data
+  println """\"${it.article.'@resource'}","${it.k0.solution.title.text()}","$authors", ${it.k1}"""
 }
