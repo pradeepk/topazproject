f250b36bb2d76f7d0449400133e64ae660f60b2e
ebrown <ebrown@48088309-b40f-0410-b149-b14953253675>
2007-03-30 06:11:12 +0000

    Add option to output csv instead of xml and a few other things...
    I probably went overboard, but I've been complaining about the lack of an
    ITQL shell for too long...
     * Add csv output (the obvious thing sans subqueries) - use -m csv
     * Don't echo contents of script files by default
     * History is now kept per command (instead of per-line)
     * History is now saved in ~/.runitql_history
     * For results that are just messages, don't display wrapped xml
     * Support changing runtime via @magic in shell or in files
       * csv mode on: @mode = "csv"
       * xml mode on: @mode = "xml"
       * prettyprint off: @pp = false
       * verbose on: @verbose = true
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@2486 48088309-b40f-0410-b149-b14953253675

--- packages/tools/src/main/resources/scripts/runitql.groovy
+++ packages/tools/src/main/resources/scripts/runitql.groovy
@@ -11,18 +11,22 @@ import org.topazproject.mulgara.itql.ItqlHelper;
 import org.apache.commons.lang.text.StrMatcher;
 import org.apache.commons.lang.text.StrTokenizer;
 import jline.ConsoleReader;
+import jline.History;
 
 // Constants
 MULGARA_BASE = "localhost:9091"
 MULGARA_LOC = "/mulgara-service/services/ItqlBeanService"
+csv = "csv" // In case somebody runs @mode = csv instead of @mode = "csv"
 
 def cli = new CliBuilder(usage: 'runitql [-M mulgarahost:port] [-f script] [-pvN]')
 cli.h(longOpt:'help', 'usage information')
 cli.v(longOpt:'verbose', 'turn on verbose mode')
+cli.e(longOpt:'echo', 'echo script file when running')
 cli.M(args:1, 'Mulgara host:port')
 cli.f(args:1, 'script file')
 cli.p(longOpt:'prompt', 'show the prompt even for a script file')
 cli.N(longOpt:'noprettyprint', 'Do not pretty-print results')
+cli.m(args:1, 'mode')
 
 /* If this is run from maven, re-parse arguments
  * i.e. cd head/topazproject/tools/runscripts; mvn -e -o -Prungroovy exec:java -Dargs="script args"
@@ -36,7 +40,10 @@ if (!opt) return
 if (opt.h) { cli.usage(); return }
 def file = (opt.f) ? new File(opt.f).newInputStream() : System.in
 bPrompt = (opt.p || !opt.f)
-bPrettyPrint = !opt.N
+pp = !opt.N
+mode = opt.m
+echo = opt.v || opt.e || !opt.f
+def writer = echo ? new OutputStreamWriter(System.out) : new StringWriter()
 def mulgaraBase = (opt.M) ? opt.M : MULGARA_BASE
 def mulgaraUri  = "http://${mulgaraBase}${MULGARA_LOC}"
 verbose = opt.v
@@ -48,7 +55,32 @@ itql = new ItqlHelper(new URI(mulgaraUri))
 
 def prompt() {
   if (bPrompt)
-    print "itql> " 
+    print "itql> "
+}
+
+def showResults(result) {
+  def ans = new XmlSlurper().parseText(result)
+  if (ans.query.message.text()) {
+    if (echo) println ans.query.message
+  } else if (mode == "csv" && ans.query[0].solution) {
+    def vars = [ ]
+    ans.query[0].variables.children().each() { vars.add(it.name()) }
+//    println vars.toString()[1..-2]
+    ans.query[0].solution.each() { sol ->
+      def vals = [ ]
+      vars.each() { var ->
+        def val = sol."$var"."@resource".toString()
+        if (!val) val = sol."$var".toString()
+        vals.add(val)
+      }
+      println vals.toString()[1..-2]
+    }
+  } else {
+    if (pp)
+      new XmlNodePrinter().print(new XmlParser().parseText(result))
+    else
+      print "$result\n"
+  }
 }
 
 def execute(query) {
@@ -56,12 +88,19 @@ def execute(query) {
     print "$query\n"
   try {
     result = itql.doQuery("$query;", null)
-    if (bPrettyPrint)
-      new XmlNodePrinter().print(new XmlParser().parseText(result))
-    else
-      print "$result\n"
+    showResults result
   } catch (Throwable e) {
     print "Error running query:\n$e\n"
+    if (verbose)
+      e.printStackTrace()
+  }
+}
+
+def eval(s) {
+  try {
+    evaluate(s)
+  } catch (Throwable e) {
+    print "Error evaluating: $s\n:$e\n"
     if (verbose)
       e.printStackTrace()
   }
@@ -74,22 +113,38 @@ prompt()
 String query = ""
 
 // Use jline for some attempt at readline functionality
-cr = new ConsoleReader(file, new OutputStreamWriter(System.out))
+cr = new ConsoleReader(file, writer)
 cr.setDefaultPrompt("itql> ")
+cr.setUseHistory(false)
+try {
+  histfile = new File(System.getProperty("user.home") + File.pathSeparator + ".runitql_history")
+  cr.setHistory(new History(histfile))
+} catch (IOException e) {
+  println "Error loading history: $e"
+}
 
 while ((line = cr.readLine()) != null) { // Loop over lines with jline
 //file.eachLine { line -> // Old way (without jline)
-  if (line != "" && line[0] == '#') line = '' // strip comments
+  if (line != "" && line[0] == '#') 
+    line = '' // strip comments
+  else if (line != "" && line[0] == '@') {
+    eval(line.substring(1))
+    cr.getHistory().addToHistory(line)
+    line = '' // strip expression
+  }
+
   while (line.indexOf(';') != -1) {
     pos = line.indexOf(';')
     query += " " + line[0..pos-1].trim()
-    if (query.trim() != "")
+    if (query.trim() != "") {
       execute query.trim()
+      cr.getHistory().addToHistory(query.trim() + ";")
+    }
     query = ""
     line = line.substring(pos+1)
   }
   if (line.trim() != "")
-    query += line.trim()
+    query += " " + line.trim()
   prompt()
 }
 
