094a57e97e42e31418853fa7bac403fc062a88d4
ebrown <ebrown@48088309-b40f-0410-b149-b14953253675>
2007-06-05 08:06:41 +0000

    Move config in /etc/topaz/plosone.properties to /etc/topaz/plosone.xml
    Added <hosts><mailhost> to /etc/topaz/plosone.xml
    (It is defaulted if missing - as all things will be shortly.)
    
    Config can still be simplified, but at least it is getting consolidated.
    
    The SpringPlaceholderConfigurer gets its values from commons-configuration
    instead of a properties file.
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@2853 48088309-b40f-0410-b149-b14953253675

---
+++ plosone/libs/conf-helper/src/main/java/org/plos/configuration/SpringPlaceholderConfigurer.java
@@ -0,0 +1,28 @@
+/* $HeadURL::                                                                                    $
+ * $Id: $
+ *
+ * Copyright (c) 2006 by Topaz, Inc.
+ * http://topazproject.org
+ *
+ * Licensed under the Educational Community License version 1.0
+ * http://opensource.org/licenses/ecl1.php
+ */
+package org.plos.configuration;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.apache.commons.configuration.Configuration;
+import java.util.Properties;
+
+public class SpringPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+  protected String resolvePlaceholder(String placeholder, Properties properties) {
+    Configuration conf = ConfigurationStore.getInstance().getConfiguration();
+    String value = null;
+
+    if (conf != null)
+      value = conf.getString(placeholder);
+    if (value == null)
+      value = super.resolvePlaceholder(placeholder, properties);
+
+    return value;
+  }
+}
