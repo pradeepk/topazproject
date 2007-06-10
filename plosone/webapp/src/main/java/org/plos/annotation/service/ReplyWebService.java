04b22451c4abee6d72cb7b790059dde1f0e4e252
ronald <ronald@48088309-b40f-0410-b149-b14953253675>
2007-06-10 13:21:41 +0000

    Make sure all Reply instances from queries are loaded before the transaction is closed. This is a temporary hack until either eager loading is implemented and/or we have transactions spawning the complete request.
    git-svn-id: http://svn.topazproject.org/svn/head@2891 48088309-b40f-0410-b149-b14953253675

--- plosone/webapp/src/main/java/org/plos/annotation/service/ReplyWebService.java
+++ plosone/webapp/src/main/java/org/plos/annotation/service/ReplyWebService.java
@@ -363,8 +363,9 @@ public class ReplyWebService extends BaseAnnotationService {
       TransactionHelper.doInTx(session,
                                new TransactionHelper.Action<List<Reply>>() {
           public List<Reply> run(Transaction tx) {
-            return tx.getSession().createCriteria(Reply.class).add(Restrictions.eq("root", root))
-                      .add(Restrictions.eq("inReplyTo", inReplyTo)).list();
+            return loadInstances(tx.getSession().createCriteria(Reply.class)
+                                   .add(Restrictions.eq("root", root))
+                                   .add(Restrictions.eq("inReplyTo", inReplyTo)).list());
           }
         });
 
@@ -409,9 +410,9 @@ public class ReplyWebService extends BaseAnnotationService {
       TransactionHelper.doInTx(session,
                                new TransactionHelper.Action<List<Reply>>() {
           public List<Reply> run(Transaction tx) {
-            return tx.getSession().createCriteria(Reply.class)
-                      .add(Restrictions.eq("root", root))
-                      .add(Restrictions.walk("inReplyTo", inReplyTo)).list();
+            return loadInstances(tx.getSession().createCriteria(Reply.class)
+                                   .add(Restrictions.eq("root", root))
+                                   .add(Restrictions.walk("inReplyTo", inReplyTo)).list());
           }
         });
 
@@ -532,7 +533,7 @@ public class ReplyWebService extends BaseAnnotationService {
             else
               c.add(Restrictions.eq("state", "" + state));
 
-            return c.list();
+            return loadInstances(c.list());
           }
         });
 
@@ -544,6 +545,21 @@ public class ReplyWebService extends BaseAnnotationService {
       replies[i++] = new ReplyInfo(a, fedora);
 
     return replies;
+  }
+
+  /**
+   * Force the instances to be loaded. This is a temporary hack till OTM supports eager loading
+   * and/or we have a request-wide transaction.
+   *
+   * TODO: remove when eager loading or tx-per-request is implemented.
+   *
+   * @param list the list of replies to load
+   * @return <var>list</var>
+   */
+  private static final List<Reply> loadInstances(List<Reply> list) {
+    for (Reply r : list)
+      ;
+    return list;
   }
 
   /**
