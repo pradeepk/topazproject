pradeepk/topazproject

commit 5c44eda064f0ed8a10fc27c2930d459a6602b62d

Fix a bug in Session.get() where a lazy loaded instance is ignored and a new instance is created under certain cases. This can happen when the instance to be loaded has associations that contain references to this instance (back-pointer) and the setter for this association is triggering a load of the lazy loaded association stub (eg. if the collection is a HashSet)
The fix is to make sure to always use the instance that the users
have seen (via a setter method) by always attempting to merge the
newly loaded object with the ones in cache - even though such a check
is done before the load.

This can in some cases cause a load of the same object from the store
twice. Therefore the copy() merge is optimized to recognize proxy
objects and so the merge is simply to switch the backing object.

To support this optimization, the concept of pristine-proxies has
been changed. Session now keeps track of all proxy objects it 
generates. Also a sub-class of MethodHandler is used to obtain the 
backing store state information.

This fixes #369 where ingest() was failing because Article object 
had a duplicate pristine proxy instance and hence prevented
the write of the articleState.



git-svn-id: http://svn.topazproject.org/svn/head@2920 48088309-b40f-0410-b149-b14953253675

topazproject/libs/otm/src/main/java/org/topazproject/otm/Session.java
@@ -40,15 +40,15 @@ import org.topazproject.otm.query.Results;
  * @author Pradeep Krishnan
  */
 public class Session {
-  private static final Log      log             = LogFactory.getLog(Session.class);
-  private SessionFactory        sessionFactory;
-  private Transaction           txn             = null;
-  private Map<Id, Object>       cleanMap        = new HashMap<Id, Object>();
-  private Map<Id, Object>       dirtyMap        = new HashMap<Id, Object>();
-  private Map<Id, Object>       deleteMap       = new HashMap<Id, Object>();
-  private Set<Id>               pristineProxies = new HashSet<Id>();
-  private Map<Id, Set<Wrapper>> associations    = new HashMap<Id, Set<Wrapper>>();
-  private Set<Id>               currentIds      = new HashSet<Id>();
+  private static final Log               log            = LogFactory.getLog(Session.class);
+  private SessionFactory                 sessionFactory;
+  private Transaction                    txn            = null;
+  private Map<Id, Object>                cleanMap       = new HashMap<Id, Object>();
+  private Map<Id, Object>                dirtyMap       = new HashMap<Id, Object>();
+  private Map<Id, Object>                deleteMap      = new HashMap<Id, Object>();
+  private Map<Id, LazyLoadMethodHandler> proxies        = new HashMap<Id, LazyLoadMethodHandler>();
+  private Map<Id, Set<Wrapper>>          associations   = new HashMap<Id, Set<Wrapper>>();
+  private Set<Id>                        currentIds     = new HashSet<Id>();
 
   /**
    * Creates a new Session object.
@@ -140,7 +140,7 @@ public class Session {
     cleanMap.clear();
     dirtyMap.clear();
     deleteMap.clear();
-    pristineProxies.clear();
+    proxies.clear();
     associations.clear();
   }
 
@@ -276,12 +276,17 @@ public class Session {
     if (o == null) {
       o = getFromStore(clazz, id, checkClass(clazz));
 
-      if (o != null)
-        sync(o, id, false, false, false);
+      if (o != null) {
+        // Must merge here. Associations may have a back-pointer to this Id.
+        // If those associations are loaded from the store even before
+        // we complete this get() operation, there will be an instance
+        // in our cache for this same Id.
+        o = sync(o, id, true, false, false);
+      }
     }
 
     if (o instanceof ProxyObject)
-      o.equals(null); // ensure object is loaded
+      proxies.get(id).load(); // ensure it is loaded
 
     if ((o == null) || clazz.isInstance(o))
       return clazz.cast(o);
@@ -440,7 +445,8 @@ public class Session {
   }
 
   private void write(Id id, Object o, boolean delete) throws OtmException {
-    boolean       pristineProxy = pristineProxies.contains(id);
+    boolean       pristineProxy =
+      (o instanceof ProxyObject) ? (proxies.get(id).getLoaded() == null) : false;
     ClassMetadata cm            = sessionFactory.getClassMetadata(o.getClass());
     TripleStore   store         = sessionFactory.getTripleStore();
 
@@ -510,6 +516,9 @@ public class Session {
 
       p.set(ro.o, assocs);
     }
+
+    if (log.isDebugEnabled())
+      log.debug("Instantiated " + ro.id);
 
     return ro.o;
   }
@@ -586,6 +595,17 @@ public class Session {
     if (!cm.getSourceClass().isAssignableFrom(ocm.getSourceClass()))
       throw new OtmException(cm.toString() + " is not assignable from " + ocm);
 
+    LazyLoadMethodHandler llm = proxies.get(checkObject(o));
+
+    if (llm != null) {
+      llm.setLoaded(other);
+
+      return o;
+    }
+
+    if (log.isDebugEnabled())
+      log.debug("Copy merging " + checkObject(o));
+
     for (Mapper p : cm.getFields()) {
       Mapper op = ocm.getMapperByUri(p.getUri(), p.hasInverseUri(), p.getRdfType());
 
@@ -624,19 +644,40 @@ public class Session {
 
   private Object newDynamicProxy(final Class clazz, final Id id, final ClassMetadata cm)
                           throws OtmException {
-    MethodHandler mi =
-      new MethodHandler() {
+    LazyLoadMethodHandler mi =
+      new LazyLoadMethodHandler() {
         private Object loaded = null;
 
         public Object invoke(Object self, Method m, Method proceed, Object[] args)
                       throws Throwable {
           if (loaded == null) {
-            log.info(m.getName() + " on " + id + " is forcing a load from store");
+            if (log.isDebugEnabled())
+              log.debug(m.getName() + " on " + id + " is forcing a load from store");
+
             loaded = getFromStore(clazz, id, cm);
-            pristineProxies.remove(id);
           }
 
           return m.invoke(loaded, args);
+        }
+
+        public Object getLoaded() {
+          return loaded;
+        }
+
+        public void setLoaded(Object o) {
+          if ((loaded != o) && log.isDebugEnabled())
+            log.debug("Replacing the loaded object for proxy " + id);
+
+          loaded = o;
+        }
+
+        public void load() throws OtmException {
+          if (loaded == null) {
+            if (log.isDebugEnabled())
+              log.debug(id + " is force loaded from store");
+
+            loaded = getFromStore(clazz, id, cm);
+          }
         }
       };
 
@@ -644,7 +685,10 @@ public class Session {
       Object o = sessionFactory.getProxyMapping(clazz).newInstance();
       cm.getIdField().set(o, Collections.singletonList(id.getId()));
       ((ProxyObject) o).setHandler(mi);
-      pristineProxies.add(id);
+      proxies.put(id, mi);
+
+      if (log.isDebugEnabled())
+        log.debug("Dynamic proxy created for " + id);
 
       return o;
     } catch (Exception e) {
@@ -752,5 +796,13 @@ public class Session {
     public String toString() {
       return id;
     }
+  }
+
+  private interface LazyLoadMethodHandler extends MethodHandler {
+    public Object getLoaded();
+
+    public void setLoaded(Object o);
+
+    public void load() throws OtmException;
   }
 }

topazproject/libs/otm/src/it/java/org/topazproject/otm/OtmTest.java
@@ -18,6 +18,7 @@ import org.topazproject.otm.samples.Article;
 import org.topazproject.otm.samples.Grants;
 import org.topazproject.otm.samples.NoPredicate;
 import org.topazproject.otm.samples.NoRdfType;
+import org.topazproject.otm.samples.ObjectInfo;
 import org.topazproject.otm.samples.Permissions;
 import org.topazproject.otm.samples.PrivateAnnotation;
 import org.topazproject.otm.samples.PublicAnnotation;
@@ -1096,6 +1097,84 @@ public class OtmTest extends TestCase {
     } catch (OtmException e) {
       log.warn("test failed", e);
 
+      try {
+        if (tx != null)
+          tx.rollback();
+      } catch (OtmException re) {
+        log.warn("rollback failed", re);
+      }
+
+      throw e; // or display error message
+    } finally {
+      try {
+        session.close();
+      } catch (OtmException ce) {
+        log.warn("close failed", ce);
+      }
+    }
+  }
+  /**
+   * DOCUMENT ME!
+   *
+   * @throws OtmException DOCUMENT ME!
+   */
+  public void test08() throws OtmException {
+    Session     session = factory.openSession();
+    Transaction tx      = null;
+
+    URI         aid     = URI.create("http://localhost/article/1");
+    URI         pid1     = URI.create("http://localhost/article/1/part/1");
+    URI         pid2     = URI.create("http://localhost/article/1/part/2");
+
+    try {
+      tx = session.beginTransaction();
+
+      Article a = new Article();
+      a.setUri(aid);
+
+      ObjectInfo p1 = new ObjectInfo();
+      ObjectInfo p2 = new ObjectInfo();
+      p1.setUri(pid1);
+      p2.setUri(pid2);
+      p1.setIsPartOf(a);
+      p2.setIsPartOf(a);
+      a.getParts().add(p1);
+      a.getParts().add(p2);
+
+      session.saveOrUpdate(a);
+
+      tx.commit(); // Flush happens automatically
+    } catch (OtmException e) {
+      try {
+        if (tx != null)
+          tx.rollback();
+      } catch (OtmException re) {
+        log.warn("rollback failed", re);
+      }
+
+      throw e; // or display error message
+    } finally {
+      try {
+        session.close();
+      } catch (OtmException ce) {
+        log.warn("close failed", ce);
+      }
+    }
+    session   = factory.openSession();
+    tx        = null;
+
+    try {
+      tx = session.beginTransaction();
+
+      Article a = session.get(Article.class, aid.toString());
+
+      assertNotNull(a);
+      assertEquals(2, a.getParts().size());
+      for (ObjectInfo o : a.getParts())
+        assertTrue(o.getIsPartOf() == a);
+
+      tx.commit(); // Flush happens automatically
+    } catch (OtmException e) {
       try {
         if (tx != null)
           tx.rollback();

topazproject/libs/otm/src/test/java/org/topazproject/otm/samples/Article.java
@@ -10,9 +10,11 @@
 package org.topazproject.otm.samples;
 
 import java.net.URI;
-import java.util.Date;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.topazproject.otm.annotations.Entity;
 import org.topazproject.otm.annotations.Rdf;
@@ -26,83 +28,21 @@ import org.topazproject.otm.annotations.Predicate;
  * @version $Id$
  */
 @Entity(type = Rdf.topaz + "Article", model = "ri")
-public class Article {
-  @Id
-  private URI      uri;
-  @Predicate(uri = Rdf.dc + "title")
-  private String   title;
-  @Predicate(uri = Rdf.dc + "description")
-  private String   description;
+public class Article extends ObjectInfo {
   @Predicate(uri = Rdf.dc_terms + "available")
   private Date     articleDate;
-  @Predicate(uri = Rdf.dc + "creator")
-  private String[] authors;
   @Predicate(uri = Rdf.dc + "subject")
   private String[] subjects;
   @Predicate(uri = Rdf.topaz + "hasCategory")
   private String[] categories;
-  @Predicate(uri = Rdf.topaz + "articleState")
-  private int      state;
   @Predicate(uri = Annotation.NS + "annotates", inverse=true, notOwned=true)
   private List<PublicAnnotation> publicAnnotations = new ArrayList<PublicAnnotation>();
   @Predicate(uri = Annotation.NS + "annotates", inverse=true, notOwned=true)
   private List<PrivateAnnotation> privateAnnotations = new ArrayList<PrivateAnnotation>();
   @Predicate(uri = Reply.NS + "inReplyTo", inverse=true, notOwned=true)
   private List<ReplyThread> replies = new ArrayList<ReplyThread>();
-
-  /**
-   * Get the URI.
-   *
-   * @return the URI.
-   */
-  public URI getUri() {
-    return uri;
-  }
-
-  /**
-   * Set the URI.
-   *
-   * @param uri the URI.
-   */
-  public void setUri(URI uri) {
-    this.uri = uri;
-  }
-
-  /**
-   * Get the article title.
-   *
-   * @return the article title.
-   */
-  public String getTitle() {
-    return title;
-  }
-
-  /**
-   * Set the article title.
-   *
-   * @param title the article title.
-   */
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  /**
-   * Get the article description (abstract).
-   *
-   * @return the article description.
-   */
-  public String getDescription() {
-    return description;
-  }
-
-  /**
-   * Set the article description.
-   *
-   * @param description the article description.
-   */
-  public void setDescription(String description) {
-    this.description = description;
-  }
+  @Predicate(uri = Rdf.dc_terms + "hasPart")
+  private Set<ObjectInfo> parts = new HashSet<ObjectInfo>();
 
   /**
    * Get the article date.
@@ -120,24 +60,6 @@ public class Article {
    */
   public void setArticleDate(Date articleDate) {
     this.articleDate = articleDate;
-  }
-
-  /**
-   * Get the article's authors.
-   *
-   * @return the article's authors.
-   */
-  public String[] getAuthors() {
-    return authors;
-  }
-
-  /**
-   * Set the article's authors.
-   *
-   * @param authors the article's authors.
-   */
-  public void setAuthors(String[] authors) {
-    this.authors = authors;
   }
 
   /**
@@ -178,22 +100,12 @@ public class Article {
     this.categories = categories;
   }
 
-  /**
-   * Get the article's state.
-   *
-   * @return the article's state.
-   */
-  public int getState() {
-    return state;
+  public Set<ObjectInfo> getParts() {
+    return parts;
   }
 
-  /**
-   * Set the article's state.
-   *
-   * @param state the article's state.
-   */
-  public void setState(int state) {
-    this.state = state;
+  public void setParts(Set<ObjectInfo> parts) {
+    this.parts = parts;
   }
 
   public void setPrivateAnnotations(List<PrivateAnnotation> privateAnnotations) {

topazproject/libs/otm/src/test/java/org/topazproject/otm/samples/ObjectInfo.java
@@ -1,0 +1,311 @@
+/* $HeadURL::                                                                                     $
+ * $Id$
+ *
+ * Copyright (c) 2006 by Topaz, Inc.
+ * http://topazproject.org
+ *
+ * Licensed under the Educational Community License version 1.0
+ * http://opensource.org/licenses/ecl1.php
+ */
+package org.topazproject.otm.samples;
+
+import java.net.URI;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
+import org.topazproject.otm.annotations.Entity;
+import org.topazproject.otm.annotations.Id;
+import org.topazproject.otm.annotations.Predicate;
+import org.topazproject.otm.annotations.PredicateMap;
+import org.topazproject.otm.annotations.Rdf;
+
+/**
+ * Model for the objects that are stored mostly in mulgara and partially in the blob-store
+ * (fedora).<p>
+ *
+ * Much of the structure here mirrors how things were originally stored in fedora.
+ *
+ * @author Eric Brown
+ */
+@Entity(model = "ri")
+public class ObjectInfo {
+  @Id
+  private URI uri;
+
+  // dublin-core predicates:
+  @Predicate(uri = Rdf.dc + "title")
+  private String title;
+  @Predicate(uri = Rdf.dc + "description", dataType = Rdf.rdf + "XMLLiteral")
+  private String description;
+  @Predicate(uri = Rdf.dc + "creator")
+  private Set<String> authors = new HashSet<String>();
+  @Predicate(uri = Rdf.dc + "date", dataType = Rdf.xsd + "date")
+  private Date date;
+  @Predicate(uri = Rdf.dc + "identifier")
+  private String identifier; // looks like a uri -- doi as a uri (for doi to uri conversion?)
+  @Predicate(uri = Rdf.dc + "rights", dataType = Rdf.rdf + "XMLLiteral")
+  private String rights;
+  /** Will be: http://purl.org/dc/dcmitype/{Text|StillImage|Dataset|MovingImage|Sound} */
+  @Predicate(uri = Rdf.dc + "type")
+  private URI dc_type;
+  @Predicate(uri = Rdf.dc + "contributor")
+  private Set<String> contributors = new HashSet<String>();
+
+  @Predicate(uri = Rdf.dc_terms + "isPartOf")
+  private Article isPartOf;
+
+  // Topaz predicates:
+
+  @Predicate(uri = Rdf.topaz + "nextObject")
+  private ObjectInfo nextObject;
+  @Predicate(uri = Rdf.topaz + "isPID")
+  private String pid;
+  /** The state of this article (or its parts). */
+  @Predicate(uri = Rdf.topaz + "articleState")
+  private int state;
+  @Predicate(uri = Rdf.topaz + "hasRepresentation")
+  private Set<String> representations = new HashSet<String>(); // PDF, TIF, PNG_S, DOC, ...
+  @Predicate(uri = Rdf.topaz + "contextElement")
+  private String contextElement;
+
+  /**
+   * There is a convention for two dynamic predicates:
+   * <ul>
+   *  <li>&lt;topaz:<em>content-type</em>-contentType&gt;
+   *  <li>&lt;topaz:<em>content-type</em>-objectSize&gt;
+   * </ul>
+   *
+   * So the data map should have two entries. One for the contentType and one
+   * for the objectSize.
+   */
+  @PredicateMap
+  private Map<String, List<String>> data = new HashMap<String, List<String>>();
+
+  /**
+   * @return the authors
+   */
+  public Set<String> getAuthors() {
+    return authors;
+  }
+
+  /**
+   * @param authors the set of authors for this object
+   */
+  public void setAuthors(Set<String> authors) {
+    this.authors = authors;
+  }
+
+  /**
+   * @return the contextElement
+   */
+  public String getContextElement() {
+    return contextElement;
+  }
+
+  /**
+   * @param contextElement the contextElement to set
+   */
+  public void setContextElement(String contextElement) {
+    this.contextElement = contextElement;
+  }
+
+  /**
+   * @return the contributors
+   */
+  public Set<String> getContributors() {
+    return contributors;
+  }
+
+  /**
+   * @param contributors the contributors to set
+   */
+  public void setContributors(Set<String> contributors) {
+    this.contributors = contributors;
+  }
+
+  /**
+   * @return the data
+   */
+  public Map<String, List<String>> getData() {
+    return data;
+  }
+
+  /**
+   * @param data the data to set
+   */
+  public void setData(Map<String, List<String>> data) {
+    this.data = data;
+  }
+
+  /**
+   * @return the date
+   */
+  public Date getDate() {
+    return date;
+  }
+
+  /**
+   * @param date the date to set
+   */
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  /**
+   * @return the dc_type
+   */
+  public URI getDc_type() {
+    return dc_type;
+  }
+
+  /**
+   * @param dc_type the dc_type to set
+   */
+  public void setDc_type(URI dc_type) {
+    this.dc_type = dc_type;
+  }
+
+  /**
+   * @return the description
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+   * @param description the description to set
+   */
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  /**
+   * @return the id
+   */
+  public URI getUri() {
+    return uri;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setUri(URI uri) {
+    this.uri = uri;
+  }
+
+  /**
+   * @return the identifier
+   */
+  public String getIdentifier() {
+    return identifier;
+  }
+
+  /**
+   * @param identifier the identifier to set
+   */
+  public void setIdentifier(String identifier) {
+    this.identifier = identifier;
+  }
+
+  /**
+   * @return the isPartOf
+   */
+  public Article getIsPartOf() {
+    return isPartOf;
+  }
+
+  /**
+   * @param isPartOf the isPartOf to set
+   */
+  public void setIsPartOf(Article isPartOf) {
+    this.isPartOf = isPartOf;
+  }
+
+  /**
+   * @return the nextObject
+   */
+  public ObjectInfo getNextObject() {
+    return nextObject;
+  }
+
+  /**
+   * @param nextObject the nextObject to set
+   */
+  public void setNextObject(ObjectInfo nextObject) {
+    this.nextObject = nextObject;
+  }
+
+  /**
+   * @return the pid
+   */
+  public String getPid() {
+    return pid;
+  }
+
+  /**
+   * @param pid the pid to set
+   */
+  public void setPid(String pid) {
+    this.pid = pid;
+  }
+
+  /**
+   * @return the representations
+   */
+  public Set<String> getRepresentations() {
+    return representations;
+  }
+
+  /**
+   * @param representations the representations to set
+   */
+  public void setRepresentations(Set<String> representations) {
+    this.representations = representations;
+  }
+
+  /**
+   * @return the rights
+   */
+  public String getRights() {
+    return rights;
+  }
+
+  /**
+   * @param rights the rights to set
+   */
+  public void setRights(String rights) {
+    this.rights = rights;
+  }
+
+  /**
+   * @return the state
+   */
+  public int getState() {
+    return state;
+  }
+
+  /**
+   * @param state the state to set
+   */
+  public void setState(int state) {
+    this.state = state;
+  }
+
+  /**
+   * @return the title
+   */
+  public String getTitle() {
+    return title;
+  }
+
+  /**
+   * @param title the title to set
+   */
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}

commit e784252549e7482bfed1eb91828d41beb555366e

Fix for an issue introduced in r2920. Don't assume ProxyObjects are only created by OTM. Users may be using Javassist too.
git-svn-id: http://svn.topazproject.org/svn/head@2922 48088309-b40f-0410-b149-b14953253675

topazproject/libs/otm/src/main/java/org/topazproject/otm/Session.java
@@ -285,8 +285,12 @@ public class Session {
       }
     }
 
-    if (o instanceof ProxyObject)
-      proxies.get(id).load(); // ensure it is loaded
+    if (o instanceof ProxyObject) {
+      LazyLoadMethodHandler llm = proxies.get(id);
+
+      if (llm != null)
+        llm.load(); // ensure it is loaded
+    }
 
     if ((o == null) || clazz.isInstance(o))
       return clazz.cast(o);
@@ -445,10 +449,10 @@ public class Session {
   }
 
   private void write(Id id, Object o, boolean delete) throws OtmException {
-    boolean       pristineProxy =
-      (o instanceof ProxyObject) ? (proxies.get(id).getLoaded() == null) : false;
-    ClassMetadata cm            = sessionFactory.getClassMetadata(o.getClass());
-    TripleStore   store         = sessionFactory.getTripleStore();
+    LazyLoadMethodHandler llm           = (o instanceof ProxyObject) ? proxies.get(id) : null;
+    boolean               pristineProxy = (llm != null) ? (llm.getLoaded() == null) : false;
+    ClassMetadata         cm            = sessionFactory.getClassMetadata(o.getClass());
+    TripleStore           store         = sessionFactory.getTripleStore();
 
     if (delete || !pristineProxy) {
       if (log.isDebugEnabled())