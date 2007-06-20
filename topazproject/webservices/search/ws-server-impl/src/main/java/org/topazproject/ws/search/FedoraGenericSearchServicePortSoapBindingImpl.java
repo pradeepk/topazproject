8f6f09f7adac803faf85fcefbafe5de2b6aa8c04
pradeep <pradeep@48088309-b40f-0410-b149-b14953253675>
2007-06-20 04:19:45 +0000

    More cleaning up.
     * Removed users service
     * Removed common/ws-server-impl and common/ws-web-app
     * Removed references to the above from search service.
     * Removed remaining references to XACML in search service (see r2950)
     * Removed CAS references from search service
     * Removed references to TopazContext from search service.
    
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@2957 48088309-b40f-0410-b149-b14953253675

--- topazproject/webservices/search/ws-server-impl/src/main/java/org/topazproject/ws/search/FedoraGenericSearchServicePortSoapBindingImpl.java
+++ topazproject/webservices/search/ws-server-impl/src/main/java/org/topazproject/ws/search/FedoraGenericSearchServicePortSoapBindingImpl.java
@@ -20,12 +20,7 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.topazproject.common.impl.TopazContext;
-import org.topazproject.common.ws.ImplInvocationHandler;
-import org.topazproject.common.ws.WSTopazContext;
 import org.plos.configuration.ConfigurationStore;
-import org.topazproject.xacml.ws.WSXacmlUtil;
-import org.topazproject.xacml.AbstractSimplePEP;
 
 import dk.defxws.fedoragsearch.server.Operations; // API
 import dk.defxws.fedoragsearch.server.SOAPImpl;
@@ -36,8 +31,6 @@ public class FedoraGenericSearchServicePortSoapBindingImpl implements Operations
   private static final Log log =
       LogFactory.getLog(FedoraGenericSearchServicePortSoapBindingImpl.class);
 
-  private TopazContext ctx = new WSTopazContext(getClass().getName());
-  private WSSearchPEP  pep = null;
   private Operations   impl;
 
   public void init(Object context) throws ServiceException {
@@ -45,11 +38,6 @@ public class FedoraGenericSearchServicePortSoapBindingImpl implements Operations
       log.trace("ServiceLifecycle#init");
 
     try {
-      // get the pep
-      pep = new WSSearchPEP((ServletEndpointContext) context);
-
-      ctx.init(context); // ctx contains itql, apim, user, uploader, apia, ...
-
       // override fgs' config from our commons-config stuff
       Configuration tcfg = ConfigurationStore.getInstance().getConfiguration();
       tcfg = tcfg.subset("topaz.fedoragsearch");
@@ -60,7 +48,7 @@ public class FedoraGenericSearchServicePortSoapBindingImpl implements Operations
       }
 
       // create the impl
-      impl = (Operations)ImplInvocationHandler.newProxy(new SOAPImpl(), ctx, log);
+      impl = new SOAPImpl();
     } catch (Exception e) {
       log.error("Failed to initialize SearchImpl", e);
       throw new ServiceException(e);
@@ -71,7 +59,6 @@ public class FedoraGenericSearchServicePortSoapBindingImpl implements Operations
     if (log.isTraceEnabled())
       log.trace("ServiceLifecycle#destroy");
 
-    ctx.destroy();
     impl = null;
   }
 
@@ -80,7 +67,6 @@ public class FedoraGenericSearchServicePortSoapBindingImpl implements Operations
    */
   public String updateIndex(String action, String value, String repositoryName, String indexName,
                             String indexDocXslt, String resultPageXslt) throws RemoteException {
-    setupContext();
     return impl.updateIndex(action, value, repositoryName, indexName, indexDocXslt, resultPageXslt);
   }
 
@@ -90,7 +76,6 @@ public class FedoraGenericSearchServicePortSoapBindingImpl implements Operations
   public String gfindObjects(String query, long hitPageStart, int hitPageSize, int snippetsMax,
                              int fieldMaxLength, String indexName, String resultPageXslt)
       throws RemoteException {
-    setupContext();
     return impl.gfindObjects(query, hitPageStart, hitPageSize, snippetsMax, fieldMaxLength,
                              indexName, resultPageXslt);
   }
@@ -100,7 +85,6 @@ public class FedoraGenericSearchServicePortSoapBindingImpl implements Operations
    */
   public String browseIndex(String startTerm, int termPageSize, String fieldName, String indexName,
                             String resultPageXslt) throws RemoteException {
-    setupContext();
     return impl.browseIndex(startTerm, termPageSize, fieldName, indexName, resultPageXslt);
   }
 
@@ -109,7 +93,6 @@ public class FedoraGenericSearchServicePortSoapBindingImpl implements Operations
    */
   public String getRepositoryInfo(String repositoryName, String resultPageXslt)
       throws RemoteException {
-    setupContext();
     return impl.getRepositoryInfo(repositoryName, resultPageXslt);
   }
 
@@ -117,26 +100,7 @@ public class FedoraGenericSearchServicePortSoapBindingImpl implements Operations
    * @see dk.defxws.fedoragsearch.server.Operations#getIndexInfo
    */
   public String getIndexInfo(String indexName, String resultPageXslt) throws RemoteException {
-    setupContext();
     return impl.getIndexInfo(indexName, resultPageXslt);
   }
 
-  private void setupContext() throws RemoteException {
-    ctx.activate();
-    ctx.passivate();
-  }
-
-  private static class WSSearchPEP extends AbstractSimplePEP {
-    protected static final String[] SUPPORTED_ACTIONS = new String[] { "articles:readMetaData" };
-    protected static final String[][] SUPPORTED_OBLIGATIONS = new String[][] { null };
-
-    static {
-      init(WSSearchPEP.class, SUPPORTED_ACTIONS, SUPPORTED_OBLIGATIONS);
-    }
-
-    public WSSearchPEP(ServletEndpointContext context) throws Exception {
-      super(WSXacmlUtil.lookupPDP(context, "topaz.search.pdpName"),
-            WSXacmlUtil.createSubjAttrs(context));
-    }
-  }
 }
