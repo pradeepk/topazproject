5d57fe478f502116882a3ac8ecda483d19ee77b9
pradeep <pradeep@48088309-b40f-0410-b149-b14953253675>
2007-06-22 16:58:26 +0000

    Make the XACML Itql queries share the same ItqlHandle with OTM.
    git-svn-id: http://svn.topazproject.org/svn/head@2997 48088309-b40f-0410-b149-b14953253675

--- plosone/webapp/src/main/java/org/plos/xacml/cond/ItqlQueryFunction.java
+++ plosone/webapp/src/main/java/org/plos/xacml/cond/ItqlQueryFunction.java
@@ -15,20 +15,19 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.pool.BaseKeyedPoolableObjectFactory;
-import org.apache.commons.pool.KeyedObjectPool;
-import org.apache.commons.pool.impl.GenericKeyedObjectPool;
-
-import org.topazproject.authentication.ProtectedService;
-import org.topazproject.authentication.ProtectedServiceFactory;
-
-import org.plos.configuration.ConfigurationStore;
+import org.plos.configuration.OtmConfiguration;
 
 import org.topazproject.mulgara.itql.ItqlHelper;
 import org.topazproject.mulgara.itql.StringAnswer;
+
+import org.topazproject.otm.Connection;
+import org.topazproject.otm.Session;
+import org.topazproject.otm.SessionFactory;
+import org.topazproject.otm.Transaction;
+import org.topazproject.otm.stores.ItqlStore.ItqlStoreConnection;
 
 import com.sun.xacml.EvaluationCtx;
 import com.sun.xacml.cond.EvaluationResult;
@@ -40,30 +39,12 @@ import com.sun.xacml.cond.EvaluationResult;
  * @author Pradeep Krishnan
  */
 public class ItqlQueryFunction extends DBQueryFunction {
+  private static final Log log = LogFactory.getLog(ItqlQueryFunction.class);
+
   /**
    * The name of this function as it appears in XACML policies.
    */
   public static String FUNCTION_NAME = FUNCTION_BASE + "itql";
-  private static KeyedObjectPool pool;
-
-  static {
-    GenericKeyedObjectPool.Config poolConfig = new GenericKeyedObjectPool.Config();
-    poolConfig.maxActive                       = 20;
-    poolConfig.maxIdle                         = 20;
-    poolConfig.maxTotal                        = 20;
-    poolConfig.maxWait                         = 1000; // time to wait for handle if none available
-    poolConfig.minEvictableIdleTimeMillis      = 5 * 60 * 1000; // 5 min
-    poolConfig.minIdle                         = 0;
-    poolConfig.numTestsPerEvictionRun          = 5;
-    poolConfig.testOnBorrow                    = false;
-    poolConfig.testOnReturn                    = false;
-    poolConfig.testWhileIdle                   = true;
-    poolConfig.timeBetweenEvictionRunsMillis   = 5 * 60 * 1000; // 5 min
-    poolConfig.whenExhaustedAction             = GenericKeyedObjectPool.WHEN_EXHAUSTED_BLOCK;
-
-    pool                                       = new GenericKeyedObjectPool(new ItqlHelperFactory(),
-                                                                            poolConfig);
-  }
 
   /**
    * Creates an ItqlQueryFunction instance.
@@ -97,37 +78,66 @@ public class ItqlQueryFunction extends DBQueryFunction {
                                        String[] bindings)
                                 throws QueryException {
     // Create the query
-    query = bindStatic(query, bindings);
+    query                      = bindStatic(query, bindings);
 
-    ItqlHelper itql;
+    StringAnswer answer;
+    Transaction  txn           = null;
+    boolean      wasActive     = false;
 
     // Create an ItqlHelper
     try {
-      itql = (ItqlHelper) pool.borrowObject(conf);
-    } catch (QueryException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new QueryException("Unable to initialize connector to ITQL interpreter bean service "
-                               + conf, e);
-    }
+      OtmConfiguration oc = OtmConfiguration.getInstance();
 
-    StringAnswer answer;
+      if (oc == null)
+        throw new QueryException("" + OtmConfiguration.class + " not initialized");
 
-    // Execute the query
-    boolean returnObject = false;
+      SessionFactory sf = oc.getFactory();
 
-    try {
-      answer         = new StringAnswer(itql.doQuery(query, null));
-      returnObject   = true;
+      if (sf == null)
+        throw new QueryException("" + SessionFactory.class + " not initialized");
+
+      Session s = sf.getCurrentSession();
+
+      if (s == null)
+        throw new QueryException("Failed to get current otm session");
+
+      txn         = s.getTransaction();
+      wasActive   = (txn != null);
+
+      if (txn == null)
+        txn = s.beginTransaction();
+
+      if (txn == null)
+        throw new QueryException("Failed to start an otm transaction");
+
+      Connection isc = txn.getConnection();
+
+      if (isc == null)
+        throw new QueryException("Failed to get an otm connection");
+
+      if (!(isc instanceof ItqlStoreConnection))
+        throw new QueryException("Expecting an instance of " + ItqlStoreConnection.class);
+
+      ItqlHelper itql = ((ItqlStoreConnection) isc).getItqlHelper();
+
+      if (itql == null)
+        throw new QueryException("Failed to get an instance of " + ItqlHelper.class);
+
+      answer = new StringAnswer(itql.doQuery(query, null));
+
+      if (!wasActive)
+        txn.commit();
+
+      txn = null;
     } catch (Exception e) {
       throw new QueryException("query '" + query + "' execution failed.", e);
     } finally {
       try {
-        if (returnObject)
-          pool.returnObject(conf, itql);
-        else
-          pool.invalidateObject(conf, itql);
+        if (!wasActive && (txn != null))
+          txn.rollback();
       } catch (Throwable t) {
+        if (log.isDebugEnabled())
+          log.debug("rollback failed", t);
       }
     }
 
@@ -190,43 +200,5 @@ public class ItqlQueryFunction extends DBQueryFunction {
     s.append(parts[i]);
 
     return s.toString();
-  }
-
-  private static class ItqlHelperFactory extends BaseKeyedPoolableObjectFactory {
-    private Configuration configuration = ConfigurationStore.getInstance().getConfiguration();
-
-    public Object makeObject(Object key) throws Exception {
-      Configuration subset = configuration.subset((String) key);
-
-      if ((subset == null) || (subset.getString("uri") == null))
-        throw new QueryException("Can't find configuration " + key);
-
-      ProtectedService service;
-
-      try {
-        service = ProtectedServiceFactory.createService(subset, (HttpSession) null);
-      } catch (Exception e) {
-        throw new QueryException("Unable to obtain an itql service configuration instance", e);
-      }
-
-      String     serviceUri = service.getServiceUri();
-
-      ItqlHelper itql;
-
-      // Create an ItqlHelper
-      try {
-        itql                = new ItqlHelper(service);
-      } catch (Exception e) {
-        throw new QueryException("Unable to initialize connector to ITQL interpreter bean service at "
-                                 + serviceUri, e);
-      }
-
-      return itql;
-    }
-
-    public void destroyObject(Object key, Object obj) throws Exception {
-      ItqlHelper itql = (ItqlHelper) obj;
-      itql.close();
-    }
   }
 }
