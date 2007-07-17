pradeepk/topazproject

commit f4e7d1ea7f7b0004e5d97d66ad9c8be50f5f9728

Share the ItqlHelper handle from otm transaction in ArticleUtil.delete().
ArticleUtil is shared with command line tools and it is unaware of 
otm-models and SessionFactory. Instead of adding all that support for 
command line tools now into ArticleUtil, we should have an integrated 
plosone.jar (as has been discussed before) that can be a more useful lib 
than this.

Also the getObjectURL() method in ArticleUtil is no longer called from 
plosone app. Instead a static method is added to create the fedora
REST URL for accessing a representation.

With this, all usages of ItqlHelper in plosone is tied to an OTM 
transaction and hence we are ready to do the 'Open View in Session'
(See http://www.hibernate.org/43.html).



git-svn-id: http://svn.topazproject.org/svn/head@3090 48088309-b40f-0410-b149-b14953253675

plosone/webapp/src/main/java/org/plos/article/service/ArticleOtmService.java
@@ -180,31 +180,16 @@ public class ArticleOtmService extends BaseConfigurableService {
     // logged in user is automatically resolved by the ServletActionContextAttribute
     pep.checkAccess(ArticlePEP.GET_OBJECT_URL, URI.create(obj));
 
-    // let the Article utils do the real work
-    ArticleUtil articleUtil = null;
-    try {
-      articleUtil = new ArticleUtil();  // no arg, utils use default config
-    } catch (MalformedURLException ex) {
-      throw new RuntimeException(ex);
-    } catch (ServiceException se) {
-      throw new RemoteException("Failed getting URL for \"" + obj + "\", \"" + rep + "\"", se);
-    }
+    return TransactionHelper.doInTxE(session,
+                              new TransactionHelper.ActionE<String, NoSuchObjectIdException>() {
+      public String run(Transaction tx) throws NoSuchObjectIdException {
+        Article a = tx.getSession().get(Article.class, obj);
+        if (a == null)
+          throw new NoSuchObjectIdException(obj);
 
-    // trap the Exception for better debgging
-    String objectURL = null;
-    try {
-      objectURL= articleUtil.getObjectURL(obj, rep);
-      if (log.isDebugEnabled()) {
-        log.debug("ArticleOtmService.getObjectURL("+obj+", " + rep + ") = " + objectURL);
+        return ArticleUtil.getFedoraDataStreamURL(a.getPid(), rep);
       }
-    } catch (NoSuchObjectIdException nsoe) {
-      if (log.isDebugEnabled()) {
-        log.debug("failed to ArticleOtmService.getObjectURL(" + obj + ", " + rep + ")");
-      }
-      throw nsoe;
-    }
-
-    return objectURL;
+    });
   }
 
   /**
@@ -224,14 +209,21 @@ public class ArticleOtmService extends BaseConfigurableService {
     // logged in user is automatically resolved by the ServletActionContextAttribute
     pep.checkAccess(ArticlePEP.DELETE_ARTICLE, URI.create(article));
 
-    // let the Article utils do the real work
-    ArticleUtil articleUtil = null;
+    Transaction txn = null;
     try {
-     articleUtil = new ArticleUtil();  // no arg, utils use default config
-    } catch (MalformedURLException ex) {
-      throw new RuntimeException(ex);
+      ctx.activate();
+      txn = session.beginTransaction();
+      ItqlHelper itql = ((ItqlStoreConnection)txn.getConnection()).getItqlHelper();
+      ArticleUtil.delete(article, itql);
+      txn = null;
+    } finally {
+      try {
+        if (txn != null)
+          txn.rollback();
+      } catch (Throwable t) {
+        log.debug("rollback failed", t);
+      }
     }
-    articleUtil.delete(article);
   }
 
   /**

plosone/libs/article-util/src/main/java/org/plos/article/util/ArticleUtil.java
@@ -58,7 +58,7 @@ public class ArticleUtil {
   private final ItqlHelper itql;
   private final FgsOperations[] fgs;
   private final Ingester   ingester;
-  private final URI        fedoraServer;
+  private static final URI        fedoraServer = getFedoraBaseUri();
 
   private static final String ITQL_GET_PID =
       ("select $pid from ${MODEL} where <${uri}> <topaz:isPID> $pid;").
@@ -117,7 +117,6 @@ public class ArticleUtil {
     this.itql     = new ItqlHelper(mulgaraSvc);
     this.fgs      = getFgsOperations();
     this.ingester = new Ingester(itql, apim, uploader, fgs);
-    this.fedoraServer = getFedoraBaseUri();
   }
 
   /**
@@ -144,10 +143,31 @@ public class ArticleUtil {
    * @throws RemoteException if some other error occured
    */
   public void delete(String article) throws NoSuchArticleIdException, RemoteException {
-    String txn = "delete " + article;
+    delete(article, itql, apim, fgs);
+  }
+
+  public static void delete(String article, ItqlHelper itql)
+    throws NoSuchArticleIdException, RemoteException {
+    try {
+      delete(article, itql, APIMStubFactory.create(
+                             new PasswordProtectedService(CONF.getString("topaz.services.fedora.uri"),
+                                      CONF.getString("topaz.services.fedora.userName"),
+                                      CONF.getString("topaz.services.fedora.password"))),
+                            getFgsOperations());
+    } catch (MalformedURLException e) {
+      throw new RemoteException("Bad configuration", e);
+    } catch (ServiceException e) {
+      throw new RemoteException("Failed to load search client stubs", e);
+    }
+  }
+
+  public static void delete(String article, ItqlHelper itql, FedoraAPIM apim,
+                  FgsOperations[] fgs) throws NoSuchArticleIdException, RemoteException {
+    String txn = itql.isInTransaction() ? null : ("delete " + article);
     try {
       // TODO: Remove article via otm
-      itql.beginTxn(txn);
+      if (txn != null)
+        itql.beginTxn(txn);
 
       String[][] objList = findAllObjects(article, itql);
       if (log.isDebugEnabled())
@@ -193,7 +213,8 @@ public class ArticleUtil {
         itql.doUpdate(ItqlHelper.bindValues(ITQL_DELETE_PP, "subj", uri), null);
       }
 
-      itql.commitTxn(txn);
+      if (txn != null)
+        itql.commitTxn(txn);
       txn = null;
     } catch (AnswerException ae) {
       throw new RemoteException("Error querying RDF", ae);
@@ -228,6 +249,11 @@ public class ArticleUtil {
     return fedoraServer.resolve(path).toString();
   }
 
+  public static String getFedoraDataStreamURL(String pid, String ds) {
+    String path = "/fedora/get/" + pid + "/" + ds;
+    return fedoraServer.resolve(path).toString();
+  }
+
   private static FgsOperations[] getFgsOperations() throws ServiceException {
     FgsOperations ops[] = new FgsOperations[FGS_URLS.size()];
     for (int i = 0; i < ops.length; i++) {
@@ -258,7 +284,7 @@ public class ArticleUtil {
     return uri;
   }
 
-  protected String[][] findAllObjects(String subj, ItqlHelper itql)
+  protected static String[][] findAllObjects(String subj, ItqlHelper itql)
       throws NoSuchArticleIdException, RemoteException, AnswerException {
     ItqlHelper.validateUri(subj, "subject");
 

plosone/webapp/src/main/java/org/plos/article/service/RepresentationInfo.java
@@ -93,18 +93,7 @@ public class RepresentationInfo {
         }
       }
 
-      String url = null;
-      try {
-        url = (new ArticleUtil()).getObjectURL(objectInfo.getId().toString(), format);
-      } catch (MalformedURLException ex) {
-        // TODO: data integ issue
-      } catch (NoSuchObjectIdException ex) {
-        // TODO: data integ issue
-      } catch (ServiceException ex) {
-        // TODO: data integ issue
-      } catch (RemoteException ex) {
-        // TODO: data integ issue
-      }
+      String url = ArticleUtil.getFedoraDataStreamURL(objectInfo.getPid(), format);
 
       RepresentationInfo representationInfo = new RepresentationInfo();
       representationInfo.setContentType(contentType);

commit 2d75d501e3f8d5c3b96f855064821ddde200b377

Fix a bug introduced in r3090. Article components also have representations.
git-svn-id: http://svn.topazproject.org/svn/head@3214 48088309-b40f-0410-b149-b14953253675

plosone/webapp/src/main/java/org/plos/article/service/ArticleOtmService.java
@@ -183,7 +183,7 @@ public class ArticleOtmService extends BaseConfigurableService {
     return TransactionHelper.doInTxE(session,
                               new TransactionHelper.ActionE<String, NoSuchObjectIdException>() {
       public String run(Transaction tx) throws NoSuchObjectIdException {
-        Article a = tx.getSession().get(Article.class, obj);
+        ObjectInfo a = tx.getSession().get(ObjectInfo.class, obj);
         if (a == null)
           throw new NoSuchObjectIdException(obj);
 