pradeepk/topazproject

commit 38f4bace60070a1fe5711756eeb594aa28317e97

Removed the wrapped object in lazy loaded objects. Instead values are directly set on the lazy loaded objects.
Also a bit of a refactoring on the Session.get() impl on the triple-store.
Added an AbstractTripleStore. (todo move up more things to AbstractTripleStore 
from ItqlStore)

Closes #381.



git-svn-id: http://svn.topazproject.org/svn/head@3271 48088309-b40f-0410-b149-b14953253675

topazproject/libs/otm/src/main/java/org/topazproject/otm/Session.java
@@ -281,7 +281,7 @@ public class Session {
       o = dirtyMap.get(id);
 
     if (o == null) {
-      o = getFromStore(clazz, id, checkClass(clazz));
+      o = getFromStore(id, checkClass(clazz), null);
 
       if (o != null) {
         // Must merge here. Associations may have a back-pointer to this Id.
@@ -292,12 +292,8 @@ public class Session {
       }
     }
 
-    if (o instanceof ProxyObject) {
-      LazyLoadMethodHandler llm = proxies.get(id);
-
-      if (llm != null)
-        llm.load(); // ensure it is loaded
-    }
+    if (o instanceof ProxyObject)
+        o.equals(null); // ensure it is loaded
 
     if ((o == null) || clazz.isInstance(o))
       return clazz.cast(o);
@@ -355,8 +351,7 @@ public class Session {
     Id id = checkObject(o);
 
     if (dirtyMap.containsKey(id) || cleanMap.containsKey(id)) {
-      Class clazz = o.getClass();
-      o = getFromStore(clazz, id, checkClass(clazz));
+      o = getFromStore(id, checkClass(o.getClass()), o);
       sync(o, id, true, false, false);
     }
   }
@@ -531,7 +526,7 @@ public class Session {
 
   private void write(Id id, Object o, boolean delete) throws OtmException {
     LazyLoadMethodHandler llm           = (o instanceof ProxyObject) ? proxies.get(id) : null;
-    boolean               pristineProxy = (llm != null) ? (llm.getLoaded() == null) : false;
+    boolean               pristineProxy = (llm != null) ? llm.isLoaded() : false;
     ClassMetadata         cm            = sessionFactory.getClassMetadata(o.getClass());
     TripleStore           store         = sessionFactory.getTripleStore();
 
@@ -550,62 +545,14 @@ public class Session {
     }
   }
 
-  private Object getFromStore(Class clazz, Id id, ClassMetadata cm)
+  private Object getFromStore(Id id, ClassMetadata cm, Object instance)
                        throws OtmException {
     if (txn == null)
       throw new OtmException("No transaction active");
 
     TripleStore              store = sessionFactory.getTripleStore();
 
-    TripleStore.ResultObject ro    = store.get(cm, id.getId(), txn);
-
-    return (ro == null) ? null : instantiate(ro);
-  }
-
-  private Object instantiate(TripleStore.ResultObject ro)
-                      throws OtmException {
-    for (Map.Entry<Mapper, List<String>> e : ro.unresolvedAssocs.entrySet()) {
-      List   assocs = new ArrayList();
-      Mapper p      = e.getKey();
-
-      for (String val : e.getValue()) {
-        // lazy load
-        Class       clazz = p.getComponentType();
-        Set<String> types = ro.types.get(val);
-
-        if (types != null)
-          clazz = sessionFactory.mostSpecificSubClass(clazz, types);
-
-        if (clazz != null) {
-          Object a = load(clazz, val);
-
-          if (a != null)
-            assocs.add(a);
-        }
-      }
-
-      p.set(ro.o, assocs);
-    }
-
-    for (Map.Entry<Mapper, List<TripleStore.ResultObject>> e : ro.resolvedAssocs.entrySet()) {
-      List   assocs = new ArrayList();
-      Mapper p      = e.getKey();
-
-      for (TripleStore.ResultObject val : e.getValue()) {
-        Object io = instantiate(val);
-        Object a  = sync(io, new Id(io.getClass(), val.id), true, false, false);
-
-        if (a != null)
-          assocs.add(a);
-      }
-
-      p.set(ro.o, assocs);
-    }
-
-    if (log.isDebugEnabled())
-      log.debug("Instantiated " + ro.id);
-
-    return ro.o;
+    return store.get(cm, id.getId(), instance, txn);
   }
 
   private Object sync(final Object other, final Id id, final boolean merge, final boolean update,
@@ -683,14 +630,6 @@ public class Session {
     if (!cm.getSourceClass().isAssignableFrom(ocm.getSourceClass()))
       throw new OtmException(cm.toString() + " is not assignable from " + ocm);
 
-    LazyLoadMethodHandler llm = proxies.get(checkObject(o));
-
-    if (llm != null) {
-      llm.setLoaded(other);
-
-      return o;
-    }
-
     if (log.isDebugEnabled())
       log.debug("Copy merging " + checkObject(o));
 
@@ -734,39 +673,34 @@ public class Session {
                           throws OtmException {
     LazyLoadMethodHandler mi =
       new LazyLoadMethodHandler() {
-        private Object loaded = null;
+        private boolean loaded = false;
+        private boolean loading = false;
 
         public Object invoke(Object self, Method m, Method proceed, Object[] args)
                       throws Throwable {
-          if (loaded == null) {
+          if (loaded)
+            return proceed.invoke(self, args);
+
+          if (!loading) {
             if (log.isDebugEnabled())
               log.debug(m.getName() + " on " + id + " is forcing a load from store");
 
-            loaded = getFromStore(clazz, id, cm);
+            loading = true;
+            try {
+              getFromStore(id, cm, self);
+            } finally {
+              loading = false;
+            }
+            loaded = true;
           }
 
-          return m.invoke(loaded, args);
+          return proceed.invoke(self, args);
         }
 
-        public Object getLoaded() {
+        public boolean isLoaded() {
           return loaded;
         }
 
-        public void setLoaded(Object o) {
-          if ((loaded != o) && log.isDebugEnabled())
-            log.debug("Replacing the loaded object for proxy " + id);
-
-          loaded = o;
-        }
-
-        public void load() throws OtmException {
-          if (loaded == null) {
-            if (log.isDebugEnabled())
-              log.debug(id + " is force loaded from store");
-
-            loaded = getFromStore(clazz, id, cm);
-          }
-        }
       };
 
     try {
@@ -894,10 +828,6 @@ public class Session {
   }
 
   private interface LazyLoadMethodHandler extends MethodHandler {
-    public Object getLoaded();
-
-    public void setLoaded(Object o);
-
-    public void load() throws OtmException;
+    public boolean isLoaded();
   }
 }

topazproject/libs/otm/src/main/java/org/topazproject/otm/TripleStore.java
@@ -24,7 +24,7 @@ import org.topazproject.otm.query.Results;
  * An abstraction to represent triple stores.
  *
  * @author Pradeep Krishnan
-  */
+ */
 public interface TripleStore {
   /**
    * Opens a connection to the store.
@@ -74,13 +74,14 @@ public interface TripleStore {
    *
    * @param cm the class metadata for the object
    * @param id the id/subject-uri for the object
+   * @param instance the instance to be refreshed or null 
    * @param txn the transaction context
    *
-   * @return the result that may contain a partially created object
+   * @return the result instance
    *
    * @throws OtmException on an error
    */
-  public ResultObject get(ClassMetadata cm, String id, Transaction txn)
+  public Object get(ClassMetadata cm, String id, Object instance, Transaction txn)
                    throws OtmException;
 
   /**
@@ -158,18 +159,4 @@ public interface TripleStore {
    */
   public void setCriterionBuilder(String func, CriterionBuilder builder)
                            throws OtmException;
-
-  public static class ResultObject {
-    public Object                          o;
-    public String                          id;
-    public Map<Mapper, List<String>>       unresolvedAssocs = new HashMap<Mapper, List<String>>();
-    public Map<Mapper, List<ResultObject>> resolvedAssocs   =
-      new HashMap<Mapper, List<ResultObject>>();
-    public Map<String, Set<String>> types;
-
-    public ResultObject(Object o, String id) {
-      this.o    = o;
-      this.id   = id;
-    }
-  }
 }

topazproject/libs/otm/src/main/java/org/topazproject/otm/stores/AbstractTripleStore.java
@@ -1,0 +1,147 @@
+/* $HeadURL::                                                                            $
+ * $Id$
+ *
+ * Copyright (c) 2007 by Topaz, Inc.
+ * http://topazproject.org
+ *
+ * Licensed under the Educational Community License version 1.0
+ * http://opensource.org/licenses/ecl1.php
+ */
+package org.topazproject.otm.stores;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.topazproject.otm.ClassMetadata;
+import org.topazproject.otm.OtmException;
+import org.topazproject.otm.Session;
+import org.topazproject.otm.SessionFactory;
+import org.topazproject.otm.TripleStore;
+import org.topazproject.otm.mapping.Mapper;
+import org.topazproject.otm.query.GenericQueryImpl;
+
+/**
+ * A common base class for triple-store impls.
+ *
+ * @author Pradeep Krishnan
+ */
+public abstract class AbstractTripleStore implements TripleStore {
+  /**
+   * Instantiate an object based on the statements about it found in the triple-store
+   *
+   * @param session the session under which this object is instantiated
+   * @param instance the instance to which values are to be set or null
+   * @param cm the class metadata describing
+   * @param id the object's id/subject-uri
+   * @param fvalues p-o mapping with s being the id
+   * @param rvalues p-s mapping with o being the id
+   * @param types rdf:type look-ahead for o values (crosses rdf:List, rdf:Bag)
+   *
+   * @return the object instance
+   *
+   * @throws OtmException on an error
+   */
+  protected Object instantiate(Session session, Object instance, ClassMetadata cm, String id,
+                               Map<String, List<String>> fvalues,
+                               Map<String, List<String>> rvalues, Map<String, Set<String>> types)
+                        throws OtmException {
+    SessionFactory sf    = session.getSessionFactory();
+    Class          clazz = cm.getSourceClass();
+
+    try {
+      if (instance == null)
+        instance = clazz.newInstance();
+    } catch (Exception e) {
+      throw new OtmException("Failed to instantiate " + clazz, e);
+    }
+
+    cm.getIdField().set(instance, Collections.singletonList(id));
+
+    // re-map values based on the rdf:type look ahead
+    Map<Mapper, List<String>> mvalues = new HashMap();
+    boolean                   inverse = false;
+
+    for (Map<String, List<String>> values : new Map[] { fvalues, rvalues }) {
+      for (String p : values.keySet()) {
+        for (String o : values.get(p)) {
+          Mapper m = cm.getMapperByUri(sf, p, inverse, types.get(o));
+
+          if (m != null) {
+            List<String> v = mvalues.get(m);
+
+            if (v == null)
+              mvalues.put(m, v = new ArrayList<String>());
+
+            v.add(o);
+          }
+        }
+      }
+
+      inverse = true;
+    }
+
+    // now assign values to fields
+    for (Mapper m : mvalues.keySet()) {
+      if (m.getSerializer() != null)
+        m.set(instance, mvalues.get(m));
+      else {
+        List assocs = new ArrayList();
+
+        for (String val : mvalues.get(m)) {
+          // lazy load
+          clazz = m.getComponentType();
+
+          Set<String> t = types.get(val);
+
+          if (t != null)
+            clazz = sf.mostSpecificSubClass(clazz, t);
+
+          if (clazz != null) {
+            Object a = session.load(clazz, val);
+
+            if (a != null)
+              assocs.add(a);
+          }
+        }
+
+        m.set(instance, assocs);
+      }
+    }
+
+    boolean found = false;
+
+    for (Mapper m : cm.getFields()) {
+      if (m.getMapperType() == Mapper.MapperType.PREDICATE_MAP) {
+        found = true;
+
+        break;
+      }
+    }
+
+    if (found) {
+      Map<String, List<String>> map = new HashMap<String, List<String>>();
+      map.putAll(fvalues);
+
+      for (Mapper m : cm.getFields()) {
+        if (m.getMapperType() == Mapper.MapperType.PREDICATE_MAP)
+          continue;
+
+        if (m.hasInverseUri())
+          continue;
+
+        map.remove(m.getUri());
+      }
+
+      for (Mapper m : cm.getFields()) {
+        if (m.getMapperType() == Mapper.MapperType.PREDICATE_MAP)
+          m.setRawValue(instance, map);
+      }
+    }
+
+    return instance;
+  }
+}

topazproject/libs/otm/src/main/java/org/topazproject/otm/stores/ItqlStore.java
@@ -54,7 +54,7 @@ import org.topazproject.otm.query.Results;
  * 
  * @author Ronald Tschalär
  */
-public class ItqlStore implements TripleStore {
+public class ItqlStore extends AbstractTripleStore {
   private static final Log log = LogFactory.getLog(ItqlStore.class);
   private static final Map<Object, List<ItqlHelper>> conCache = new HashMap();
   private        final URI serverUri;
@@ -314,7 +314,8 @@ public class ItqlStore implements TripleStore {
     }
   }
 
-  public ResultObject get(ClassMetadata cm, String id, Transaction txn) throws OtmException {
+  public Object get(ClassMetadata cm, String id, Object instance, Transaction txn) 
+             throws OtmException {
     ItqlStoreConnection isc = (ItqlStoreConnection) txn.getConnection();
     SessionFactory      sf  = txn.getSession().getSessionFactory();
 
@@ -411,70 +412,7 @@ public class ItqlStore implements TripleStore {
         fvalues.put(p, getRdfBag(id, p, mUri, txn, types));
     }
 
-    // create result
-    ResultObject ro;
-    try {
-      ro = new ResultObject(clazz.newInstance(), id);
-    } catch (Exception e) {
-      throw new OtmException("Failed to instantiate " + clazz, e);
-    }
-
-    cm.getIdField().set(ro.o, Collections.singletonList(id));
-
-    // re-map values based on the rdf:type look ahead
-    Map<Mapper, List<String>> mvalues = new HashMap();
-    boolean inverse = false;
-    for (Map<String, List<String>> values : new Map[] { fvalues, rvalues }) {
-      for (String p : values.keySet()) {
-        for (String o : values.get(p)) {
-          Mapper m = cm.getMapperByUri(sf, p, inverse, types.get(o));
-          if (m != null) {
-            List<String> v = mvalues.get(m);
-            if (v == null)
-              mvalues.put(m, v = new ArrayList<String>());
-            v.add(o);
-          }
-        }
-      }
-      inverse = true;
-    }
-
-    // now assign values to fields
-    for (Mapper m : mvalues.keySet()) {
-      if (m.getSerializer() != null)
-        m.set(ro.o, mvalues.get(m));
-      else
-        ro.unresolvedAssocs.put(m, mvalues.get(m));
-    }
-
-    ro.types = types;
-
-    boolean found = false;
-    for (Mapper m : cm.getFields()) {
-      if (m.getMapperType() == Mapper.MapperType.PREDICATE_MAP) {
-        found = true;
-        break;
-      }
-    }
-
-    if (found) {
-      Map<String, List<String>> map = new HashMap<String, List<String>>();
-      map.putAll(fvalues);
-      for (Mapper m : cm.getFields()) {
-        if (m.getMapperType() == Mapper.MapperType.PREDICATE_MAP)
-          continue;
-        if (m.hasInverseUri())
-          continue;
-        map.remove(m.getUri());
-      }
-      for (Mapper m : cm.getFields()) {
-        if (m.getMapperType() == Mapper.MapperType.PREDICATE_MAP)
-          m.setRawValue(ro.o, map);
-      }
-    }
-
-    // done
-    return ro;
+    return instantiate(txn.getSession(), instance, cm, id, fvalues, rvalues, types);
   }
 
   private void buildGetSelect(StringBuilder qry, ClassMetadata cm, String id, Transaction txn)

topazproject/libs/otm/src/main/java/org/topazproject/otm/stores/MemStore.java
@@ -45,7 +45,7 @@ import org.topazproject.otm.query.Results;
  *
  * @author Pradeep Krishnan
  */
-public class MemStore implements TripleStore {
+public class MemStore extends AbstractTripleStore {
   private Storage storage = new Storage();
 
   /*
@@ -99,7 +99,7 @@ public class MemStore implements TripleStore {
   /*
    * inherited javadoc
    */
-  public ResultObject get(ClassMetadata cm, String id, Transaction txn)
+  public Object get(ClassMetadata cm, String id, Object instance, Transaction txn)
                    throws OtmException {
     MemStoreConnection        msc     = (MemStoreConnection) txn.getConnection();
     Storage                   storage = msc.getStorage();
@@ -127,7 +127,8 @@ public class MemStore implements TripleStore {
       }
     }
 
-    return instantiate(txn.getSession().getSessionFactory(), cm.getSourceClass(), id, value, rvalue);
+    return instantiate(txn.getSession(), instance, cm, id, value, rvalue, 
+        new HashMap<String, Set<String>>());
   }
 
   /*
@@ -263,53 +264,6 @@ public class MemStore implements TripleStore {
     return ids;
   }
 
-  /*
-     public T <Collection<T>> find(Class<T> clazz, List<Criteria> criteria,
-         List<Field> orderBy, long offset, long size);
-   */
-  private ResultObject instantiate(SessionFactory sessionFactory, Class clazz, String id,
-                                   Map<String, List<String>> props, Map<String, List<String>> rprops)
-                            throws OtmException {
-    List<String> types = props.get(Rdf.rdf + "type");
-
-    clazz = sessionFactory.mostSpecificSubClass(clazz, types);
-
-    ClassMetadata cm = sessionFactory.getClassMetadata(clazz);
-
-    if ((types.size() == 0) && (cm.getType() != null))
-      return null;
-
-    ResultObject ro;
-
-    try {
-      ro = new ResultObject(clazz.newInstance(), id);
-    } catch (Exception e) {
-      throw new OtmException("instantiation failed", e);
-    }
-
-    cm.getIdField().set(ro.o, Collections.singletonList(id));
-
-    // A field could be of rdf:type. So remove the values used in class identification
-    // from the set of rdf:type values before running through and setting field values. 
-    types.removeAll(cm.getTypes());
-
-    int count = types.size();
-
-    for (Mapper p : cm.getFields()) {
-      List<String> vals = p.hasInverseUri() ? rprops.get(p.getUri()) : props.get(p.getUri());
-      count += vals.size();
-
-      if (p.getSerializer() != null)
-        p.set(ro.o, vals);
-      else
-        ro.unresolvedAssocs.put(p, vals);
-    }
-
-    // Put back what we removed
-    types.addAll(cm.getTypes());
-
-    return (count > 0) ? ro : null;
-  }
 
   private static class PropertyId {
     private String model;

commit df550f31fff263ce9deca65b904d84467949fa9d

Fix a bug introduced in r3271. A test was reversed inadvertently.
Added a unit-test to ensure that updates to lazy loaded objects 
are persisted.



git-svn-id: http://svn.topazproject.org/svn/head@3316 48088309-b40f-0410-b149-b14953253675

topazproject/libs/otm/src/main/java/org/topazproject/otm/Session.java
@@ -527,7 +527,7 @@ public class Session {
 
   private void write(Id id, Object o, boolean delete) throws OtmException {
     LazyLoadMethodHandler llm           = (o instanceof ProxyObject) ? proxies.get(id) : null;
-    boolean               pristineProxy = (llm != null) ? llm.isLoaded() : false;
+    boolean               pristineProxy = (llm != null) ? !llm.isLoaded() : false;
     ClassMetadata         cm            = sessionFactory.getClassMetadata(o.getClass());
     TripleStore           store         = sessionFactory.getTripleStore();
 

topazproject/libs/otm/src/it/java/org/topazproject/otm/OtmTest.java
@@ -229,19 +229,47 @@ public class OtmTest extends TestCase {
     try {
       tx = session.beginTransaction();
 
-      Annotation a = session.get(Annotation.class, "http://localhost/annotation/1");
+      Annotation a = session.load(PublicAnnotation.class, "http://localhost/annotation/1");
 
       assertNotNull(a);
+      assertEquals(42, a.getState());
+      a.setState(0);
       assertNotNull(a.foobar);
 
       assertTrue(a instanceof PublicAnnotation);
 
-      assertEquals(42, a.getState());
       assertEquals("Pradeep", a.getCreator());
       assertEquals(Annotation.NS + "Comment", a.getType());
       assertEquals("FOO", a.foobar.foo);
       assertEquals("BAR", a.foobar.bar);
 
+      session.saveOrUpdate(a);
+
+      tx.commit();
+    } catch (OtmException e) {
+      try {
+        if (tx != null)
+          tx.rollback();
+      } catch (OtmException re) {
+        log.warn("rollback failed", re);
+      }
+
+      throw e; // or display error message
+    } finally {
+      try {
+        session.close();
+      } catch (OtmException ce) {
+        log.warn("close failed", ce);
+      }
+    }
+
+    session   = factory.openSession();
+    tx        = null;
+
+    try {
+      tx = session.beginTransaction();
+      Annotation a = session.load(PublicAnnotation.class, "http://localhost/annotation/1");
+      assertEquals(0, a.getState());
       session.delete(a);
 
       a = session.get(Annotation.class, "http://localhost/annotation/1");