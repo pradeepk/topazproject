6ce69fe872e522dfac4bb10aa1babb7630b709f7
pradeep <pradeep@48088309-b40f-0410-b149-b14953253675>
2007-09-07 23:14:58 +0000

    Improving the robustness a bit:   * ask ant to report errors on exec   * 'stop-everything' done only the first time and rest is based on the state   * change the start-up sync a bit to ensure plosone waits for mulgara
    Also added some documentation.
    
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@3648 48088309-b40f-0410-b149-b14953253675

--- plosone/it/src/main/groovy/org/plosone/it/Env.groovy
+++ plosone/it/src/main/groovy/org/plosone/it/Env.groovy
@@ -11,18 +11,37 @@
 package org.plosone.it;
 import org.apache.tools.ant.taskdefs.Antlib;
 
+/**
+ * An integration test environment for PlosOne. The environment runs
+ * its own copy of mulgara and fedora. The DummySSO Filter is enabled
+ * instead of using CAS. The mulgara and fedora stores can be pre-populated
+ * with canned data. Same with lucene and ingestion-queue etc. In addition
+ * tests can reset the data used to the original state any time by calling 
+ * the restore() function.
+ * <p>
+ * Multiple environments can be installed - however only one environment can 
+ * be running at a time. So running tests in parallel is not an option.
+ * <p>
+ * The commands here launches mvn to run the ant-tasks-plugin for tasks like 
+ * fedora-install, mulgara-install etc. Make sure this plugin is installed
+ * in the local maven repository and the 'mvn' executable is in your PATH.
+ * 
+ * @author Pradeep Krishnan
+ */
 public class Env {
   private AntBuilder ant = new AntBuilder();
   private String install;
   private String data;
   private String opts;
   private static Env active = null;
-  private String mvnExec = null;
   private String fedoraHome;
 
   public static boolean stopOnExit = true;
+  private static boolean stopOnStart = true;
+  private static String ext; 
 
   static {
+    ext = (System.properties.'os.name'.toLowerCase().indexOf('windows') > -1) ? '.bat' : '';
     def shutdownHook = new Thread( {
         if ((active != null) && stopOnExit)
           active.stop();
@@ -52,59 +71,74 @@ public class Env {
   }
 
   /**
-   * Stop all services and clean the install directory
+   * Stop all services and clean the install directory.
    */
   public void clean() {
+    ant.echo 'Cleaning ...' 
     stop();
     ant.delete(dir:install)
+    ant.echo 'Finished cleaning'
   }
 
   /**
-   * Stop all services
+   * Stop all services.
    */ 
   public void stop() {
-    for (task in ['plosone-stop', 'mulgara-stop', 'fedora-stop'])
-      try { antTask(task) } catch (Throwable t) { } 
+    if ((active == null) && (stopOnStart == false))
+      return
+    Env env = (active == null) ? this : active
+
+    ant.echo 'Stopping all services on ...'
+    for (task in ['plosone-stop', 'fedora-stop', 'mulgara-stop'])
+      try { env.antTask(task) } catch (Throwable t) { } 
     active = null
+    stopOnStart = false
+    ant.echo 'Stopped all services'
   }
 
   /**
    * Install all services. A marker file is placed after the install
    * so that subsequent calls to install can bypass the actual install.
-   * 
    */
   public void install() {
     File f = new File(path(install, '/installed'));
     if (f.exists())
       return;
 
-    clean()
+    ant.echo 'Installing ...'
+
+    clean()  // clean any previous partial installs
 
     for (task in ['ecqs-install', 'fedora-install', 'mulgara-install', 'plosone-install'])
       antTask(task)
-   
-    load()
 
+    load()  // load data
+
+    // install the log4j files
+    resource('/plosoneLog4j.xml')
+    resource('/mulgaraLog4j.xml')
+
+    // finally create the marker file
     ant.touch(file: path(install, '/installed'))
+    ant.echo 'Finished installation'
   }
 
   /**
    * Restore the data to the same state as a fresh install.
-   *
    */
   public void restore() {
-    if (active != null) 
-      active.stop()
-    else
-       stop()
+    ant.echo 'Restoring data ...'
+    stop()
     ant.delete(dir: path(install, "/data"))
     load()
+    ant.echo 'Finished restoring data'
   }
 
  /**
   * Start all services. If another environment is currently running, that is stopped first.
   */
   public void start() {
+    ant.echo 'Starting services for ...'
     if (active != null) {
       if (active.install.equals(install))
          return
@@ -115,35 +149,52 @@ public class Env {
     fedora()
     waitFor('http://localhost:9091/mulgara-service/')
     ant.echo 'Mulgara started'
-    publishingApp()
-    waitFor('http://localhost:9090/userdocs/')
+    plosone()
+    // FIXME: mvn seems to have registered an auth-handler that is sending
+    // out the repository password for topaz instead of the ones from here 
+    //waitFor('http://fedoraAdmin:fedoraAdmin@localhost:9090/fedora/')
+    waitFor('http://localhost:9090/')
     ant.echo 'Fedora started'
     waitFor('http://localhost:8080/plosone-webapp/')
-    ant.echo 'Publishing app started'
+    ant.echo 'Plosone started'
+    ant.echo 'All services are up and running'
   }
   
+  /**
+   * Execute an ant-tasks plugin task. 
+   */
   private void antTask(task) {
-    ant.exec(executable: mavenExecutable) {
+    ant.echo 'Executing ant-tasks:' + task + ' ...'
+    ant.exec(executable: 'mvn' + ext, failonerror:true) {
       arg(line: 'ant-tasks:' + task + opts)
     }
+    ant.echo 'Finished execution of ant-tasks:' + task
   }
 
+  /**
+   * Start up mulgara.
+   */
   private void mulgara() {
-    ant.echo 'Starting mulgara'
+    ant.echo 'Starting mulgara ...'
     ant.forget {
-      ant.exec(executable: mavenExecutable) {
+      exec(executable: 'mvn' + ext, failonerror:true) {
         arg(line: '-f ' + pom() + ' ant-tasks:mulgara-start -DDEST_DIR=' + install
          + ' -Dtopaz.mulgara.databaseDir=' + path(install, '/data/mulgara') 
-         + ' -Dlog4j.configuration='+ mulgaraLog4j())
+         + ' -Dlog4j.configuration='+ pathUrl(install, '/mulgaraLog4j.xml'))
       }
-      ant.echo 'Mulgara stopped'
+      echo 'Mulgara stopped'
     }
   }
 
+  /**
+   * Start up fedora. mvn ant-tasks:fedora-start hangs in windows. So inline it here.
+   */
   private void fedora() {
     ant.echo("Starting mckoi ...")
     ant.forget {
-       ant.exec(dir: fedoraHome, executable:path(fedoraHome, "/server/bin/mckoi-start") + ext()) {
+       exec(dir: fedoraHome, 
+            executable:path(fedoraHome, "/server/bin/mckoi-start") + ext,
+            failonerror:true) {
          env(key:"FEDORA_HOME", file:fedoraHome)
        }
     }
@@ -151,40 +202,58 @@ public class Env {
     ant.echo("Starting fedora ...")
     ant.delete(file: path(fedoraHome, '/server/status'))
     ant.forget {
-       ant.exec(dir: fedoraHome, executable:path(fedoraHome, "/server/bin/fedora-start") + ext()) {
+       exec(dir: fedoraHome,
+            executable:path(fedoraHome, "/server/bin/fedora-start") + ext,
+            failonerror:true) {
          arg(line: "mckoi")
          env(key:"FEDORA_HOME", file:fedoraHome)
        }
     }
   }
 
-  private void publishingApp() {
-    ant.echo 'Starting publishing app'
+  /**
+   * Start up plosone.
+   */
+  private void plosone() {
+    ant.echo 'Starting plosone ...'
     ant.forget {
-      ant.exec(executable: mavenExecutable) {
+      exec(executable: 'mvn' + ext, failonerror:true) {
         arg(line: '-f ' + pom() + ' ant-tasks:plosone-start -DDEST_DIR=' + install
          + ' -Dorg.plos.configuration.overrides=defaults-dev.xml -Dlog4j.configuration=' 
-         + publishingAppLog4j() 
+         + pathUrl(install, '/plosoneLog4j.xml') 
          + ' -Dpub.spring.ingest.source=' + path(install, '/data/ingestion-queue')
          + ' -Dpub.spring.ingest.destination=' + path(install, '/data/ingested')
          + ' -Dtopaz.search.indexpath=' + path(install, '/data/lucene')
          + ' -Dtopaz.search.defaultfields=description,title,body,creator'      
        )
       }
-      ant.echo 'Publishing app Stopped'
+      echo 'Plosone Stopped'
     }
   }
 
+  /**
+   * Wait for the service to start up. Tries for over 2 minutes before giving up.
+   */
   private String waitFor(String uri) {
     Throwable saved = null;
     for (i in 1..120) {
-      try {return uri.toURL().getText()} catch (Throwable e)  {saved = e}
+      try {return uri.toURL().text} catch (Throwable e)  {saved = e}
       ant.echo i + ' Waiting for ' + uri + ' ...'
       sleep 1000
     }
+    ant.echo 'Failed to start the service at ' + uri
     throw saved
   }
   
+  /**
+   * Locate a pom.xml file to pass to mvn.  Only for the tasks that require a pom.
+   * Looks up the pom in the current working directory or its parents. If a
+   * pom is not found there, then the install directory and its parents are looked up.
+   *
+   * Note that the expectation here is to find a pom that is the head/pom.xml or
+   * a child of the head/pom.xml where head represents the head of the plosone 
+   * project source tree.
+   */
   private String pom () {
     String p = pom(new File(System.properties.'user.dir'))
     if (p == null)
@@ -194,6 +263,13 @@ public class Env {
     return p
   }
 
+  /**
+   * Look up a pom in this directory or recursively its parents.
+   * 
+   * @param dir the directory to look for pom.xml
+   *
+   * @return the full path of the pom.xml file found or null
+   */
   private String pom(File dir) {
     if (dir == null)
       return null;
@@ -203,10 +279,27 @@ public class Env {
     return pom(dir.parentFile)
   }
 
-
+  /**
+   * Load canned data into the environment. The canned data is expected to be
+   * a mvn artifact and is expected to be a tar.gz of the expected data directory
+   * lay out. The directory layout is as follows:
+   * <code>
+   *       data
+   *       `-- README
+   *       `-- lucene
+   *       `-- ingestion-queue
+   *       `-- ingested
+   *       `-- fedora
+   *       |   |-- objects
+   *       |   |-- datastreams
+   *       `-- mulgara (mulgara data dir)
+   * </code>
+   */
   private void load() {
+    ant.echo 'Loading/Creating data for ...'
+
     if (data != null) {
-      ant.exec(executable: mavenExecutable, failonerror:true) {
+      ant.exec(executable: 'mvn' + ext, failonerror:true) {
         arg(line: '-f ' + pom() + ' ant-tasks:tgz-explode -Dlocation=' + install 
           + ' -Dtype=tgz -Ddependencies=' + data)
       }
@@ -216,6 +309,7 @@ public class Env {
     d = new File(install, 'data')
     if (!d.exists())
        d.mkdir()
+
     File m = new File(d, 'mulgara')
     if (!m.exists())
       m.mkdir()
@@ -237,14 +331,20 @@ public class Env {
       l.mkdir()
 
     fedoraRebuild();
+
+    ant.echo 'Finished loading data'
   }
 
-  // mvn ant-tasks:fedora-rebuild hangs in windows. So inline-it here.
+  /**
+   * Rebuild the fedora database. mvn ant-tasks:fedora-rebuild hangs in windows. So inline-it here.
+   */
   private void fedoraRebuild() {
     ant.echo("Rebuilding fedora data ...")
     ant.echo("Starting mckoi ...")
     ant.forget {
-       ant.exec(dir: fedoraHome, executable:path(fedoraHome, "/server/bin/mckoi-start") + ext()) {
+       exec(dir: fedoraHome, 
+            executable:path(fedoraHome, "/server/bin/mckoi-start") + ext,
+            failonerror:true) {
          env(key:"FEDORA_HOME", file:fedoraHome)
        }
     }
@@ -256,30 +356,31 @@ public class Env {
       fileset(dir:path(install, "/data/fedora"))
     }
     ant.echo("Invoking fedora-rebuild ...")
-    ant.exec(dir: fedoraHome, executable:path(fedoraHome, "/server/bin/fedora-rebuild") + ext(),
+    ant.exec(dir: fedoraHome, 
+             executable:path(fedoraHome, "/server/bin/fedora-rebuild") + ext,
+             failonerror:true,
              input: resource('/fedora-rebuild-input')) {
       arg(line:"mckoi")
       env(key:"FEDORA_HOME", file:fedoraHome)
     }
     ant.echo("Stopping mckoi ...")
-    ant.exec(dir: fedoraHome, executable:path(fedoraHome, "/server/bin/mckoi-stop") + ext()) {
+    ant.exec(dir: fedoraHome,
+            executable:path(fedoraHome, "/server/bin/mckoi-stop") + ext,
+            failonerror:true) {
       arg(line:"fedoraAdmin fedoraAdmin")
       env(key:"FEDORA_HOME", file:fedoraHome)
     }
+    ant.sleep(seconds:"5")
     ant.echo("Fedora rebuild completed.")
   }
 
-  
-  private String publishingAppLog4j() {
-    File f = new File(resource('/plosoneLog4j.xml'))
-    return f.toURL().toString()
-  }
-  
-  private String mulgaraLog4j() {
-     File f = new File(resource('/mulgaraLog4j.xml'))
-     return f.toURL().toString()
-  }
-
+  /**
+   * Copy a resource from class-path and return the file-name.
+   *
+   * @param name the resource to find in class-path
+   * 
+   * @return the filename where the resource was copied into.
+   */
   public String resource(String name) {
      String input = path(install, name)
      def out = new BufferedOutputStream(new FileOutputStream(input))
@@ -288,6 +389,11 @@ public class Env {
      return input
   }
 
+  /**
+   * Execute a groovy shell script with arguments.
+   *
+   * @param args command line where first argument is the script-file to execute
+   */
   public void script(String[] args) {
     String res = args[0]
     String[] sargs = new String[args.length - 1]
@@ -295,26 +401,19 @@ public class Env {
     for (x in 1 .. args.length - 1)
         sargs[x-1] = args[x]
 
-    println 'executing script ' + res + ' with args ' + sargs
+    ant.echo 'Executing script ' + res + ' with args ' + sargs
     GroovyShell shell = new GroovyShell();
     shell.run(new File(res), sargs)
   }
 
-  private String ext() {
-    return (System.properties.'os.name'.toLowerCase().indexOf('windows') > -1) ? '.bat' : '';
-  }
-
-  private String getMavenExecutable() {
-    if (mvnExec == null) {
-      if (System.properties.'os.name'.toLowerCase().indexOf('windows') > -1) {
-        mvnExec = 'mvn.bat'
-      } else {
-        mvnExec = 'mvn'
-      }
-    }
-    return mvnExec
-  }
-
+  /**
+   * Convert to a platform specific file path.
+   *
+   * @param dir a directory or root path
+   * @param file a filename or a relative path
+   *
+   * @return platform specific file path
+   */
   private String path(String dir, String file) {
     String sep = System.properties.'file.separator'
         
@@ -330,4 +429,16 @@ public class Env {
     return file
   }
 
+  /**
+   * Convert a path to a file: URL.
+   *
+   * @param dir a directory or root path
+   * @param file a filename or a relative path
+   *
+   * @return the file: URL for the path
+   */
+  private String pathUrl(String dir, String file) {
+    File f = new File(path(dir, file))
+    return f.toURL().toString()
+  }
 }
