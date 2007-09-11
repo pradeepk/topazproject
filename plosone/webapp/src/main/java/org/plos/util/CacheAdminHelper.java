fbe69c0736368e5a183c25770f716ad99446314d
ronald <ronald@48088309-b40f-0410-b149-b14953253675>
2007-09-11 14:36:40 +0000

    Added helper for Ehcache.
    git-svn-id: http://svn.topazproject.org/svn/head@3668 48088309-b40f-0410-b149-b14953253675

--- plosone/webapp/src/main/java/org/plos/util/CacheAdminHelper.java
+++ plosone/webapp/src/main/java/org/plos/util/CacheAdminHelper.java
@@ -14,6 +14,9 @@ import com.opensymphony.oscache.base.CacheEntry;
 import com.opensymphony.oscache.base.NeedsRefreshException;
 import com.opensymphony.oscache.general.GeneralCacheAdministrator;
 
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -98,18 +101,63 @@ public class CacheAdminHelper {
     return res;
   }
 
+  /** 
+   * Look up a value in the cache, updating the cache with a new value if not found or if the old
+   * value expired.
+   * 
+   * @param cache   the cache to look up the value in
+   * @param key     the key to use for the lookup
+   * @param refresh the max-age of entries in the cache (in seconds), or -1 for indefinite
+   * @param lock    the object to synchronized on
+   * @param desc    a short description of the object being retrieved (for logging only)
+   * @param updater the updater to call to get the value if not found in the cache; may be null
+   * @return the value in the cache, or returned by the <var>updater</var> if not in the cache
+   */
+  public static <T> T getFromCache(Ehcache cache, String key, int refresh, Object lock,
+                                   String desc, EhcacheUpdater<T> updater) {
+    synchronized (lock) {
+      Element e = cache.get(key);
+
+      if (e == null) {
+        if (updater == null)
+          return null;
+
+        if (log.isDebugEnabled())
+          log.debug("retrieving " + desc + " from db");
+
+        e = new Element(key, updater.lookup());
+        if (refresh > 0)
+          e.setTimeToLive(refresh);
+
+        cache.put(e);
+      } else if (log.isDebugEnabled()) {
+        log.debug("retrieved " + desc + " from cache");
+      }
+
+      return (T) e.getValue();
+    }
+  }
+
   /**
    * The interface updaters must implement.
    */
   public static interface CacheUpdater<T> {
     /** 
-     * This is run within the context of a transaction.
-     * 
      * @param updated (output) set <var>updated[0]</var> to true if the lookup/retrieval was
      *                successful
      * @return the value to return; if <var>updated[0]</var> was set to true then this value will
      *         be put into the cache
      */
     T lookup(boolean[] updated);
+  }
+
+  /**
+   * The interface Ehcache updaters must implement.
+   */
+  public static interface EhcacheUpdater<T> {
+    /** 
+     * @return the value to return; this value will be put into the cache
+     */
+    T lookup();
   }
 }
