pradeepk/topazproject

commit f481ba79cec2a7be440c04aad252e8c062b702dc

Added system property switches:
-DskipStartEnv (default = false): Will skip starting the mulgara / fedora / plosone services on startup of NavTest. 

-DstopOnExit (default = true): Will leave services up and running at end of test if set to false

-DstopOnStart (default = true): Will skip services shut-down on start startup. 

This will allow you to start up an expected environment for a test and leave the services up and running. Then you can browse the local server, write / edit tests, and execute the tests again without having to restart all the services.

git-svn-id: http://svn.topazproject.org/svn/head@3683 48088309-b40f-0410-b149-b14953253675

plosone/it/src/main/groovy/org/plosone/it/Env.groovy
@@ -44,6 +44,12 @@ public class Env {
 
   static {
     ext = (System.properties.'os.name'.toLowerCase().indexOf('windows') > -1) ? '.bat' : '';
+    if (System.properties.containsKey('stopOnExit')) {
+      stopOnExit = Boolean.valueOf(System.properties.getProperty("stopOnExit")).booleanValue();
+    }
+    if (System.properties.containsKey('stopOnStart')) {
+      stopOnStart = Boolean.valueOf(System.properties.getProperty('stopOnStart')).booleanValue();
+    }
     def shutdownHook = new Thread( {
         if ((active != null) && stopOnExit)
           active.stop();
@@ -86,8 +92,10 @@ public class Env {
    * Stop all services.
    */ 
   public void stop() {
-    if ((active == null) && (stopOnStart == false))
+    if ( ((active == null) && (stopOnStart == false)) || (stopOnExit == false)) {
+      ant.exho 'skipping stop() of services.'
       return
+    }
     Env env = (active == null) ? this : active
 
     ant.echo 'Stopping all services on ...'

plosone/it/pom.xml
@@ -110,13 +110,29 @@
     <profile>
       <id>it-helper</id>
       <build>
-        <plugins>
-          <plugin>
-            <groupId>org.apache.maven.plugins</groupId>
-            <artifactId>maven-surefire-plugin</artifactId>
-            <version>2.3</version>
-          </plugin>
-        </plugins>
+      	<plugins>
+      		<plugin>
+      			<groupId>org.apache.maven.plugins</groupId>
+      			<artifactId>maven-surefire-plugin</artifactId>
+      			<version>2.3</version>
+      			<configuration>
+      				<systemProperties>
+      					<property>
+      						<name>skipStartEnv</name>
+      						<value>${skipStartEnv}</value>
+      					</property>
+      					<property>
+      						<name>stopOnExit</name>
+      						<value>${stopOnExit}</value>
+      					</property>
+      					<property>
+      						<name>stopOnStart</name>
+      						<value>${stopOnStart}</value>
+      					</property>
+      				</systemProperties>
+      			</configuration>
+      		</plugin>
+      	</plugins>
       </build>
     </profile>
   </profiles>

plosone/it/src/it/java/org/plosone/it/BootStrapTest.java
@@ -38,26 +38,44 @@ public class BootStrapTest extends AbstractPlosOneTest {
    */
   @BeforeClass
   public void setUp() {
+	log.info("BootStrapTest.setUp() --> installEnvs()");
     installEnvs();
+	log.info("BootStrapTest.setUp() --> initTesters()");
     initTesters();
+    log.info("BootStrapTest.setUp() --> restoring environment...");
     getEmptyEnv().restore();
+    try {
+    	log.info("BootStrapTest.setUp() --> sleeping...");
+		Thread.sleep(3000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	log.info("BootStrapTest.setUp() --> starting env services...");
     getEmptyEnv().start();
   }
 
   /**
-   * DOCUMENT ME!
-   */
-  @Test
-  public void testJournalInstall() {
-    Env env = getEmptyEnv();
-    String script = env.resource("/createjournal.groovy");
-    String plosone = env.resource("/journal-plosone.xml");
-    String ct = env.resource("/journal-clinicaltrials.xml");
-    env.script(new String[]{script, "-j", plosone});
-    env.script(new String[]{script, "-j", ct});
-    env.stop();
-    env.start();
-  }
+	 * DOCUMENT ME!
+	 */
+	@Test
+	public void testJournalInstall() {
+		log.info("\n\n\n\nStarting Test: testJournalInstall...");
+		Env env = getEmptyEnv();
+		String script = env.resource("/createjournal.groovy");
+		String plosone = env.resource("/journal-plosone.xml");
+		String ct = env.resource("/journal-clinicaltrials.xml");
+		env.script(new String[] { script, "-j", plosone });
+		env.script(new String[] { script, "-j", ct });
+		env.stop();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		env.start();
+	}
 
   @Test(dependsOnMethods={"testJournalInstall"})
   public void testPlosOneHomePage() {

plosone/it/src/it/java/org/plosone/it/NavTest.java
@@ -13,6 +13,7 @@ package org.plosone.it;
 import java.lang.reflect.Method;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +22,7 @@ import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.plosone.it.pages.AbstractPage;
 import org.plosone.it.pages.HomePage;
 import org.plosone.it.pages.ArticlePage;
 import org.plosone.it.pages.SearchResultsPage;
@@ -49,7 +51,9 @@ public class NavTest extends AbstractPlosOneTest {
   public void setUp() {
     installEnvs();
     initTesters();
-    getBasicEnv().start();
+    if (!("true".equalsIgnoreCase(System.getProperty("skipStartEnv")))) {
+    	getBasicEnv().start();
+    }
     setUpArticles();
   }
 
@@ -182,5 +186,104 @@ public class NavTest extends AbstractPlosOneTest {
     ap.verifyPage();
     ap.logOut();
     ap.verifyPage();
+  }
+
+  /**
+   * testBrowseByDate verifies that browse by date is working for all supported browsers. 
+   * The browse by date page is navigated to via the drop down menu on the home page. 
+   * The article retrieved is common to both PLos and CT Journals. 
+   */
+  @Test
+  public void testBrowseByDate() {
+  	for (String browser : browsers.keySet()) {
+	  	log.info("Testing Browse Atricles by Date [browser="+browser);
+	  	PlosOneWebTester tester = getTester(AbstractPage.J_PONE, browser);
+	  	HomePage hp = new HomePage(tester, AbstractPage.J_PONE);
+	  	hp.gotoPage();
+	  	hp.getTester().clickLinkWithExactText("By Publication Date");
+	  	// Click on the Dec -> 20 link
+	  	hp.getTester().clickLinkWithExactText("20");
+	  	hp.getTester().assertTextPresent("Isolation of Non-Tuberculous Mycobacteria in Children Investigated for Pulmonary Tuberculosis");
+  	}
+  }
+  
+  /**
+   * testBrowseBySubject verifies that browse by subject is working for all supported browsers. 
+   * The browse by subject page is navigated to via the drop down menu on the home page. 
+   * The article retrieved is common to both PLos and CT Journals. 
+   */
+  @Test
+  public void testBrowseBySubject() {
+  	for (String browser : browsers.keySet()) {
+	  	log.info("Testing Browse Atricles by Subject [browser="+browser);
+	  	PlosOneWebTester tester = getTester(AbstractPage.J_PONE, browser);
+	  	HomePage hp = new HomePage(tester, AbstractPage.J_PONE);
+	  	hp.gotoPage();
+	  	hp.getTester().clickLinkWithExactText("By Subject");
+	  	hp.getTester().clickLinkWithExactText("Infectious Diseases (1)");
+	  	hp.getTester().assertTextPresent("Isolation of Non-Tuberculous Mycobacteria in Children Investigated for Pulmonary Tuberculosis");
+  	}
+  }
+  
+  /**
+   * testBrowseByDateJournalFilter verifies that articles in the CT Journal are not
+   * retrieved using browse by date within the PLoS Journal. 
+   */
+  @Test
+  public void testBrowseByDateJournalFilterPLoS() {
+  	String browser = browsers.keySet().iterator().next(); // just grab the first browser
+  	log.info("Testing that CT-only articles are not presented when using browse by date in PLoS Journal [browser="+browser+"]");
+  	PlosOneWebTester tester = getTester(AbstractPage.J_PONE, browser);
+  	HomePage hp = new HomePage(tester, AbstractPage.J_PONE);
+  	hp.gotoPage();
+  	hp.getTester().clickLinkWithExactText("By Publication Date");
+  	hp.getTester().assertLinkNotPresentWithExactText("15"); // The date for Jun 15th 2007
+  }
+  
+
+  /**
+   * testBrowseByDateJournalFilter verifies that articles in the CT Journal are not
+   * retrieved using browse by date within the PLoS Journal. 
+   */
+  @Test
+  public void testBrowseByDateJournalFilterCT() {
+  	String browser = browsers.keySet().iterator().next(); // just grab the first browser
+  	log.info("Testing that PLoS-only articles are not presented when using browse by date in CT Journal [browser="+browser+"]");
+  	PlosOneWebTester tester = getTester(AbstractPage.J_CT, browser);
+  	HomePage hp = new HomePage(tester, AbstractPage.J_CT);
+  	hp.gotoPage();
+  	hp.getTester().clickLinkWithExactText("By Publication Date");
+  	hp.getTester().assertLinkNotPresentWithExactText("17"); // The date for Jan 17th 2007
+  }
+  
+  /**
+   * testBrowseByDateJournalFilter verifies that articles in the CT Journal are not
+   * retrieved using browse by subject within the PLoS Journal. 
+   */
+  @Test
+  public void testBrowseBySubjectJournalFilterPLoS() {
+  	String browser = browsers.keySet().iterator().next(); // just grab the first browser
+  	log.info("Testing that CT-only articles are not presented when browse by subject in PLoS Journal [browser="+browser+"]");
+  	PlosOneWebTester tester = getTester(AbstractPage.J_PONE, browser);
+  	HomePage hp = new HomePage(tester, AbstractPage.J_PONE);
+  	hp.gotoPage();
+  	hp.getTester().clickLinkWithExactText("By Subject");
+  	hp.getTester().assertLinkNotPresentWithExactText("Women's Health (1)"); // The date for Jun 15th 2007
+  }
+  
+  /**
+   * testBrowseByDateJournalFilter verifies that articles in the PLoS Journal are not
+   * retrieved using browse by subject within the CT Journal. 
+   */
+  @Test
+  public void testBrowseBySubjectJournalFilterCT() {
+  	String browser = browsers.keySet().iterator().next(); // just grab the first browser
+  	log.info("Testing that PLoS-only articles are not presented when browse by subject in CT Journal [browser="+browser+"]");
+  	PlosOneWebTester testerCt = getTester(AbstractPage.J_CT, browser);
+  	HomePage hpCt = new HomePage(testerCt, AbstractPage.J_CT);
+  	hpCt.gotoPage();
+  	hpCt.getTester().clickLinkWithExactText("By Subject");
+  	hpCt.getTester().assertLinkNotPresentWithExactText("Evolutionary Biology (1)"); // The date for Jun 15th 2007
+  	
   }
 }

commit 8c327a6303a25d9bd68206e8dbf5df5d79a26592

stop() should not own its own decide based on 'stopOnExit'. The shutdownHook does that.
This fixes the problem when running tests using 'mvn -Pit-helper install' 
introduced in r3683.



git-svn-id: http://svn.topazproject.org/svn/head@3699 48088309-b40f-0410-b149-b14953253675

plosone/it/src/main/groovy/org/plosone/it/Env.groovy
@@ -96,7 +96,7 @@ public class Env {
    * Stop all services.
    */
   public void stop() {
-    if ( ((active == null) && (stopOnStart == false)) || (stopOnExit == false)) {
+    if ((active == null) && (stopOnStart == false)) {
       ant.echo 'skipping stop() of services.'
       return
     }