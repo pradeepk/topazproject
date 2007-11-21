pradeepk/topazproject

commit 80c29372ef494d5b6c2c109a5eea4ecf3b928d66

Session now tracks changes to an object and only selectively persists the fields that are modified.
Currently you still have to call saveOrUpdate() to mark
an object for persistence. Auto-detection is not in place yet. 



git-svn-id: http://svn.topazproject.org/svn/head@4071 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/java/org/topazproject/otm/stores/ItqlStore.java
@@ -83,8 +83,9 @@ public class ItqlStore extends AbstractTripleStore {
     ((ItqlStoreConnection) con).close();
   }
 
-  public void insert(ClassMetadata cm, String id, Object o, Transaction txn) throws OtmException {
-    Map<String, List<Mapper>> mappersByModel = groupMappersByModel(cm);
+  public void insert(ClassMetadata cm, Collection<Mapper> fields, String id, Object o, 
+      Transaction txn) throws OtmException {
+    Map<String, List<Mapper>> mappersByModel = groupMappersByModel(cm, fields);
     StringBuilder insert = new StringBuilder(500);
 
     // for every model create an insert statement
@@ -116,13 +117,14 @@ public class ItqlStore extends AbstractTripleStore {
     }
   }
 
-  private static Map<String, List<Mapper>> groupMappersByModel(ClassMetadata cm) {
+  private static Map<String, List<Mapper>> groupMappersByModel(ClassMetadata cm, 
+      Collection<Mapper> fields) {
     Map<String, List<Mapper>> mappersByModel = new HashMap<String, List<Mapper>>();
 
     if (cm.getTypes().size() > 0)
       mappersByModel.put(cm.getModel(), new ArrayList<Mapper>());
 
-    for (Mapper p : cm.getFields()) {
+    for (Mapper p : fields) {
       String m = (p.getModel() != null) ? p.getModel() : cm.getModel();
       List<Mapper> pList = mappersByModel.get(m);
       if (pList == null)
@@ -214,8 +216,9 @@ public class ItqlStore extends AbstractTripleStore {
     }
   }
 
-  public void delete(ClassMetadata cm, String id, Transaction txn) throws OtmException {
-    Map<String, List<Mapper>> mappersByModel = groupMappersByModel(cm);
+  public void delete(ClassMetadata cm, Collection<Mapper> fields, String id, Object o, 
+      Transaction txn) throws OtmException {
+    Map<String, List<Mapper>> mappersByModel = groupMappersByModel(cm, fields);
     StringBuilder delete = new StringBuilder(500);
 
     // for every model create a delete statement

topaz/core/src/main/java/org/topazproject/otm/Session.java
@@ -13,6 +13,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -51,6 +52,7 @@ public class Session {
   private final Map<Id, Set<Wrapper>>          associations   = new HashMap<Id, Set<Wrapper>>();
   private final Set<Id>                        currentIds     = new HashSet<Id>();
   private final Map<String, Filter>            filters        = new HashMap<String, Filter>();
+  private final Map<Id, InstanceState>         states         = new HashMap<Id, InstanceState>();
 
   /**
    * Empty constructor for spring scoped proxy. The resulting session instance is not usable
@@ -158,6 +160,7 @@ public class Session {
     deleteMap.clear();
     proxies.clear();
     associations.clear();
+    states.clear();
   }
 
   /**
@@ -579,18 +582,48 @@ public class Session {
     ClassMetadata         cm            = sessionFactory.getClassMetadata(o.getClass());
     TripleStore           store         = sessionFactory.getTripleStore();
 
-    if (delete || !pristineProxy) {
+    if (delete) {
       if (log.isDebugEnabled())
         log.debug("deleting from store: " + id);
 
-      store.delete(cm, id.getId(), txn);
-    }
-
-    if (!delete && !pristineProxy) {
+     states.remove(id);
+     store.delete(cm, cm.getFields(), id.getId(), o, txn);
+    } else if (pristineProxy) {
       if (log.isDebugEnabled())
-        log.debug("inserting into store: " + id);
+        log.debug("Update skipped for " + id + ". This is a proxy object and is not even loaded.");
+    } else {
+      InstanceState state = states.get(id);
+      Collection<Mapper> fields;
+      if (state != null)
+        fields = state.update(o, cm, this);
+      else {
+        fields = cm.getFields();
+        states.put(id, new InstanceState(o, cm, this));
+      }
 
-      store.insert(cm, id.getId(), o, txn);
+      if ((fields.size() == 0) && (state != null)) {
+        if (log.isDebugEnabled())
+          log.debug("Update skipped for " + id + ". No changes to the object state.");
+      } else {
+        if (fields.size() == cm.getFields().size()) {
+          if (log.isDebugEnabled())
+            log.debug("insert/update in to store: " + id);
+        } else {
+          if (log.isDebugEnabled()) {
+            Collection<Mapper> skips = new ArrayList(cm.getFields());
+            skips.removeAll(fields);
+            StringBuilder buf = new StringBuilder(100);
+            char sep = ' ';
+            for (Mapper m : skips) {
+              buf.append(sep).append(m.getName());
+              sep = ',';
+            }
+            log.debug("Partial update for " + id + ". Skipped:" + buf);
+          }
+        }
+        store.delete(cm, fields, id.getId(), o, txn);
+        store.insert(cm, fields, id.getId(), o, txn);
+      }
     }
   }
 
@@ -606,6 +639,12 @@ public class Session {
 
     if (instance instanceof PostLoadEventListener)
       ((PostLoadEventListener)instance).onPostLoad(this, instance);
+
+    // Remember state for change tracking
+    if (instance != null)
+      states.put(id, new InstanceState(instance, cm, this));
+    else
+      states.remove(id);
 
     return instance;
   }
@@ -889,5 +928,45 @@ public class Session {
 
   private interface LazyLoadMethodHandler extends MethodHandler {
     public boolean isLoaded();
+  }
+
+  private static class InstanceState {
+    private final Map<Mapper, List<String>> vmap;
+    private Map<String, List<String>>       pmap;
+
+
+    public InstanceState(Object instance, ClassMetadata cm, Session session) {
+      vmap   = new HashMap<Mapper, List<String>>();
+      pmap   = null;
+      update(instance, cm, session);
+    }
+
+    public Collection<Mapper> update(Object instance, ClassMetadata cm, Session session) {
+      List<Mapper> mappers = new ArrayList<Mapper>();
+
+      for (Mapper m : cm.getFields()) {
+        if (m.getMapperType() == Mapper.MapperType.PREDICATE_MAP) {
+          Map<String, List<String>> nv = (Map<String, List<String>>) m.getRawValue(instance, true);
+          boolean                   eq = (pmap == null) ? (nv == null) : pmap.equals(nv);
+
+          if (!eq) {
+            pmap = nv;
+            mappers.add(m);
+          }
+        } else {
+          List<String> ov = vmap.get(m);
+          List<String> nv =
+            (m.getSerializer() != null) ? m.get(instance) : session.getIds(m.get(instance));
+          boolean      eq = (ov == null) ? (nv == null) : ov.equals(nv);
+
+          if (!eq) {
+            vmap.put(m, nv);
+            mappers.add(m);
+          }
+        }
+      }
+
+      return mappers;
+    }
   }
 }

topaz/core/src/main/java/org/topazproject/otm/TripleStore.java
@@ -13,6 +13,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.topazproject.otm.criterion.CriterionBuilder;
+import org.topazproject.otm.mapping.Mapper;
 import org.topazproject.otm.query.GenericQueryImpl;
 import org.topazproject.otm.query.Results;
 
@@ -54,16 +55,45 @@ public interface TripleStore {
               throws OtmException;
 
   /**
-   * Removes an object from the triple store.
+   * Persists parts of an object in the triple store.
    *
    * @param cm the class metadata for the object
+   * @param fields the fields that are to be selectively persisted
    * @param id the id/subject-uri for the object
+   * @param o the object
    * @param txn the transaction context
    *
    * @throws OtmException on an error
    */
-  public void delete(ClassMetadata cm, String id, Transaction txn)
+  public void insert(ClassMetadata cm, Collection<Mapper> fields, String id, Object o, Transaction txn)
               throws OtmException;
+
+  /**
+   * Removes an object from the triple store.
+   *
+   * @param cm the class metadata for the object
+   * @param id the id/subject-uri for the object
+   * @param o the object
+   * @param txn the transaction context
+   *
+   * @throws OtmException on an error
+   */
+  public void delete(ClassMetadata cm, String id, Object o, Transaction txn)
+              throws OtmException;
+
+  /**
+   * Removes parts of an object from the triple store.
+   *
+   * @param cm the class metadata for the object
+   * @param fields the fields that are to be selectively deleted
+   * @param id the id/subject-uri for the object
+   * @param o the object
+   * @param txn the transaction context
+   *
+   * @throws OtmException on an error
+   */
+  public void delete(ClassMetadata cm, Collection<Mapper> fields, String id, Object o, 
+                     Transaction txn) throws OtmException;
 
   /**
    * Gets an object from the triple store.

topaz/core/src/main/java/org/topazproject/otm/stores/AbstractTripleStore.java
@@ -20,6 +20,7 @@ import org.topazproject.otm.ClassMetadata;
 import org.topazproject.otm.OtmException;
 import org.topazproject.otm.Session;
 import org.topazproject.otm.SessionFactory;
+import org.topazproject.otm.Transaction;
 import org.topazproject.otm.TripleStore;
 import org.topazproject.otm.criterion.CriterionBuilder;
 import org.topazproject.otm.mapping.Mapper;
@@ -153,6 +154,22 @@ public abstract class AbstractTripleStore implements TripleStore {
     }
 
     return instance;
+  }
+
+  /*
+   * inherited javadoc
+   */
+  public void insert(ClassMetadata cm, String id, Object o, Transaction txn)
+              throws OtmException {
+    insert(cm, cm.getFields(), id, o, txn);
+  }
+
+  /*
+   * inherited javadoc
+   */
+  public void delete(ClassMetadata cm, String id, Object o, Transaction txn)
+              throws OtmException {
+    delete(cm, cm.getFields(), id, o, txn);
   }
 
   /*

topaz/core/src/main/java/org/topazproject/otm/stores/MemStore.java
@@ -62,14 +62,14 @@ public class MemStore extends AbstractTripleStore {
   /*
    * inherited javadoc
    */
-  public void insert(ClassMetadata cm, String id, Object o, Transaction txn)
-              throws OtmException {
+  public void insert(ClassMetadata cm, Collection<Mapper> fields, String id, Object o, 
+      Transaction txn) throws OtmException {
     MemStoreConnection msc     = (MemStoreConnection) txn.getConnection();
     Storage            storage = msc.getStorage();
 
     storage.insert(cm.getModel(), id, Rdf.rdf + "type", cm.getTypes().toArray(new String[0]));
 
-    for (Mapper p : cm.getFields()) {
+    for (Mapper p : fields) {
       if (p.hasInverseUri())
         continue;
 
@@ -84,13 +84,13 @@ public class MemStore extends AbstractTripleStore {
   /*
    * inherited javadoc
    */
-  public void delete(ClassMetadata cm, String id, Transaction txn)
-              throws OtmException {
+  public void delete(ClassMetadata cm, Collection<Mapper> fields, String id, Object o,
+      Transaction txn) throws OtmException {
     MemStoreConnection msc     = (MemStoreConnection) txn.getConnection();
     Storage            storage = msc.getStorage();
     String             model   = cm.getModel();
 
-    for (Mapper m : cm.getFields())
+    for (Mapper m : fields)
       storage.remove(model, id, m.getUri());
   }
 

commit 58935e7514d633c7becd9ea3312ddc1f54bdfabd

Fix a bug introduced in r4071 where rdf:type field values where deleted when a partial update was performed.
git-svn-id: http://svn.topazproject.org/svn/head@4095 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/java/org/topazproject/otm/stores/ItqlStore.java
@@ -218,15 +218,19 @@ public class ItqlStore extends AbstractTripleStore {
 
   public <T> void delete(ClassMetadata<T> cm, Collection<Mapper> fields, String id, T o, 
                          Transaction txn) throws OtmException {
+    final boolean partialDelete = (cm.getFields().size() != fields.size());
     Map<String, List<Mapper>> mappersByModel = groupMappersByModel(cm, fields);
     StringBuilder delete = new StringBuilder(500);
 
     // for every model create a delete statement
     for (String m : mappersByModel.keySet()) {
+      int len = delete.length();
       delete.append("delete ");
-      buildDeleteSelect(delete, getModelUri(m, txn), mappersByModel.get(m),
-                        m.equals(cm.getModel()) && cm.getTypes().size() > 0, id);
-      delete.append("from <").append(getModelUri(m, txn)).append(">;");
+      if (buildDeleteSelect(delete, getModelUri(m, txn), mappersByModel.get(m),
+                        !partialDelete && m.equals(cm.getModel()) && cm.getTypes().size() > 0, id))
+        delete.append("from <").append(getModelUri(m, txn)).append(">;");
+      else
+        delete.setLength(len);
     }
 
     if (log.isDebugEnabled())
@@ -240,10 +244,11 @@ public class ItqlStore extends AbstractTripleStore {
     }
   }
 
-  private void buildDeleteSelect(StringBuilder qry, String model, List<Mapper> mappers,
+  private boolean buildDeleteSelect(StringBuilder qry, String model, List<Mapper> mappers,
                                  boolean delTypes, String id)
       throws OtmException {
     qry.append("select $s $p $o from <").append(model).append("> where ");
+    int len = qry.length();
 
     // build forward statements
     boolean found = delTypes;
@@ -314,6 +319,8 @@ public class ItqlStore extends AbstractTripleStore {
         .append(" and ($s <rdf:type> <rdf:List> or $s <rdf:type> <rdf:Bag> ")
         .append("or $s <rdf:type> <rdf:Seq> or $s <rdf:type> <rdf:Alt>) )");
     }
+
+    return (qry.length() > len);
   }
 
   public <T> T get(ClassMetadata<T> cm, String id, T instance, Transaction txn,

topaz/core/src/it/java/org/topazproject/otm/BasicOtmTest.java
@@ -78,7 +78,9 @@ public class BasicOtmTest extends AbstractOtmTest {
     log.info("Testing basic CRUD operations ...");
     doInSession(new Action() {
         public void run(Session session) throws OtmException {
-          session.saveOrUpdate(new PublicAnnotation(URI.create("http://localhost/annotation/1")));
+          Annotation a;
+          session.saveOrUpdate(a = new PublicAnnotation(URI.create("http://localhost/annotation/1")));
+          a.setType(Annotation.NS + "Comment");
         }
       });
     doInSession(new Action() {
@@ -88,7 +90,6 @@ public class BasicOtmTest extends AbstractOtmTest {
 
           a.setCreator("Pradeep");
           a.setState(42);
-          a.setType(Annotation.NS + "Comment");
 
           if (a.foobar == null)
             a.foobar = new SampleEmbeddable();