4fe762fe970162eb16efa9465e85e48e4f456118
alex <alex@48088309-b40f-0410-b149-b14953253675>
2007-11-21 23:50:35 +0000

    #re 704 - added ArticleType configuration and some definitions
    git-svn-id: http://svn.topazproject.org/svn/head@4101 48088309-b40f-0410-b149-b14953253675

--- plos/webapp/src/main/java/org/plos/model/article/ArticleType.java
+++ plos/webapp/src/main/java/org/plos/model/article/ArticleType.java
@@ -14,22 +14,27 @@ import java.net.URI;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.SortedMap;
 
+import org.apache.commons.configuration.Configuration;
+import org.plos.configuration.ConfigurationStore;
 import org.plos.models.PLoS;
 
 public class ArticleType {
-  private static HashMap<URI, ArticleType> _knownArticleTypes = new HashMap<URI, ArticleType>();
-  private static HashMap<URI, ArticleType> _newArticleTypes = new HashMap<URI, ArticleType>();
+  private static HashMap<String, ArticleType> _knownArticleTypes = new HashMap<String, ArticleType>();
+  private static List<ArticleType> _articleTypeOrder = new ArrayList<ArticleType>();
+  private static HashMap<String, ArticleType> _newArticleTypes = new HashMap<String, ArticleType>();
+  private static ArticleType theDefaultArticleType = null;
   static {
-    addArticleType(URI.create(PLoS.PLOS_ArticleType + "research-article"), "Research Article");
-    addArticleType(URI.create(PLoS.PLOS_ArticleType + "editorial"), "Editorial");
-    addArticleType(URI.create(PLoS.PLOS_ArticleType + "correction"), "Correction");
-    addArticleType(URI.create(PLoS.PLOS_ArticleType + "article-commentary"), "Expert Commentary");
+  	configureArticleTypes(ConfigurationStore.getInstance().getConfiguration());
   }
   
   private URI uri;
   private String heading;
+	private boolean defaultArticleType = false;
   
   private ArticleType(URI articleTypeUri, String displayHeading) {
     uri = articleTypeUri;
@@ -37,9 +42,9 @@ public class ArticleType {
   }
   
   public static ArticleType getTypeForURI(URI uri) {
-    ArticleType at = _knownArticleTypes.get(uri);
+    ArticleType at = _knownArticleTypes.get(uri.toString());
     if (at == null) {
-      at = _newArticleTypes.get(uri);
+      at = _newArticleTypes.get(uri.toString());
       if ((at == null) && (uri != null)) {
         String uriStr = uri.toString();
         if (uriStr.contains("/")) {
@@ -50,7 +55,7 @@ public class ArticleType {
             // ignore and just use encoded uriStr :(
           }
           at = new ArticleType(uri, uriStr);
-          _newArticleTypes.put(uri, at);
+          _newArticleTypes.put(uri.toString(), at);
         }
       }
     }
@@ -58,11 +63,12 @@ public class ArticleType {
   }
   
   public static boolean addArticleType(URI uri, String heading) {
-    if (_knownArticleTypes.containsKey(uri)) {
+    if (_knownArticleTypes.containsKey(uri.toString())) {
       return false;
     }
     ArticleType at = new ArticleType(uri, heading);
-    _knownArticleTypes.put(uri, at);
+    _knownArticleTypes.put(uri.toString(), at);
+    _articleTypeOrder.add(at);
     return true;
   }
 
@@ -75,14 +81,42 @@ public class ArticleType {
   }
   
   /**
-   * Returns an ordered list of ArticleTypes that should be displayed by the JPS. 
-   * @return
+   * Returns an unmodifiable ordered list of known ArticleTypes as read in order from the configuration
+   * in configureArticleTypes(). 
+   * 
+   * @return Collection of ArticleType(s)
    */
-  public static Collection<ArticleType> getOrderedListForDisplay() {
-    // TODO - need to define a list of ArticleTypes that should be 
-    // displayed in the JPS. These should be ordered. This could ultimately
-    // be defined in mulgara. For now, just return all known ArticleTypes in
-    // a list. 
-    return new ArrayList<ArticleType>(_knownArticleTypes.values());
+  public static List<ArticleType> getOrderedListForDisplay() {
+    return Collections.unmodifiableList(_articleTypeOrder);
   }
+  
+  /**
+	 * Read in the ArticleTypes from the pubApp configuration (hint: normally defined in defauls.xml) 
+	 * and add them to the list of known ArticleType(s). The order of article types found in the 
+	 * configuration is significant and is returned in a Collection from getOrderedListForDisplay(). 
+	 */
+	public static void configureArticleTypes(Configuration myConfig) {
+    int count = 0;
+    String basePath = "pub.articleTypeList.articleType";
+    String uriStr;
+    String headingStr;
+    String isDefault;
+    do {
+    	StringBuffer baseIndex = new StringBuffer(basePath).append("(").append(count).append(").");
+    	uriStr = myConfig.getString(baseIndex.toString() + "typeUri");
+    	headingStr = myConfig.getString(baseIndex.toString() + "typeHeading");
+    	isDefault = myConfig.getString(baseIndex.toString() + "default");
+    	if ((uriStr != null) && (headingStr != null)) {
+    		addArticleType(URI.create(uriStr), headingStr);
+    		if ("true".equalsIgnoreCase(isDefault)) {
+    			theDefaultArticleType = getTypeForURI(URI.create(uriStr));
+    		}
+    	}
+    	count++;
+    } while (uriStr != null);
+	}
+	
+	public static ArticleType getDefaultArticleType() {
+		return theDefaultArticleType;
+	}
 }
