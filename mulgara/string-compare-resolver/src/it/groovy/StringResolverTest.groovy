673d1f36729a73d79f41a72b8c2d7d64b44b78ed
ronald <ronald@48088309-b40f-0410-b149-b14953253675>
2007-12-18 01:10:55 +0000

    Don't use ItqlClient for the tests, as that creates a circular dependency that isn't solvable the way maven works. Instead, use raw mulgara API's directly.
    git-svn-id: http://svn.topazproject.org/svn/head@4212 48088309-b40f-0410-b149-b14953253675

--- mulgara/string-compare-resolver/src/it/groovy/StringResolverTest.groovy
+++ mulgara/string-compare-resolver/src/it/groovy/StringResolverTest.groovy
@@ -8,99 +8,81 @@
  * http://opensource.org/licenses/ecl1.php
  */
 
-import org.topazproject.mulgara.itql.DefaultItqlClientFactory
-import org.topazproject.mulgara.itql.ItqlClient
-import org.topazproject.mulgara.itql.ItqlClientFactory
+import org.mulgara.itql.ItqlInterpreterBean;
+import org.mulgara.server.SessionFactory;
+import org.mulgara.server.driver.SessionFactoryFinder;
 
 class StringResolverTest extends GroovyTestCase {
   String MULGARA    = 'local:///topazproject'
   String TEST_MODEL = '<local:///topazproject#EqualIgnoreCaseTests>'
   String RSLV_MODEL = '<local:///topazproject#str>'
   String RSLV_TYPE  = '<http://topazproject.org/models#StringCompare>'
-  ItqlClientFactory itqlFactory = new DefaultItqlClientFactory()
-  ItqlClient itql
+  ItqlInterpreterBean itql
 
   void setUp() {
-    itql = itqlFactory.createClient(MULGARA.toURI())
-    itql.setAliases([topaz:'http://rdf.topazproject.org/RDF/'])
-    itql.doUpdate("create ${RSLV_MODEL} ${RSLV_TYPE};")
-    itql.doUpdate("create ${TEST_MODEL};")
-    itql.doUpdate("insert <foo:1> <bar:is> 'a' into ${TEST_MODEL};")
-    itql.doUpdate("insert <foo:2> <bar:is> 'b' into ${TEST_MODEL};")
-    itql.doUpdate("insert <foo:X> <bar:is> 'c' into ${TEST_MODEL};")
+    SessionFactory sf = SessionFactoryFinder.newSessionFactory(MULGARA.toURI());
+    itql = new ItqlInterpreterBean(sf.newSession(), sf.getSecurityDomain());
+    itql.setAliasMap([topaz:'http://rdf.topazproject.org/RDF/'.toURI()])
+    itql.executeUpdate("create ${RSLV_MODEL} ${RSLV_TYPE};")
+    itql.executeUpdate("create ${TEST_MODEL};")
+    itql.executeUpdate("insert <foo:1> <bar:is> 'a' into ${TEST_MODEL};")
+    itql.executeUpdate("insert <foo:2> <bar:is> 'b' into ${TEST_MODEL};")
+    itql.executeUpdate("insert <foo:X> <bar:is> 'c' into ${TEST_MODEL};")
   }
 
   void testEqualIgnoresCase() {
     def query = """select \$s from ${TEST_MODEL} 
                     where \$s <bar:is> \$o 
                       and \$o <topaz:equalsIgnoreCase> 'B' in ${RSLV_MODEL};"""
-    def ans = itql.doQuery(query)[0]
-    assert ans.next()
-    assert ans.getString('s') == 'foo:2'
+    def results = itql.executeQueryToString(query)
+    def ans = new XmlSlurper().parseText(results)
+    assert ans.query[0].solution.s.'@resource' == 'foo:2'
   }
 
   void testEqualIgnoreCaseSubject() {
     def query = """select \$s from ${TEST_MODEL}
                     where \$s <topaz:equalsIgnoreCase> <foo:x> in ${RSLV_MODEL};"""
-    def ans = itql.doQuery(query)[0]
-    assert ans.next()
-    assert ans.getString('s') == 'foo:X'
-    assert !ans.next()
+    def ans = new XmlSlurper().parseText(itql.executeQueryToString(query))
+    assert ans.query[0].solution.s.'@resource' == 'foo:X'
   }
 
   void testLt1() {
     def query = """select \$s from ${TEST_MODEL}
                     where \$s <bar:is> \$o
                       and \$o <topaz:lt> 'b' in ${RSLV_MODEL};"""
-    def ans = itql.doQuery(query)[0]
-    assert ans.next()
-    assert ans.getString('s') == 'foo:1'
-    assert !ans.next()
+    def ans = new XmlSlurper().parseText(itql.executeQueryToString(query))
+    assert ans.query[0].solution.s.'@resource' == 'foo:1'
   }
 
   void testLt2() {
     def query = """select \$s from ${TEST_MODEL}
                     where \$s <bar:is> \$o
                       and \$o <topaz:lt> 'c' in ${RSLV_MODEL};"""
-    def ans = itql.doQuery(query)[0]
-    assert ans.next()
-    assert ans.getString('s') == 'foo:1'
-    assert ans.next()
-    assert ans.getString('s') == 'foo:2'
-    assert !ans.next()
+    def ans = new XmlSlurper().parseText(itql.executeQueryToString(query))
+    assert ans.query[0].solution*.s.'@resource'.list() == [ 'foo:1', 'foo:2' ]
   }
 
   void testLe1() {
     def query = """select \$s from ${TEST_MODEL}
                     where \$s <bar:is> \$o
                       and \$o <topaz:le> 'b' in ${RSLV_MODEL};"""
-    def ans = itql.doQuery(query)[0]
-    assert ans.next()
-    assert ans.getString('s') == 'foo:1'
-    assert ans.next()
-    assert ans.getString('s') == 'foo:2'
-    assert !ans.next()
+    def ans = new XmlSlurper().parseText(itql.executeQueryToString(query))
+    assert ans.query[0].solution*.s.'@resource'.list() == [ 'foo:1', 'foo:2' ]
   }
 
   void testGt1() {
     def query = """select \$s from ${TEST_MODEL}
                     where \$s <bar:is> \$o
                       and \$o <topaz:gt> 'b' in ${RSLV_MODEL};"""
-    def ans = itql.doQuery(query)[0]
-    assert ans.next()
-    assert ans.getString('s') == 'foo:X'
-    assert !ans.next()
+    def ans = new XmlSlurper().parseText(itql.executeQueryToString(query))
+    assert ans.query[0].solution*.s.'@resource' == 'foo:X'
   }
 
   void testGe1() {
     def query = """select \$s from ${TEST_MODEL}
                     where \$s <bar:is> \$o
                       and \$o <topaz:ge> 'b' in ${RSLV_MODEL};"""
-    def ans = itql.doQuery(query)[0]
-    assert ans.next()
-    assert ans.getString('s') == 'foo:2'
-    assert ans.next()
-    assert ans.getString('s') == 'foo:X'
-    assert !ans.next()
+    def ans = new XmlSlurper().parseText(itql.executeQueryToString(query))
+    assert ans.query[0].solution*.s.'@resource'.list() == [ 'foo:2', 'foo:X' ]
   }
 }
