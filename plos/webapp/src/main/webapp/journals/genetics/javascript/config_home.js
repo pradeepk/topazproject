feb28a70cc47268d3a6de5b8b725d66d72608771
jsuttor <jsuttor@48088309-b40f-0410-b149-b14953253675>
2008-01-04 22:04:14 +0000

    Merged revisions 4256 via svnmerge from http://gandalf.topazproject.org/svn/branches/0.8.2
    ........
      r4256 | stoomey | 2007-12-26 17:20:27 -0800 (Wed, 26 Dec 2007) | 1 line
      
      Added third tab, "Most Viewed"
    ........
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@4417 48088309-b40f-0410-b149-b14953253675

--- plos/webapp/src/main/webapp/journals/genetics/javascript/config_home.js
+++ plos/webapp/src/main/webapp/journals/genetics/javascript/config_home.js
@@ -19,6 +19,12 @@ tabsListMap[tabsListMap.length] = {tabKey:    "featuredComments",
                                    urlLoad:   "/article/mostCommented.action",
                                    urlSave:   ""};
 
+tabsListMap[tabsListMap.length] = {tabKey:    "mostViewed",
+                                   title:     "Most Viewed",
+                                   className: "viewed",
+                                   urlLoad:   "/article/mostViewed.action",
+                                   urlSave:   ""};
+                                   
 var querystring = topaz.htmlUtil.getQuerystring();
 var tabSelectId = "";
 
