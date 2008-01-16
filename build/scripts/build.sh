44435e13d76bef5ab45211d0a7323db901bd6abc
ronald <ronald@48088309-b40f-0410-b149-b14953253675>
2008-01-16 00:44:48 +0000

    There's a bug in maven 2.0.4 (fixed in 2.0.7 and possibly earlier) that cause maven to pick the wrong version of a transitive dependency. So until we switch don't deploy topaz-extras.
    git-svn-id: http://svn.topazproject.org/svn/head@4541 48088309-b40f-0410-b149-b14953253675

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -76,7 +76,7 @@ echo ${SVNVERSION} > ${MVN_LAST_BUILD}
 # Deploy the OTM jars
 if [ ${N} -eq 0 ]; then
   echo "Deploying jars"
-  for pkg in "." "mulgara" "mulgara/string-compare-resolver" "mulgara/mulgara-client" "topaz" "topaz/core" "topaz/extras" ; do
+  for pkg in "." "mulgara" "mulgara/string-compare-resolver" "mulgara/mulgara-client" "topaz" "topaz/core" ; do
     ${MVN} ${MVNARGS} -N -f "$pkg/pom.xml" -Dmaven.test.skip=true deploy
   done
 fi
