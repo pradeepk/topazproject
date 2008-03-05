1019a25281c3306dc38aaabe7e966d0df849c0ea
jsuttor <jsuttor@48088309-b40f-0410-b149-b14953253675>
2008-03-05 00:24:32 +0000

    merge r4866 (backport) from /branches/0.8.2.2/:
    addresses #806 - Added serialVersionUID to the newly made serializable model classes to ensure proper class version checking at the jvm level plus minor code clean up.
    
    
    git-svn-id: http://svn.topazproject.org/svn/branches/0.8.2.1@4871 48088309-b40f-0410-b149-b14953253675

--- plos/libs/otm-models/src/main/java/org/plos/models/FormalCorrection.java
+++ plos/libs/otm-models/src/main/java/org/plos/models/FormalCorrection.java
@@ -4,6 +4,8 @@ import org.topazproject.otm.annotations.Entity;
 
 @Entity(type = FormalCorrection.RDF_TYPE)
 public class FormalCorrection extends Correction implements ArticleAnnotation {
+  private static final long serialVersionUID = 4949878990530615857L;
+  
   public static final String RDF_TYPE = Annotea.TOPAZ_TYPE_NS + "FormalCorrection";
   public String getType() {
     return RDF_TYPE;
--- plos/libs/otm-models/src/main/java/org/plos/models/MinorCorrection.java
+++ plos/libs/otm-models/src/main/java/org/plos/models/MinorCorrection.java
@@ -4,6 +4,8 @@ import org.topazproject.otm.annotations.Entity;
 
 @Entity(type = MinorCorrection.RDF_TYPE)
 public class MinorCorrection extends Correction  implements ArticleAnnotation {
+  private static final long serialVersionUID = -5374711498322357045L;
+  
   public static final String RDF_TYPE = Annotea.TOPAZ_TYPE_NS + "MinorCorrection";
   public String getType() {
     return RDF_TYPE;
