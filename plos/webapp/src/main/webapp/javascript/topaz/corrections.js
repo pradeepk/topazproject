c2c54f6480c77cca95c1f7990dbde8b373923eef
alex <alex@48088309-b40f-0410-b149-b14953253675>
2008-03-15 01:26:03 +0000

    Merged revisions 4649 via svnmerge from http://gandalf.topazproject.org/svn/branches/0.8.2.1
    ........
      r4649 | jkirton | 2008-02-06 15:58:45 -0800 (Wed, 06 Feb 2008) | 6 lines
      
      addreses #748, #749
      
      Formal Correction Header stubbed.
      
       - Added new action: getFormalCorrections
       - Added js to auto-populate the formal correction header once an article is loaded and to refresh when an article's body is re-loaded.
    ........
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@4987 48088309-b40f-0410-b149-b14953253675

--- plos/webapp/src/main/webapp/javascript/topaz/corrections.js
+++ plos/webapp/src/main/webapp/javascript/topaz/corrections.js
@@ -13,9 +13,9 @@ topaz.corrections = {
   style_minrcrctn: 'minrcrctn', // css class name for minor corrections
   style_frmlcrctn: 'frmlcrctn', // css class name for formal corrections
   style_fch: 'fch', // css class name for the formal correction header
+  annid: 'annid', // dom node attribute name to use to store annotation ids 
   
   aroot: null, // the top-most element of the article below which corrections are applied
-  fch: null, // the formal corrections header element ref
   fch: null, // the formal corrections header element ref
   fclist: null, // the formal corrections ordered list element ref
 
@@ -26,10 +26,6 @@ topaz.corrections = {
     this.aroot = dojo.byId('articleContainer');
     this.fch = dojo.byId('fch');
     this.fclist = dojo.byId('fclist');
-
-    // identify article corrections
-    this.arrElmMc = dojo.html.getElementsByClass(this.style_minrcrctn, this.aroot);
-    this.arrElmFc = dojo.html.getElementsByClass(this.style_frmlcrctn, this.aroot);
   },
   
   _num: function(arr) { return arr == null ? 0 : arr.length; },
@@ -51,13 +47,122 @@ topaz.corrections = {
    * Applies correction specific decorations to the article
    */
   apply: function() {
+    // [re-]identify article corrections
+    this.arrElmMc = dojo.html.getElementsByClass(this.style_minrcrctn, this.aroot);
+    this.arrElmFc = dojo.html.getElementsByClass(this.style_frmlcrctn, this.aroot);
+
     this._clearFCEntries();
     var show = (this.numFormalCrctns() > 0);
     if(show) {
-      // add or update formal correction header
-      if(!this.fch) this._clearFCEntries();
+      // [re-]fetch the formal corrections for the article
+      var targetUri = _annotationForm.target.value;
+      _ldc.show();
+      var bindArgs = {
+        url: _namespace + "/annotation/getFormalCorrections.action?target=" + targetUri,
+        method: "get",
+        error: function(type, error, evt){
+         var err = document.createTextNode("ERROR [AJAX]:" + error.toSource());
+         alert("ERROR:" + error.toSource());
+         return false;
+        },
+        load: function(type, data, evt) {
+          var jsonObj = dojo.json.evalJson(data);
+          if (jsonObj.actionErrors.length > 0) {
+            var errorMsg = "";
+            for (var i=0; i<jsonObj.actionErrors.length; i++) {
+              errorMsg = errorMsg + jsonObj.actionErrors[i] + "\n";
+            }
+            alert("ERROR [actionErrors]: " + errorMsg);
+          }
+          else {
+            // success
+            topaz.corrections.addFormalCorrections(jsonObj.formalCorrections);
+          }
+          _ldc.hide();
+          return false;
+        },
+        mimetype: "text/html",
+        transport: "XMLHTTPTransport"
+      };
+      dojo.io.bind(bindArgs);
     }
     this.fch.style.display = show? '' : 'none';
 
-  }//apply
+  },//apply
+
+  /**
+   * _toLi
+   *
+   * Creates an html li element for the given formal correction 
+   * used in the order list w/in the formal correction header.
+   *
+   * @param fc formal correction json obj
+   * @retun li element
+   */
+  _toLi: function(fc) {
+    var li = document.createElement('li');
+    li.innerHTML = fc.escapedTruncatedComment;
+    var p = li.firstChild;
+    var tn = p.firstChild;
+    tn.nodeValue = tn.nodeValue + ' (';
+    
+    var a = document.createElement('a');
+    a.setAttribute('href', '#');
+    a.setAttribute(this.annid, fc.id);
+    a.innerHTML = 'read formal correction';
+    dojo.event.connect(a, "onclick", topaz.corrections.onClickFC);
+    p.appendChild(a);
+    p.appendChild(document.createTextNode(')'));
+    return li;
+  },
+
+  /**
+   * addFormalCorrections
+   *
+   * Adds formal corrections to the formal correction header.
+   *
+   * @param arr Array of formal corrections
+   * @return void
+   */
+  addFormalCorrections: function(arr) {
+    for(var i=0; i<arr.length; i++) this.fclist.appendChild(this._toLi(arr[i]));
+  },
+
+  /**
+   * _findFrmlCrctnByAnnId
+   *
+   * Finds a formal correction node given an annotation id
+   * by searching the formal corrections node array property of this object
+   *
+   * @param annId The annotation (guid) id
+   * @return The found formal correction node or null if not found
+   */
+  _findFrmlCrctnByAnnId: function(annId) {
+    if(this.arrElmFc == null) return null;
+    var n, naid;
+    for(var i=0; i<this.arrElmFc.length; i++){
+      n = this.arrElmFc[i];
+      naid = dojo.html.getAttribute(n, 'annotationid');
+      if(naid == annId) return n
+    }
+    return null;
+  },
+
+  /**
+   * onClickFC
+   *
+   * Event handler for links in the formal correctionn header's ordered list of formal corrections.
+   *
+   * Scrolls into view the portion of the article containing the given correction (annotation) id
+   * then opens the note (comment) window for the bound bug.
+   *
+   * @param e event
+   */
+  onClickFC: function(e) {
+    var annId = dojo.html.getAttribute(e.target, topaz.corrections.annid);
+    e.preventDefault();
+    var fcn = topaz.corrections._findFrmlCrctnByAnnId(annId);
+    var annAnchor = fcn.firstChild;
+    topaz.displayComment.show(annAnchor); 
+  }
 }
