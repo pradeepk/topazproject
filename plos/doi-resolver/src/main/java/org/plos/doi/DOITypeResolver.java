276273e26ad52f7ee8f99cfab66045acb601b7b3
alex <alex@48088309-b40f-0410-b149-b14953253675>
2008-03-18 19:08:07 +0000

    Merged revisions 4660 via svnmerge from http://gandalf.topazproject.org/svn/branches/0.8.2.1
    ........
      r4660 | alex | 2008-02-08 09:27:21 -0800 (Fri, 08 Feb 2008) | 7 lines
      
      fixes #479 - Extended the DOI Resolver to map annotation DOIs to the plosone server. The supported doi URL should be formatted as:  
      
      http://doi-resolver-host:port/web-app-context-path/10.1371/annotation/xxxxx
      
      where "annotation/xxxxx" above is the part of the annotation DOI <info:doi/annotation/xxxxx>. Note that the word "annotation" need not be present, but it is part of the actual DOI we typically assign to annotations. 
      
      In this case, the DOIResolver makes no assumptions about the given annotation DOI format (unlike the prior implementation for article DOI's). The DOI of an annotation contains no reference to the journal, so the DOIResolver must query the root annotated item (an article?), and will use the journal regular expressions already defined to figure out which plos journal to redirect the annotation request to. Ultimately we rely on the article doi containing a reference to the journal, which is not ideal. We need a more reliable way to determine the correct journal to redirect to in a future implementation. This could be fixed when the whole journal-filtering implementation is re-visited. 
    ........
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@5004 48088309-b40f-0410-b149-b14953253675

--- plos/doi-resolver/src/main/java/org/plos/doi/DOITypeResolver.java
+++ plos/doi-resolver/src/main/java/org/plos/doi/DOITypeResolver.java
@@ -11,18 +11,21 @@ package org.plos.doi;
 
 
 import java.net.URI;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.topazproject.otm.OtmException;
 import org.topazproject.otm.RdfUtil;
 import org.topazproject.otm.Session;
 import org.topazproject.otm.SessionFactory;
 import org.topazproject.otm.Transaction;
 import org.topazproject.otm.impl.SessionFactoryImpl;
-import org.topazproject.otm.stores.ItqlStore;
 import org.topazproject.otm.query.Results;
+import org.topazproject.otm.stores.ItqlStore;
 
 /**
  * Resolver for the rdf:type of a DOI-URI.
@@ -31,8 +34,9 @@ import org.topazproject.otm.query.Results;
  */
 public class DOITypeResolver {
   private static final String MODEL = "<rmi://localhost/topazproject#filter:model=ri>";
+  private static final Log log = LogFactory.getLog(DOITypeResolver.class);
   private static final String QUERY = "select $t from " + MODEL + " where <${doi}> <rdf:type> $t";
-
+  private static final String GET_ANNOTATES_QUERY = "select $x from " + MODEL + " where <${doi}> <http://www.w3.org/2000/10/annotation-ns#annotates> $x";
 
   //
   private final SessionFactory sf;
@@ -48,7 +52,6 @@ public class DOITypeResolver {
     sf = new SessionFactoryImpl();
     sf.setTripleStore(new ItqlStore(mulgaraUri));
   }
-
   /**
    * Queries the ITQL database and returns all known rdf:type values of a URI.
    *
@@ -58,7 +61,7 @@ public class DOITypeResolver {
    *
    * @throws OtmException if an exception occurred talking to the service
    */
-  public String[] getRdfTypes(URI doi) throws OtmException {
+  public Set<String> getRdfTypes(URI doi) throws OtmException {
     String query = RdfUtil.bindValues(QUERY, "doi", doi.toString());
 
     List<String> types = new ArrayList<String>();
@@ -77,6 +80,41 @@ public class DOITypeResolver {
       sess.close();
     }
 
-    return types.toArray(new String[types.size()]);
+    return new HashSet<String>(types);
+  }
+
+  /**
+   * Recursively attempts to find the annotated root (article?) of the given annotation doi. Retruns the doi
+   * found that does not annotate another doi. 
+   * 
+   * @param annotationDoi
+   * @return
+   * @throws ItqlInterpreterException
+   * @throws RemoteException
+   * @throws AnswerException
+   */
+  public String getAnnotatedRoot(String annotationDoi) {
+    /* TODO - need to reimplement this without ItqlHelper... as that doesn't appear to be 
+     * supported any longer in head. 
+     * 
+    String query = ItqlHelper.bindValues(GET_ANNOTATES_QUERY, "doi", annotationDoi.toString());
+    
+    String results;
+    synchronized (this) {
+      results = itql.doQuery(query, null);
+    }
+    StringAnswer answer = new StringAnswer(results);
+    List<String[]> rows = (List<String[]>)((StringAnswer.StringQueryAnswer) (answer.getAnswers().get(0))).getRows();
+    
+    if ((rows.size() > 0) && (rows.get(0).length > 0)) {
+      String annotatedDoi = rows.get(0)[0];
+      if (annotatedDoi.equals(annotationDoi)) {
+        log.error("Circular dependency found for annotation doi: '"+annotationDoi+"'");
+        return annotatedDoi;
+      }
+      return getAnnotatedRoot(annotatedDoi);
+    }
+    */
+    return annotationDoi;
   }
 }
