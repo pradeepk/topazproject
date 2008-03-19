9e53a77ed010e400698139cd67332955c0123678
alex <alex@48088309-b40f-0410-b149-b14953253675>
2008-03-19 17:54:21 +0000

    Merged revisions 4679 via svnmerge from http://gandalf.topazproject.org/svn/branches/0.8.2.1
    ........
      r4679 | alex | 2008-02-12 13:30:33 -0800 (Tue, 12 Feb 2008) | 1 line
      
      re#743 - Implemented advanced query builder in SearchAction and hooked up to existing form fields in searchStart.ftl. Checking in prematurely so that work can begin on the javascript portions of the page. More testing to be done / more tests to be written yet... The sort order has not been implemented - and it may not be possible to build these requirements into the query string from initial research. 
    ........
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@5029 48088309-b40f-0410-b149-b14953253675

--- plos/webapp/src/main/java/org/plos/search/action/SearchAction.java
+++ plos/webapp/src/main/java/org/plos/search/action/SearchAction.java
@@ -14,15 +14,26 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.plos.ApplicationException;
 import org.plos.action.BaseActionSupport;
+import org.plos.article.service.BrowseService;
 import org.plos.search.SearchResultPage;
 import org.plos.search.SearchUtil;
 import org.plos.search.service.SearchHit;
 import org.plos.search.service.SearchService;
 import org.plos.util.FileUtils;
+import org.topazproject.otm.query.QueryParser;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Search Action class to search for simple or advanced search.
@@ -34,13 +45,31 @@ public class SearchAction extends BaseActionSupport {
   private int startPage = 0;
   private int pageSize = 10;
   private SearchService searchService;
+  private BrowseService browseService;
 
   private static final Log log = LogFactory.getLog(SearchAction.class);
   private Collection<SearchHit> searchResults;
+  Map<String, Integer> categoryInfos = new HashMap<String, Integer>(); // empty map for non-null safety
   private String title;
   private String text;
   private int totalNoOfResults;
   private String description;
+  private String[] creator = null;
+  private String authorNameOp;
+  private String textSearchAll;
+  private String textSearchExactPhrase;
+  private String textSearchAtLeastOne;
+  private String textSearchWithout;
+  private String textSearchOption;
+  private String dateTypeSelect;
+  private String startYear;
+  private String startMonth;
+  private String startDay;
+  private String endYear;
+  private String endMonth;
+  private String endDay;
+  private String subjectCatOpt;
+  private String[] limitToCategory = null;
 
   /**
    * @return return simple search result
@@ -53,7 +82,7 @@ public class SearchAction extends BaseActionSupport {
    * @return return simple search result
    */
   public String executeAdvancedSearch() {
-    query = getAdvancedQuery();
+    query = buildAdvancedQuery();
     return executeSearch(query);
   }
 
@@ -61,6 +90,7 @@ public class SearchAction extends BaseActionSupport {
     try {
       if (StringUtils.isBlank(queryString)) {
         addActionError("Please enter a query string.");
+        categoryInfos = browseService.getCategoryInfos();
         return INPUT;
       }
       final SearchResultPage searchResultPage = searchService.find(queryString, startPage, pageSize);
@@ -88,16 +118,188 @@ public class SearchAction extends BaseActionSupport {
     }
   }
 
-  private String getAdvancedQuery() {
+  private String buildAdvancedQuery() {
     final Collection<String> fields = new ArrayList<String>();
 
-    if (StringUtils.isNotBlank(title)) fields.add("title:" + title);
-    if (StringUtils.isNotBlank(text)) fields.add(text);
-    if (StringUtils.isNotBlank(description)) fields.add("description:" + description);
-
+    // Build Search terms for Authors if specified
+    if ((creator != null) && (creator.length > 0) && (StringUtils.isNotBlank(creator[1]))) {
+      StringBuffer buf = new StringBuffer("(creator:(");
+      boolean allAuthors = false;
+      if ("all".equals(authorNameOp)) {
+        allAuthors = true;
+      }
+      for (int i=0; i<creator.length; i++) {
+        String creatorName = creator[i];
+        if (StringUtils.isNotBlank(creatorName)) {
+          buf.append("\"");
+          buf.append(escape(creatorName));
+          buf.append("\"");
+        }
+        if ((i < creator.length-1) && (StringUtils.isNotBlank(creator[i+1]))) {
+          if (allAuthors) {
+            buf.append(" AND ");
+          } else {
+            buf.append(" OR ");
+          }
+        }
+      }
+      buf.append(" ))");
+      fields.add(buf.toString());
+    }
+    
+    // Build Search Article Text section of advanced search. 
+    
+    String textSearchField = null;
+    if (StringUtils.isNotBlank(textSearchOption)) {
+      if ("abstract".equals(textSearchOption)) {
+        textSearchField = "description:";
+      }
+      if ("refs".equals(textSearchField)) {
+        textSearchField = "citation:";
+      }
+      if ("title".equals(textSearchField)) {
+        textSearchField = "title:";
+      }
+    }
+    // All words field should be in parenthesis with AND between each word. 
+    if (StringUtils.isNotBlank(textSearchAll)) {
+      String[] words = StringUtils.split(textSearchAll);
+      StringBuffer buf = new StringBuffer();
+      if (textSearchField != null) {
+        buf.append(textSearchField);
+      }
+      buf.append("( ");
+      for (int i=0; i<words.length; i++) {
+        buf.append(escape(words[i]));
+        if (i < words.length-1) {
+          buf.append(" AND ");
+        }
+      }
+      buf.append(" )");
+      fields.add(buf.toString());
+    }
+    
+    // Exact phrase should be placed in quotes
+    if (StringUtils.isNotBlank(textSearchExactPhrase)) {
+      StringBuffer buf = new StringBuffer();
+      if (textSearchField != null) {
+        buf.append(textSearchField);
+      }
+      buf.append("(\"");
+      buf.append(escape(textSearchExactPhrase));
+      buf.append("\")");
+      fields.add(buf.toString());
+    }
+    
+    // At least one of should be placed in parenthesis separated by spaced (OR'ed)
+    if (StringUtils.isNotBlank(textSearchAtLeastOne)) {
+      StringBuffer buf = new StringBuffer();
+      if (textSearchField != null) {
+        buf.append(textSearchField);
+      }
+      buf.append("( ");
+      buf.append(escape(textSearchAtLeastOne));
+      buf.append(" )");
+      fields.add(buf.toString());
+    }
+    
+    // Without the words - in parenthesis with NOT operator and AND'ed together
+    // E.g. (-"the" AND -"fat" AND -"cat")
+    if (StringUtils.isNotBlank(textSearchWithout)) {
+      // TODO - we might want to allow the entry of phrases to omit (entered using quotes?) 
+      // - in which case we have to be smarter about how we split...
+      String[] words = StringUtils.split(textSearchWithout);
+      StringBuffer buf = new StringBuffer();
+      if (textSearchField != null) {
+        buf.append(textSearchField);
+      }
+      buf.append("( ");
+      for (int i=0; i<words.length; i++) {
+        buf.append("-\"");
+        buf.append(escape(words[i]));
+        buf.append("\"");
+        if (i < words.length-1) {
+          buf.append(" AND ");
+        }
+      }
+      buf.append(" )");
+      fields.add(buf.toString());
+    }
+    
+    if (StringUtils.isNotBlank(dateTypeSelect)) {
+      if ("range".equals(dateTypeSelect)) {
+        if (isDigit(startYear)&& isDigit(startMonth) && isDigit(startDay) && 
+            isDigit(endYear) && isDigit(endMonth) && isDigit(endDay)) {
+          StringBuffer buf = new StringBuffer("date:[");
+          buf.append(startYear).append(startMonth).append(startDay);
+          buf.append(" TO ");
+          buf.append(endYear).append(endMonth).append(endDay);
+          buf.append("]");
+          fields.add(buf.toString());
+        }
+      } else {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String endDateStr = sdf.format(new Date());
+        Calendar cal = new GregorianCalendar();
+        if ("week".equals(dateTypeSelect)) {
+          cal.add(Calendar.DATE, -7);
+        }
+        if ("month".equals(dateTypeSelect)) {
+          cal.add(Calendar.MONTH, -1);
+        }
+        if ("month".equals(dateTypeSelect)) {
+          cal.add(Calendar.MONTH, -3);
+        }
+        String startDateStr = sdf.format(cal.getTime());
+        
+        StringBuffer buf = new StringBuffer("date:[");
+        buf.append(startDateStr).append(" TO ").append(endDateStr).append("]");
+        fields.add(buf.toString());
+      }
+    }
+    
+    if ("some".equals(subjectCatOpt)) {
+      if ((limitToCategory != null) && (limitToCategory.length > 0)) {
+        StringBuffer buf = new StringBuffer("subject:( " );
+        for (int i=0; i < limitToCategory.length; i++) {
+          buf.append("\"").append(escape(limitToCategory[i])).append("\" ");
+        }
+        buf.append(")");
+      }
+    }
+    
+    String advSearchQueryStr = StringUtils.join(fields.iterator(), " AND ");
+    if (log.isDebugEnabled()) {
+      log.debug("Generated advanced search query: "+advSearchQueryStr);
+    }
     return StringUtils.join(fields.iterator(), " AND ");
   }
-
+  
+  /**
+   * Static helper method to escape special characters in a Lucene search string with a \
+   */
+  protected static final String escapeChars = "+-&|!(){}[]^\"~*?:\\";
+  protected String escape(String in) {
+    StringBuffer buf = new StringBuffer();
+    for (int i=0; i < in.length(); i++) {
+      if (escapeChars.indexOf(in.charAt(i)) != -1) {
+        buf.append("\\");
+      }
+      buf.append(in.charAt(i));
+    }
+    return buf.toString();
+  }
+  
+  protected boolean isDigit(String str) {
+    if (str == null) return false;
+    for (int i=0; i < str.length(); i++) {
+      if (!(Character.isDigit(str.charAt(i)))) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
   /**
    * Set the simple query
    * @param query query
@@ -130,6 +332,14 @@ public class SearchAction extends BaseActionSupport {
     this.searchService = searchService;
   }
 
+  /**
+   * Spring injected BrowseService
+   * @param browseService searchService
+   */
+  public void setBrowseService(final BrowseService browseService) {
+    this.browseService = browseService;
+  }
+  
   /**
    * @return the search results.
    */
@@ -216,5 +426,133 @@ public class SearchAction extends BaseActionSupport {
    */
   public void setDescription(final String description) {
     this.description = description;
+  }
+
+  public String getTextSearchAll() {
+    return textSearchAll;
+  }
+
+  public void setTextSearchAll(String textSearchAll) {
+    this.textSearchAll = textSearchAll;
+  }
+
+  public String getTextSearchExactPhrase() {
+    return textSearchExactPhrase;
+  }
+
+  public void setTextSearchExactPhrase(String textSearchExactPhrase) {
+    this.textSearchExactPhrase = textSearchExactPhrase;
+  }
+
+  public String getTextSearchAtLeastOne() {
+    return textSearchAtLeastOne;
+  }
+
+  public void setTextSearchAtLeastOne(String textSearchAtLeastOne) {
+    this.textSearchAtLeastOne = textSearchAtLeastOne;
+  }
+
+  public String getTextSearchWithout() {
+    return textSearchWithout;
+  }
+
+  public void setTextSearchWithout(String textSearchWithout) {
+    this.textSearchWithout = textSearchWithout;
+  }
+
+  public String getTextSearchOption() {
+    return textSearchOption;
+  }
+
+  public void setTextSearchOption(String textSearchOption) {
+    this.textSearchOption = textSearchOption;
+  }
+
+  public String getDateTypeSelect() {
+    return dateTypeSelect;
+  }
+
+  public void setDateTypeSelect(String dateTypeSelect) {
+    this.dateTypeSelect = dateTypeSelect;
+  }
+
+  public String getStartYear() {
+    return startYear;
+  }
+
+  public void setStartYear(String startYear) {
+    this.startYear = startYear;
+  }
+
+  public String getStartMonth() {
+    return startMonth;
+  }
+
+  public void setStartMonth(String startMonth) {
+    this.startMonth = startMonth;
+  }
+
+  public String getStartDay() {
+    return startDay;
+  }
+
+  public void setStartDay(String startDay) {
+    this.startDay = startDay;
+  }
+
+  public String getEndYear() {
+    return endYear;
+  }
+
+  public void setEndYear(String endYear) {
+    this.endYear = endYear;
+  }
+
+  public String getEndMonth() {
+    return endMonth;
+  }
+
+  public void setEndMonth(String endMonth) {
+    this.endMonth = endMonth;
+  }
+
+  public String getEndDay() {
+    return endDay;
+  }
+
+  public void setEndDay(String endDay) {
+    this.endDay = endDay;
+  }
+
+  public String getSubjectCatOpt() {
+    return subjectCatOpt;
+  }
+
+  public void setSubjectCatOpt(String subjectCatOpt) {
+    this.subjectCatOpt = subjectCatOpt;
+  }
+
+  public Map<String, Integer> getCategoryInfos() {
+    return categoryInfos;
+  }
+
+  public void setCategoryInfos(Map<String, Integer> categoryInfos) {
+    this.categoryInfos = categoryInfos;
+  }
+
+  public String[] getCreator() {
+    return creator;
+  }
+
+  public void setCreator(String[] creator) {
+    this.creator = creator;
+  }
+
+  public String[] getLimitToCategory() {
+    return limitToCategory;
+  }
+
+  public void setLimitToCategory(String[] limitToCategory) {
+    this.limitToCategory = limitToCategory;
   }
 }
