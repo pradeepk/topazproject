7c2ccd927fae8431f5a30c524fa5ad8927bb5e27
alex <alex@48088309-b40f-0410-b149-b14953253675>
2008-03-20 23:04:03 +0000

    Merged revisions 4694 via svnmerge from http://gandalf.topazproject.org/svn/branches/0.8.2.1
    ........
      r4694 | jkirton | 2008-02-13 10:54:04 -0800 (Wed, 13 Feb 2008) | 4 lines
      
      addresses #748, #749
      Finished method that assembles annotation citation strings.
      NOTE: Verify the "comment author" format is correct.
    ........
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@5044 48088309-b40f-0410-b149-b14953253675

--- plos/webapp/src/main/java/org/plos/annotation/action/ListReplyAction.java
+++ plos/webapp/src/main/java/org/plos/annotation/action/ListReplyAction.java
@@ -10,6 +10,9 @@
 package org.plos.annotation.action;
 
 import java.net.URI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -17,21 +20,41 @@ import org.plos.ApplicationException;
 import org.plos.annotation.service.Reply;
 import org.plos.annotation.service.WebAnnotation;
 import org.plos.article.service.ArticleOtmService;
+import org.plos.journal.JournalService;
 import org.plos.models.Article;
+import org.plos.models.Journal;
+import org.springframework.beans.factory.annotation.Required;
+import org.topazproject.otm.Session;
+import org.topazproject.otm.Transaction;
+import org.topazproject.otm.util.TransactionHelper;
 
 import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
 
 /**
  * Action class to get a list of replies to annotations.
  */
+@SuppressWarnings("serial")
 public class ListReplyAction extends AnnotationActionSupport {
+
+  /**
+   * Date format used in constructing an annotation citation string
+   * <p>
+   * <code>d MMM yyyy</code>
+   * <p>
+   * E.g.: "5 Jan 2008"
+   */
+  private static final DateFormat annotationCitationDateFormat = new SimpleDateFormat("d MMM yyyy");
+
   private String root;
   private String inReplyTo;
   private Reply[] replies;
+  private Session session;
   private WebAnnotation baseAnnotation;
   private ArticleOtmService articleOtmService;
+  private JournalService journalService;
   private Article articleInfo;
   private String citation;
+  private Set<Journal> journalList;
 
   private static final Log log = LogFactory.getLog(ListReplyAction.class);
 
@@ -48,27 +71,38 @@ public class ListReplyAction extends AnnotationActionSupport {
   }
 
   /**
-   * List all the replies for a given root and inRelyTo in a threaded tree structure.
+   * List all the replies for a given root and inRelyTo in a threaded tree
+   * structure.
+   * 
    * @return webwork status for the call
    * @throws Exception Exception
    */
   public String listAllReplies() throws Exception {
     try {
-      // Allow a single 'root' param to be accepted. If 'inReplyTo' is null or empty string, set to root value. 
-      // This results in that single annotation being displayed. 
+      // Allow a single 'root' param to be accepted. If 'inReplyTo' is null or
+      // empty string, set to root value.
+      // This results in that single annotation being displayed.
       if ((inReplyTo == null) || inReplyTo.length() == 0) {
         inReplyTo = root;
       }
-      if (log.isDebugEnabled()){
+      if (log.isDebugEnabled()) {
         log.debug("listing all Replies for root: " + root);
       }
       baseAnnotation = getAnnotationService().getAnnotation(root);
       replies = getAnnotationService().listAllReplies(root, inReplyTo);
-      articleInfo = getArticleOtmService().getArticle(new URI(baseAnnotation.getAnnotates()));
-      
+      final URI articleURI = new URI(baseAnnotation.getAnnotates());
+      articleInfo = getArticleOtmService().getArticle(articleURI);
+
+      TransactionHelper.doInTx(session, new TransactionHelper.Action<Void>() {
+        public Void run(Transaction tx) {
+          journalList = journalService.getJournalsForObject(articleURI);
+          return null;
+        }
+      });
+
       // construct citation string
       citation = assembleCitationString();
-      
+
     } catch (final ApplicationException e) {
       log.error("Could not list all replies for root:" + root, e);
       addActionError("Reply fetching failed with error message: " + e.getMessage());
@@ -76,32 +110,69 @@ public class ListReplyAction extends AnnotationActionSupport {
     }
     return SUCCESS;
   }
-  
+
   /**
-   * Assemble the Correction annotation citation string.
-   * <p>FORMAT:
-   * <p>[Author of comment]. "[Comment Title]." Online comment. [Date Comment Posted]. "[Title of Original Article]." [Full name of first author of original article] et al. [Title of Journal]. {URL}
-   * @return Correction annotation citation string.
+   * Assembles a String representing an annotation citatation based on a
+   * prescribed format.
+   * <p>
+   * FORMAT: <blockquote>{Author of comment}. {Comment Title}. Online comment.
+   * {Date Comment Posted}. {Title of Original Article}. {Full name of first
+   * author of original article} et al. {Title of Journal}. {annotation URL}</blockquote>
+   * 
+   * @return A newly created String.
+   * @see http://wiki.plos.org/pmwiki.php/Topaz/Corrections for the format
+   *      specification
    */
   private String assembleCitationString() {
     assert baseAnnotation != null;
     assert articleInfo != null;
+
     StringBuffer sb = new StringBuffer(1024);
+
+    // author of comment
     sb.append(baseAnnotation.getCreatorName());
     sb.append(". ");
+
+    // comment title
     sb.append(baseAnnotation.getCommentTitle());
     sb.append(". Online comment.  ");
-    sb.append(baseAnnotation.getCreated());
+
+    // comment post date
+    synchronized (annotationCitationDateFormat) {
+      sb.append(annotationCitationDateFormat.format(baseAnnotation.getCreatedAsDate()));
+    }
     sb.append(".  ");
+
+    // original article title
     sb.append(articleInfo.getDublinCore().getTitle());
     sb.append(".  ");
-    sb.append(articleInfo.getDublinCore().getCreators().iterator().next()); 
+
+    // first author name et al.
+    // NOTE presuming the creator list, which is a Set, is ordered
+    // whereby the initial element is the "primary" author
+    try {
+      sb.append(articleInfo.getDublinCore().getCreators().iterator().next());
+    } catch (Throwable t) {
+      sb.append("-Unknown Author(s)-");
+    }
     sb.append(" et al.  ");
-    sb.append("[TODO Title of Journal].  ");
+
+    // [primary] owning journal title
+    // NOTE presuming the journal list, which is a Set, is ordered
+    // whereby the initial element is the "primary" journal
+    try {
+      sb.append(journalList.iterator().next().getDublinCore().getTitle());
+    } catch (Throwable t) {
+      sb.append("-Unknown Journal(s)-");
+    }
+    sb.append(".  ");
+
+    // annotation URI
     sb.append(baseAnnotation.getId());
+
     return sb.toString();
   }
-  
+
   /**
    * @return The constructed annotation citation string.
    */
@@ -170,5 +241,21 @@ public class ListReplyAction extends AnnotationActionSupport {
    */
   public void setArticleInfo(Article articleInfo) {
     this.articleInfo = articleInfo;
+  }
+
+  /**
+   * @param session The session to set.
+   */
+  @Required
+  public void setOtmSession(Session session) {
+    this.session = session;
+  }
+
+  /**
+   * @param journalService The journalService to set.
+   */
+  @Required
+  public void setJournalService(JournalService journalService) {
+    this.journalService = journalService;
   }
 }
