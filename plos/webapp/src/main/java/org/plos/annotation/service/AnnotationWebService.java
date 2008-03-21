dded5ea13a605919fdc99665790841a9822dc96f
alex <alex@48088309-b40f-0410-b149-b14953253675>
2008-03-21 00:01:50 +0000

    Merged revisions 4702 via svnmerge from http://gandalf.topazproject.org/svn/branches/0.8.2.1
    ........
      r4702 | alex | 2008-02-13 14:18:32 -0800 (Wed, 13 Feb 2008) | 1 line
      
      The last change took out the wrong duplicate code. We need to compare the classes using isInstance() in order to catch the subclasses of the set specified. 
    ........
    
    
    git-svn-id: http://svn.topazproject.org/svn/head@5050 48088309-b40f-0410-b149-b14953253675

--- plos/webapp/src/main/java/org/plos/annotation/service/AnnotationWebService.java
+++ plos/webapp/src/main/java/org/plos/annotation/service/AnnotationWebService.java
@@ -399,8 +399,11 @@ public class AnnotationWebService extends BaseAnnotationService {
     if (annotationClassTypes != null) {
       List<Annotation> filteredAnnotations = new ArrayList<Annotation>();
       for (Annotation a : allAnnotations) {
-        if (annotationClassTypes.contains(a.getClass())) {
-          filteredAnnotations.add(a);
+        for (Class classType : annotationClassTypes) {
+          if (classType.isInstance(a)) {
+            filteredAnnotations.add(a);
+            break;
+          }
         }
       }
       return filteredAnnotations;
