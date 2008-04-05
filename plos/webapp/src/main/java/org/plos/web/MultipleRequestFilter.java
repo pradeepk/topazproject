6bdbf2d6ee2a7913172a6ede7997f242d75df490
amit <amit@48088309-b40f-0410-b149-b14953253675>
2008-04-05 17:29:09 +0000

    Removed unused imports.
    git-svn-id: http://svn.topazproject.org/svn/head@5384 48088309-b40f-0410-b149-b14953253675

--- plos/webapp/src/main/java/org/plos/web/MultipleRequestFilter.java
+++ plos/webapp/src/main/java/org/plos/web/MultipleRequestFilter.java
@@ -20,11 +20,9 @@ package org.plos.web;
 
 import java.io.IOException;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
