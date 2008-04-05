995c41b88295e1ec353f78a044da2a3e72d51b38
amit <amit@48088309-b40f-0410-b149-b14953253675>
2008-04-05 17:26:00 +0000

    Removed unnecessary import.
    git-svn-id: http://svn.topazproject.org/svn/head@5382 48088309-b40f-0410-b149-b14953253675

--- fedora/fedora-blobstore/src/main/java/org/topazproject/fedora/otm/DefaultFedoraBlob.java
+++ fedora/fedora-blobstore/src/main/java/org/topazproject/fedora/otm/DefaultFedoraBlob.java
@@ -20,7 +20,6 @@ package org.topazproject.fedora.otm;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
