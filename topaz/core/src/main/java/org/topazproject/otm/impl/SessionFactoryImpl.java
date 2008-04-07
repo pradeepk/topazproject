pradeepk/topazproject

commit ac392d7cf82a9bc7d7bcfc5cfd3095f07b205735

I'll be damned! The PMD report actually found a real bug. The equals() in ClassMetadata was wrong and thus resulting in singleton sets for the rdf:type maps. Fixing of that exposed a hole in the most-specific sub-class algorithm.
git-svn-id: http://svn.topazproject.org/svn/head@5349 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/java/org/topazproject/otm/impl/SessionFactoryImpl.java
@@ -245,32 +245,47 @@ public class SessionFactoryImpl implements SessionFactory {
     if ((typeUris == null) || (typeUris.size() == 0))
       return (clazz == null) ? null : ((clazz.getType() != null) ? null : clazz);
 
-    ClassMetadata solution  = null;
-
+    Set<ClassMetadata> solutions = null;
     for (String uri : typeUris) {
       Set<ClassMetadata> classes = typemap.get(uri);
 
       if (classes == null)
         continue;
 
-      ClassMetadata candidate = clazz;
+      Set<ClassMetadata> candidates = new HashSet<ClassMetadata>();
 
-      //find the most specific class with the same rdf:type
       for (ClassMetadata cl : classes) {
         if (instantiable && !cl.getEntityBinder(mode).isInstantiable())
           continue;
-
-        if ((candidate == null) || candidate.isAssignableFrom(cl))
-          candidate = cl;
+        if (typeUris.contains(cl.getType()) &&
+           ((clazz == null) || clazz.isAssignableFrom(cl)))
+          candidates.add(cl);
       }
 
-      // accept this candidate only if it corresponds to the type-uris
-      if (classes.contains(candidate) 
-          && ((solution == null) || solution.isAssignableFrom(candidate)))
-            solution = candidate;
+      if ((solutions == null) || solutions.isEmpty())
+        solutions = candidates;
+      else if (!candidates.isEmpty()) {
+        Set<ClassMetadata> intersection = new HashSet<ClassMetadata>(solutions);
+        intersection.retainAll(candidates);
+        if (intersection.isEmpty())
+          solutions.addAll(candidates);
+        else
+          solutions = intersection;
+      }
     }
 
-    return solution;
+    if (solutions == null)
+      return null;
+
+    if (solutions.size() == 1)
+      return solutions.iterator().next();
+
+    ClassMetadata random = null;
+    for (ClassMetadata cl : solutions)
+      if ((random == null) || random.isAssignableFrom(cl))
+        random = cl;
+
+    return random;
   }
 
   /*
@@ -306,9 +321,7 @@ public class SessionFactoryImpl implements SessionFactory {
     for (String name : cm.getNames())
       entitymap.put(name, cm);
 
-    String type = cm.getType();
-
-    if (type != null) {
+    for (String type : cm.getTypes()) {
       Set<ClassMetadata> set = typemap.get(type);
 
       if (set == null) {

topaz/core/src/test/groovy/org/topazproject/otm/metadata/BuilderTest.groovy
@@ -810,13 +810,32 @@ public class BuilderTest extends GroovyTestCase {
     ClassMetadata wrong = rdf.sessFactory.getClassMetadata(wrongc);
     ClassMetadata right = rdf.sessFactory.getClassMetadata(rightc);
 
+    assert base.isAssignableFrom(base)
+    assert base.isAssignableFrom(right)
+    assert base.isAssignableFrom(wrong)
+    assert right.isAssignableFrom(right)
+    assert !right.isAssignableFrom(wrong)
+    assert !right.isAssignableFrom(base)
+    assert wrong.isAssignableFrom(wrong)
+    assert !wrong.isAssignableFrom(right)
+    assert !wrong.isAssignableFrom(base)
+
     ClassMetadata p = rdf.sessFactory.getSubClassMetadata(base, EntityMode.POJO, ['base:type', 'sub:type'])
+    assert right == p
+
+    p = rdf.sessFactory.getSubClassMetadata(base, EntityMode.POJO, ['sub:type', 'base:type'])
     assert right == p
 
     p = rdf.sessFactory.getSubClassMetadata(wrong, EntityMode.POJO, ['base:type', 'sub:type'])
     assert wrong == p;
 
+    p = rdf.sessFactory.getSubClassMetadata(wrong, EntityMode.POJO, ['sub:type', 'base:type'])
+    assert wrong == p;
+
     p = rdf.sessFactory.getSubClassMetadata(right, EntityMode.POJO, ['base:type', 'sub:type'])
+    assert right == p;
+
+    p = rdf.sessFactory.getSubClassMetadata(right, EntityMode.POJO, ['sub:type', 'base:type'])
     assert right == p;
 
     p = rdf.sessFactory.getSubClassMetadata(wrong, EntityMode.POJO, ['base:type'])

topaz/core/src/main/java/org/topazproject/otm/ClassMetadata.java
@@ -467,7 +467,7 @@ public class ClassMetadata {
 
     ClassMetadata o = (ClassMetadata) other;
 
-    return name.equals(name);
+    return name.equals(o.name);
   }
 
   @Override

commit df56bc1abd71b70787e41a25bb4e5e65a80573f7

Fix a bug introduced in r5349 and added one more test case.
git-svn-id: http://svn.topazproject.org/svn/head@5390 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/java/org/topazproject/otm/impl/SessionFactoryImpl.java
@@ -255,6 +255,12 @@ public class SessionFactoryImpl implements SessionFactory {
       return (clazz == null) ? null : ((clazz.getType() != null) ? null : clazz);
 
     Set<ClassMetadata> solutions = null;
+
+    if ((clazz != null) && (clazz.getType() == null)) {
+      solutions = new HashSet<ClassMetadata>();
+      solutions.add(clazz);
+    }
+
     for (String uri : typeUris) {
       Set<ClassMetadata> classes = typemap.get(uri);
 

topaz/core/src/test/groovy/org/topazproject/otm/metadata/BuilderTest.groovy
@@ -873,8 +873,11 @@ public class BuilderTest extends GroovyTestCase {
     p = rdf.sessFactory.getSubClassMetadata(wrong, EntityMode.POJO, ['sub:type'])
     assert null == p;
 
-    p = rdf.sessFactory.getSubClassMetadata(nt, EntityMode.POJO, ['junk:type'])
+    p = rdf.sessFactory.getSubClassMetadata(base, EntityMode.POJO, ['junk:type'])
     assert null == p;
+
+    p = rdf.sessFactory.getSubClassMetadata(nt, EntityMode.POJO, ['junk:type'])
+    assert nt == p;
 
     p = rdf.sessFactory.getSubClassMetadata(nt, EntityMode.POJO, [])
     assert nt == p;