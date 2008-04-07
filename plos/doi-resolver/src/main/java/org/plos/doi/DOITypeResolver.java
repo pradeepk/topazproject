4c90ca3db03e3779bfe9690b6b9ce9d1ea9ab622
pradeep <pradeep@48088309-b40f-0410-b149-b14953253675>
2008-04-07 21:44:40 +0000

    Reworked the DOI resolver and added tests. Fixes #851.
    git-svn-id: http://svn.topazproject.org/svn/head@5391 48088309-b40f-0410-b149-b14953253675

--- plos/doi-resolver/src/main/java/org/plos/doi/DOITypeResolver.java
+++ plos/doi-resolver/src/main/java/org/plos/doi/DOITypeResolver.java
@@ -18,22 +18,26 @@
  */
 package org.plos.doi;
 
-
 import java.net.URI;
-import java.util.ArrayList;
+
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import org.topazproject.otm.FetchType;
+import org.topazproject.otm.ModelConfig;
 import org.topazproject.otm.OtmException;
-import org.topazproject.otm.RdfUtil;
+import org.topazproject.otm.Rdf;
 import org.topazproject.otm.Session;
 import org.topazproject.otm.SessionFactory;
 import org.topazproject.otm.Transaction;
+import org.topazproject.otm.annotations.Entity;
+import org.topazproject.otm.annotations.Id;
+import org.topazproject.otm.annotations.Predicate;
 import org.topazproject.otm.impl.SessionFactoryImpl;
-import org.topazproject.otm.query.Results;
 import org.topazproject.otm.stores.ItqlStore;
 
 /**
@@ -42,13 +46,20 @@ import org.topazproject.otm.stores.ItqlStore;
  * @author Pradeep Krishnan
  */
 public class DOITypeResolver {
-  private static final String MODEL = "<rmi://localhost/topazproject#filter:model=ri>";
+  /**
+   * Predicate URI for a:annotates
+   */
+  public static final String ANNOTATES = "http://www.w3.org/2000/10/annotation-ns#annotates";
+
+  /**
+   * The default GRAPH  that will be searched.
+   */
+  public static final String GRAPH = "rmi://localhost/topazproject#filter:model=ri";
   private static final Log log = LogFactory.getLog(DOITypeResolver.class);
-  private static final String QUERY = "select $t from " + MODEL + " where <${doi}> <rdf:type> $t";
-  private static final String GET_ANNOTATES_QUERY = "select $x from " + MODEL + " where <${doi}> <http://www.w3.org/2000/10/annotation-ns#annotates> $x";
 
   //
   private final SessionFactory sf;
+  private String               graph;
 
   /**
    * Creates a new DOITypeResolver object.
@@ -58,72 +69,160 @@ public class DOITypeResolver {
    * @throws OtmException if an error occurred talking to the web-service
    */
   public DOITypeResolver(URI mulgaraUri) throws OtmException {
-    sf = new SessionFactoryImpl();
+    sf                         = new SessionFactoryImpl();
     sf.setTripleStore(new ItqlStore(mulgaraUri));
+    sf.preload(Resource.class);
+    sf.preload(Annotation.class);
+    setGraph(GRAPH);
   }
+
+  /**
+   * Sets the uri for the graph where the queries are to be performed.
+   *
+   * @param graph the graph uri
+   */
+  public void setGraph(String graph) {
+    this.graph = graph;
+
+    ModelConfig mc = sf.getModel("ri");
+
+    if (mc != null)
+      sf.removeModel(mc);
+
+    sf.addModel(new ModelConfig("ri", URI.create(graph), null));
+  }
+
+  /**
+   * Gets the graph where the queries are to be performed
+   *
+   * @return the graph uri
+   */
+  public String getGraph() {
+    return graph;
+  }
+
   /**
    * Queries the ITQL database and returns all known rdf:type values of a URI.
    *
    * @param doi the doi uri
    *
-   * @return returns an array of rdf:types
+   * @return returns the set of rdf:type values
    *
    * @throws OtmException if an exception occurred talking to the service
    */
-  public Set<String> getRdfTypes(URI doi) throws OtmException {
-    String query = RdfUtil.bindValues(QUERY, "doi", doi.toString());
+  public Set<String> getRdfTypes(final URI doi) throws OtmException {
+    return doInSession(new Action<Set<String>>() {
+        public Set<String> run(Session session) throws OtmException {
+          Resource r = session.get(Resource.class, doi.toString());
 
-    List<String> types = new ArrayList<String>();
-    Session sess = sf.openSession();
-    try {
-      Transaction tx = sess.beginTransaction();
-      try {
-        Results results = sess.doNativeQuery(query);
-        results.beforeFirst();
-        while (results.next())
-          types.add(results.getString(0));
-      } finally {
-        tx.commit();
-      }
-    } finally {
-      sess.close();
-    }
+          if (r == null)
+            return Collections.emptySet();
 
-    return new HashSet<String>(types);
+          return r.types;
+        }
+      });
   }
 
   /**
-   * Recursively attempts to find the annotated root (article?) of the given annotation doi. Retruns the doi
-   * found that does not annotate another doi. 
-   * 
-   * @param annotationDoi
-   * @return
-   * @throws ItqlInterpreterException
-   * @throws RemoteException
-   * @throws AnswerException
+   * Recursively attempts to find the annotated root (article?) of the given annotation doi.
+   * Retruns the doi found that does not annotate another doi.
+   *
+   * @param doi the annotation doi
+   *
+   * @return the resource that is annotated by the annotation doi
+   *
+   * @throws OtmException
    */
-  public String getAnnotatedRoot(String annotationDoi) {
-    /* TODO - need to reimplement this without ItqlHelper... as that doesn't appear to be 
-     * supported any longer in head. 
-     * 
-    String query = ItqlHelper.bindValues(GET_ANNOTATES_QUERY, "doi", annotationDoi.toString());
-    
-    String results;
-    synchronized (this) {
-      results = itql.doQuery(query, null);
-    }
-    StringAnswer answer = new StringAnswer(results);
-    List<String[]> rows = (List<String[]>)((StringAnswer.StringQueryAnswer) (answer.getAnswers().get(0))).getRows();
-    
-    if ((rows.size() > 0) && (rows.get(0).length > 0)) {
-      String annotatedDoi = rows.get(0)[0];
-      if (annotatedDoi.equals(annotationDoi)) {
-        log.error("Circular dependency found for annotation doi: '"+annotationDoi+"'");
-        return annotatedDoi;
+  public String getAnnotatedRoot(final String doi) throws OtmException {
+    return doInSession(new Action<String>() {
+        public String run(Session session) throws OtmException {
+          String      id         = doi;
+          Annotation  a          = session.get(Annotation.class, id);
+          Set<String> loopDetect = new HashSet<String>();
+
+          while (a != null) {
+            id = a.id;
+
+            if (loopDetect.contains(id))
+              throw new OtmException("Loop detected for " + doi);
+
+            loopDetect.add(id);
+            a = a.ann;
+          }
+
+          return id;
+        }
+      });
+  }
+
+  /**
+   * Run the given action within a session.
+   *
+   * @param <T>    the return type
+   * @param action the action to run
+   *
+   * @return the value returned by the action
+   *
+   * @throws OtmException on an error
+   */
+  protected <T> T doInSession(Action<T> action) throws OtmException {
+    Session     session = sf.openSession();
+    Transaction tx      = null;
+
+    try {
+      tx = session.beginTransaction(true, 0);
+
+      T result = action.run(session);
+      tx.commit(); // Flush happens automatically
+
+      return result;
+    } catch (OtmException e) {
+      try {
+        if (tx != null)
+          tx.rollback();
+      } catch (OtmException re) {
+        log.warn("rollback failed", re);
       }
-      return getAnnotatedRoot(annotatedDoi);
+
+      throw e; // or display error message
+    } finally {
+      try {
+        session.close();
+      } catch (OtmException ce) {
+        log.warn("close failed", ce);
+      }
     }
-    */
-    return annotationDoi;
+  }
+
+/**
+   * The interface actions must implement.
+   */
+  protected static interface Action<T> {
+    /**
+     * This is run within the context of a session.
+     *
+     * @param session the current transaction
+     *
+     * @return the return value
+     *
+     * @throws OtmException on an error
+     */
+    T run(Session session) throws OtmException;
+  }
+
+  @Entity(model = "ri")
+  public static class Resource {
+    @Id
+    public String                                        id;
+    @Predicate(uri = Rdf.rdf + "type")
+    public Set<String>                                   types;
+  }
+
+  @Entity(model = "ri")
+  public static class Annotation {
+    @Id
+    public String                                                         id;
+    @Predicate(uri = ANNOTATES, fetch = FetchType.eager)
+    public Annotation                                                     ann;
   }
 }
