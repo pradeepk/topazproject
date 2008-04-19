dfb38496c54f8a3495cc463ad1534073d652c1d1
ronald <ronald@48088309-b40f-0410-b149-b14953253675>
2008-04-19 10:19:54 +0000

    Clean up formatting to conform to our coding style...
    git-svn-id: http://svn.topazproject.org/svn/head@5490 48088309-b40f-0410-b149-b14953253675

--- plos/webapp/src/main/java/org/plos/web/MultipleRequestFilter.java
+++ plos/webapp/src/main/java/org/plos/web/MultipleRequestFilter.java
@@ -39,106 +39,102 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * MultipleReuquestFilter reduced load on the server caused by impatient users clicking on links multiple times. 
- * Incoming requests for the same URL within the same session are blocked until the initial request completes. 
- * Subsequent repeat requests will cause any blocked request to return immediately, and will take it's place as 
- * the latest blocked request for that URL. Once the original un-blocked request completes, it will remove the
- * URL from the requestedURLSet and wake any block thread on that URL. Once a block thread wakes to find the URL
- * not in the set, it can complete the request. If it wakes to find the URL still in the set, it assumes it was 
- * displaced by another request and will return without performing work on the server. 
+ * MultipleReuquestFilter reduced load on the server caused by impatient users clicking on links
+ * multiple times.  Incoming requests for the same URL within the same session are blocked until the
+ * initial request completes.  Subsequent repeat requests will cause any blocked request to return
+ * immediately, and will take it's place as the latest blocked request for that URL. Once the
+ * original un-blocked request completes, it will remove the URL from the requestedURLSet and wake
+ * any block thread on that URL. Once a block thread wakes to find the URL not in the set, it can
+ * complete the request. If it wakes to find the URL still in the set, it assumes it was displaced
+ * by another request and will return without performing work on the server. 
  * 
  * @author Alex Worden
- *
  */
-public class MultipleRequestFilter implements Filter
-{
+public class MultipleRequestFilter implements Filter {
   private static final Log log = LogFactory.getLog(MultipleRequestFilter.class);
   private static final String REQUEST_MAP = "REQUEST_MAP";
   private List<Pattern> includePatterns;
 
-  public void destroy()
-  {
+  public void destroy() {
     // do nothing
   }
 
-  public void init( FilterConfig config ) throws ServletException
-  {
+  public void init(FilterConfig config) throws ServletException {
     // parse all of the initialization parameters, collecting the exclude
     // patterns and the max wait parameters
     Enumeration<String> paramNames = config.getInitParameterNames();
     includePatterns = new LinkedList<Pattern>();
-    while( paramNames.hasMoreElements() )
-    {
-      String paramName = ( String )paramNames.nextElement();
-      String paramValue = config.getInitParameter( paramName );
-      if( paramName.startsWith( "includePattern" ) )
-      {
+    while (paramNames.hasMoreElements()) {
+      String paramName = (String) paramNames.nextElement();
+      String paramValue = config.getInitParameter(paramName);
+      if (paramName.startsWith("includePattern")) {
         // compile the pattern only this once
-        Pattern excludePattern = Pattern.compile( paramValue );
-        includePatterns.add( excludePattern );
+        Pattern excludePattern = Pattern.compile(paramValue);
+        includePatterns.add(excludePattern);
       }
     }
   }
-  
-  /**
-   * 
-   */
+
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-    throws IOException, ServletException
-  {
+      throws IOException, ServletException {
     HttpServletRequest httpRequest = (HttpServletRequest)request;
     HttpSession session = httpRequest.getSession();
 
     HashSet<String> requestedUrlSet = getRequestHashSet(session);
     StringBuffer buf = new StringBuffer();
     buf.append(httpRequest.getRequestURI());
-    String queryString = httpRequest.getQueryString(); 
+    String queryString = httpRequest.getQueryString();
     if ((queryString != null) && (queryString.length() > 0)) {
       buf.append("?");
       buf.append(httpRequest.getQueryString());
     }
-    if( !isFilteredRequest( buf.toString() ) )
-    {
-      chain.doFilter( request, response );
+    if (!isFilteredRequest(buf.toString())) {
+      chain.doFilter(request, response);
       return;
     }
-    
-    buf.insert(0, session.getId()); // Note, we have to add the session id since we're going to sync on this interned key. 
+
+    // Note, we have to add the session id since we're going to sync on this interned key. 
+    buf.insert(0, session.getId());
     String urlKey = (buf.toString()).intern();
-    
-    synchronized( urlKey)
-    {
+
+    synchronized (urlKey) {
       if (requestedUrlSet.contains(urlKey)) {
         urlKey.notifyAll();
-        log.debug("Waiting on URL="+urlKey);
+
+        if (log.isDebugEnabled())
+          log.debug("Waiting on URL=" + urlKey);
         try {
           urlKey.wait(60000); // Only wait a maximum of one minute
         } catch (InterruptedException e) {
-          // continue
+          if (log.isDebugEnabled())
+            log.debug("Wait interrupted", e);
         }
-        // If we are woken up and the urlKey is not null, then we were not the latest
-        // request for this URL to be woken after the first request had completed, so
-        // just return and do nothing for this request. 
+
+        /* If we are woken up and the urlKey is not null, then we were not the latest
+         * request for this URL to be woken after the first request had completed, so
+         * just return and do nothing for this request. 
+         */
         if (requestedUrlSet.contains(urlKey)) {
-          log.debug("Blocked (or timed out) Multiple Request to '"+urlKey+"' from '"+httpRequest.getRemoteAddr()+"'");
+          if (log.isDebugEnabled())
+            log.debug("Blocked (or timed out) Multiple Request to '" + urlKey + "' from '" +
+                      httpRequest.getRemoteAddr() + "'");
           return;
         }
-        log.debug("Woke from blocking URL as last request. Processing URL="+urlKey);
+
+        if (log.isDebugEnabled())
+          log.debug("Woke from blocking URL as last request. Processing URL=" + urlKey);
       }
-      
+
       requestedUrlSet.add(urlKey);
     }
-    
-    
-    try
-    {
-      chain.doFilter( request, response );
-    }
-    finally
-    {
+
+
+    try {
+      chain.doFilter(request, response);
+    } finally {
       // Upon completion of this request, remove the urlKey from the requestHashSet, then 
       // wake any threads that are waiting on the key. 
-      synchronized(urlKey) {
+      synchronized (urlKey) {
         requestedUrlSet.remove(urlKey);
         urlKey.notifyAll();
       }
@@ -151,20 +147,18 @@ public class MultipleRequestFilter implements Filter
    * @param request
    * @return
    */
-  private boolean isFilteredRequest(String path)
-  {
+  private boolean isFilteredRequest(String path) {
     // iterate through the exclude patterns.  If one matches this path,
     // then the request is excluded.
     for (Pattern p : includePatterns) {
-      Matcher m = p.matcher( path );
-      if( m.matches() )
-      {
+      Matcher m = p.matcher(path);
+      if (m.matches()) {
         return true;
       }
     }
     return false;
   }
-  
+
   @SuppressWarnings("unchecked")
   private HashSet<String> getRequestHashSet(HttpSession session) {
     HashSet<String> map;
@@ -176,5 +170,4 @@ public class MultipleRequestFilter implements Filter
     }
     return map;
   }
-
 }
