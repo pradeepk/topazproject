c22b9e66187d74927e4c785ce59b4f2ba5ad752a
ronald <ronald@48088309-b40f-0410-b149-b14953253675>
2008-05-12 08:53:00 +0000

    Mostly minor code cleanups.
    git-svn-id: http://svn.topazproject.org/svn/head@5701 48088309-b40f-0410-b149-b14953253675

--- plos/webapp/src/main/java/org/plos/user/EnsureUserAccountInterceptor.java
+++ plos/webapp/src/main/java/org/plos/user/EnsureUserAccountInterceptor.java
@@ -19,10 +19,11 @@
 package org.plos.user;
 
 import com.opensymphony.xwork2.ActionInvocation;
-import com.opensymphony.xwork2.interceptor.Interceptor;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 import org.apache.struts2.ServletActionContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import static org.plos.Constants.PLOS_ONE_USER_KEY;
 import static org.plos.Constants.ReturnCode;
 import static org.plos.Constants.SINGLE_SIGNON_USER_KEY;
@@ -42,12 +43,14 @@ import java.util.Map;
  * Ensures that the user has a profile if the user does something which requires membership
  * Get the user object for the logged in user or redirect the user to set up his profile.
  */
-public class EnsureUserAccountInterceptor implements Interceptor {
+public class EnsureUserAccountInterceptor extends AbstractInterceptor {
   private UserService userService;
   private static final Log log = LogFactory.getLog(EnsureUserAccountInterceptor.class);
 
   public String intercept(final ActionInvocation actionInvocation) throws Exception {
-    log.debug("ensure user account interceptor called");
+    if (log.isDebugEnabled())
+      log.debug("ensure user account interceptor called");
+
     final Map<String, Object> sessionMap = getUserSessionMap();
     final String userId = (String) sessionMap.get(SINGLE_SIGNON_USER_KEY);
 
@@ -62,11 +65,10 @@ public class EnsureUserAccountInterceptor implements Interceptor {
     PlosOneUser plosUser = (PlosOneUser) sessionMap.get(PLOS_ONE_USER_KEY);
     if (null != plosUser) {
       if (log.isDebugEnabled()) {
-        log.debug ("Retrieved user from session with userId: " + plosUser.getUserId());
+        log.debug("Retrieved user from session with userId: " + plosUser.getUserId());
       }
       return getReturnCodeDependingOnDisplayName(plosUser, actionInvocation);
     } else {
-      final UserService userService = getUserService();
       plosUser = userService.getUserByAuthId(userId);
       if (log.isDebugEnabled()) {
         log.debug("UserService : " + userService + " hashcode = "  + userService.hashCode());
@@ -75,12 +77,14 @@ public class EnsureUserAccountInterceptor implements Interceptor {
 
       if (null == plosUser) {
         //forward to new profile creation page
-        log.debug("This is a new user with id: " + userId);
+        if (log.isDebugEnabled())
+          log.debug("This is a new user with id: " + userId);
         return ReturnCode.NEW_PROFILE;
       } else {
         updateUserEmailAddress(plosUser, userId);
         sessionMap.put(PLOS_ONE_USER_KEY, plosUser);
-        log.debug("Existing user detected: " + userId);
+        if (log.isDebugEnabled())
+          log.debug("Existing user detected: " + userId);
         return getReturnCodeDependingOnDisplayName(plosUser, actionInvocation);
       }
     }
@@ -100,18 +104,14 @@ public class EnsureUserAccountInterceptor implements Interceptor {
     return userService.getUserContext().getSessionMap();
   }
 
-  private UserService getUserService() {
-    return userService;
-  }
-
   public void setUserService(final UserService userService) {
     this.userService = userService;
   }
 
-  private void updateUserEmailAddress (PlosOneUser user, String authId) throws ApplicationException {
-    String emailAddress = fetchUserEmailAddress (user, authId);
+  private void updateUserEmailAddress(PlosOneUser user, String authId) throws ApplicationException {
+    String emailAddress = fetchUserEmailAddress(user, authId);
     if (emailAddress != null) {
-      if (!emailAddress.equals (user.getEmail())) {
+      if (!emailAddress.equals(user.getEmail())) {
         user.setEmail(emailAddress);
         try {
           userService.setProfile(user);
@@ -135,7 +135,7 @@ public class EnsureUserAccountInterceptor implements Interceptor {
     if (presetEmail != null)
       return presetEmail;
 
-    final String emailAddressUrl = getEmailAddressUrl();
+    final String emailAddressUrl = userService.getEmailAddressUrl();
     final String url = emailAddressUrl + authId;
     try {
       return FileUtils.getTextFromUrl(url);
@@ -145,11 +145,4 @@ public class EnsureUserAccountInterceptor implements Interceptor {
       throw new ApplicationException(errorMessage, ex);
     }
   }
-
-  private String getEmailAddressUrl() {
-    return getUserService().getEmailAddressUrl();
-  }
-
-  public void destroy() {}
-  public void init() {}
 }
