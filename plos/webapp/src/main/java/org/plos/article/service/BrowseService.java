pradeepk/topazproject

commit fc96a5c4bab031d1734ec39fb66fcb064d890602

Convert browse cache to transactional cache. Addresses #900.
git-svn-id: http://svn.topazproject.org/svn/head@5644 48088309-b40f-0410-b149-b14953253675

plos/webapp/src/main/java/org/plos/article/service/BrowseService.java
@@ -35,12 +35,10 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import net.sf.ehcache.Ehcache;
-import net.sf.ehcache.Element;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.struts2.ServletActionContext;
+import org.plos.cache.Cache;
 import org.plos.journal.JournalService;
 import org.plos.model.IssueInfo;
 import org.plos.model.VolumeInfo;
@@ -85,7 +83,7 @@ public class BrowseService {
 
   private final ArticlePEP     pep;
   private       Session        session;
-  private       Ehcache        browseCache;
+  private       Cache          browseCache;
   private       JournalService journalService;
 
 
@@ -110,17 +108,16 @@ public class BrowseService {
     String key  = DATE_LIST_KEY + jnlName;
     Object lock = (DATE_LIST_LOCK + jnlName).intern();
 
-    return
-      CacheAdminHelper.getFromCache(browseCache, key, -1, lock, "article dates",
-                                    !load ? null : new CacheAdminHelper.EhcacheUpdater<Years>() {
-        public Years lookup() {
+    return browseCache.get(key, -1, 
+                  !load ? null : new Cache.SynchronizedLookup<Years, RuntimeException> (lock) {
+        public Years lookup() throws RuntimeException {
           return loadArticleDates();
         }
       });
   }
 
   private void updateArticleDates(Years dates, String jnlName) {
-    browseCache.put(new Element(DATE_LIST_KEY + jnlName, dates));
+    browseCache.put(DATE_LIST_KEY + jnlName, dates);
   }
 
   /**
@@ -173,10 +170,9 @@ public class BrowseService {
     Object lock    = (ARTBYDATE_LIST_LOCK + mod).intern();
     int    ttl     = getSecsTillMidnight();
 
-    List<URI> uris =
-      CacheAdminHelper.getFromCache(browseCache, key, ttl, lock, "articles by date",
-                                    new CacheAdminHelper.EhcacheUpdater<List<URI>>() {
-        public List<URI> lookup() {
+    List<URI> uris = browseCache.get(key, ttl,
+                              new Cache.SynchronizedLookup<List<URI>, RuntimeException>(lock) {
+        public List<URI> lookup() throws RuntimeException {
           return loadArticlesByDate(startDate, endDate);
         }
       });
@@ -220,10 +216,9 @@ public class BrowseService {
    * @return the Issue information.
    */
   public IssueInfo getIssueInfo(final URI doi) {
-    return CacheAdminHelper.getFromCache(browseCache, ISSUE_KEY + doi, -1,
-        ISSUE_LOCK + doi, "issue " + doi,
-        new CacheAdminHelper.EhcacheUpdater<IssueInfo>() {
-          public IssueInfo lookup() {
+    return browseCache.get(ISSUE_KEY + doi, -1,
+        new Cache.SynchronizedLookup<IssueInfo, RuntimeException>((ISSUE_LOCK + doi).intern()) {
+          public IssueInfo lookup() throws RuntimeException {
             return getIssueInfo2(doi);
           }
         });
@@ -347,8 +342,8 @@ public class BrowseService {
 
     String key = (VOL_INFOS_FOR_JOURNAL_KEY + (journal.getKey())).intern();
 
-    return CacheAdminHelper.getFromCache(browseCache, key, -1, key, "List of volumes for journal" + journal.getId(),
-                                         new CacheAdminHelper.EhcacheUpdater<List<VolumeInfo>>() {
+    return browseCache.get(key, -1,
+                              new Cache.SynchronizedLookup<List<VolumeInfo>, RuntimeException>(key) {
        public List<VolumeInfo> lookup() {
          final List<URI> volumeDois = journal.getVolumes();
          List<VolumeInfo> volumeInfos = loadVolumeInfos(volumeDois);
@@ -410,29 +405,15 @@ public class BrowseService {
     return getCatInfo(key, desc, load, getCurrentJournal());
   }
 
-  private Object getCatInfo(String key, String desc, boolean load, String jnlName) {
-    key += jnlName;
-
-    synchronized ((CAT_INFO_LOCK + jnlName).intern()) {
-      Element e = browseCache.get(key);
-
-      if (e == null) {
-        if (!load) {
-          return null;
-        }
-
-        if (log.isDebugEnabled()) {
-          log.debug("retrieving " + desc + " from db");
-        }
-
-        loadCategoryInfos(jnlName);
-        e = browseCache.get(key);
-      } else if (log.isDebugEnabled()) {
-        log.debug("retrieved " + desc + " from cache");
-      }
-
-      return e.getValue();
-    }
+  private Object getCatInfo(String key, String desc, boolean load, final String jnlName) {
+    final String jnlkey = key + jnlName;
+    return browseCache.get(key, -1, !load ? null : 
+        new Cache.SynchronizedLookup<Object, RuntimeException>((CAT_INFO_LOCK + jnlName).intern()) {
+          public Object lookup() throws RuntimeException {
+             loadCategoryInfos(jnlName);
+             return  browseCache.get(jnlkey);
+          }
+      });
   }
 
   private String getCurrentJournal() {
@@ -487,7 +468,7 @@ public class BrowseService {
       // update date lists
       browseCache.remove(DATE_LIST_KEY + jnlName);
 
-      for (Object key : browseCache.getKeysNoDuplicateCheck()) {
+      for (Object key : browseCache.getKeys()) {
         if ((key instanceof String) && ((String) key).startsWith(ARTBYDATE_LIST_KEY + jnlName)) {
           browseCache.remove(key);
         }
@@ -543,7 +524,7 @@ public class BrowseService {
         updateArticleDates(dates, jnlName);
       }
 
-      for (Object key : browseCache.getKeysNoDuplicateCheck()) {
+      for (Object key : browseCache.getKeys()) {
         if ((key instanceof String) && ((String) key).startsWith(ARTBYDATE_LIST_KEY + jnlName)) {
           browseCache.remove(key);
         }
@@ -581,14 +562,14 @@ public class BrowseService {
       browseCache.remove(CAT_INFO_KEY + jnlName);
       browseCache.remove(ARTBYCAT_LIST_KEY + jnlName);
     }
-    
+
     clearVolumeInfoCacheForJournal(jnlName);
 
     synchronized ((DATE_LIST_LOCK + jnlName).intern()) {
       browseCache.remove(DATE_LIST_KEY + jnlName);
     }
 
-    for (Object key : browseCache.getKeysNoDuplicateCheck()) {
+    for (Object key : browseCache.getKeys()) {
       if ((key instanceof String) && ((String) key).startsWith(ARTBYDATE_LIST_KEY + jnlName)) {
         browseCache.remove(key);
       }
@@ -643,8 +624,8 @@ public class BrowseService {
     }
 
     // save in cache
-    browseCache.put(new Element(ARTBYCAT_LIST_KEY + jnlName, artByCat));
-    browseCache.put(new Element(CAT_INFO_KEY + jnlName, catSizes));
+    browseCache.put(ARTBYCAT_LIST_KEY + jnlName, artByCat);
+    browseCache.put(CAT_INFO_KEY + jnlName, catSizes);
   }
 
   private ArticleInfo loadArticleInfo(final URI id) {
@@ -755,10 +736,9 @@ public class BrowseService {
     }
 
     return
-      CacheAdminHelper.getFromCache(browseCache, ARTICLE_KEY + id, -1, ARTICLE_LOCK + id,
-                                    "article " + id,
-                                    new CacheAdminHelper.EhcacheUpdater<ArticleInfo>() {
-        public ArticleInfo lookup() {
+      browseCache.get(ARTICLE_KEY + id, -1,
+           new Cache.SynchronizedLookup<ArticleInfo, RuntimeException>((ARTICLE_LOCK + id).intern()) {
+        public ArticleInfo lookup() throws RuntimeException {
           return loadArticleInfo(id);
         }
       });
@@ -796,7 +776,7 @@ public class BrowseService {
    * @param browseCache The browse-cache to use.
    */
   @Required
-  public void setBrowseCache(Ehcache browseCache) {
+  public void setBrowseCache(Cache browseCache) {
     this.browseCache = browseCache;
   }
 

plos/webapp/src/main/webapp/WEB-INF/applicationContext.xml
@@ -444,6 +444,14 @@
        <ref bean="journalService"/>
     </constructor-arg>
   </bean>
+  <bean id="browseCache" class="org.plos.cache.EhcacheProvider">
+    <constructor-arg index="0">
+       <ref bean="txCacheManager"/>
+    </constructor-arg>
+    <constructor-arg index="1">
+       <ref bean="browseEhcache"/>
+    </constructor-arg>
+  </bean>
   
   <!-- freemarker configs -->
   <bean id="plosOneFreemarkerManager" class="org.plos.struts2.PlosOneFreemarkerManager">
@@ -486,7 +494,7 @@
     </property>
   </bean>
 
-  <bean id="browseCache" class="org.springframework.cache.ehcache.EhCacheFactoryBean">
+  <bean id="browseEhcache" class="org.springframework.cache.ehcache.EhCacheFactoryBean">
     <property name="cacheName" value="BrowseCache"/>
   </bean>
 

commit e53cf76c2798d693359bca90b4a35c64eb712b7c

Fix a typo introduced in r5644 in the category key that caused browse by subject cache to be broken in a multi-journal setup. Thanks Amit.
Also added a debug log that shows journal membership for an Article
as the BrowseService understands it.



git-svn-id: http://svn.topazproject.org/svn/head@5729 48088309-b40f-0410-b149-b14953253675

plos/webapp/src/main/java/org/plos/article/service/BrowseService.java
@@ -412,7 +412,7 @@ public class BrowseService {
 
   private Object getCatInfo(String key, String desc, boolean load, final String jnlName) {
     final String jnlkey = key + jnlName;
-    return browseCache.get(key, -1, !load ? null : 
+    return browseCache.get(jnlkey, -1, !load ? null : 
         new Cache.SynchronizedLookup<Object, RuntimeException>((CAT_INFO_LOCK + jnlName).intern()) {
           public Object lookup() throws RuntimeException {
              loadCategoryInfos(jnlName);
@@ -539,13 +539,18 @@ public class BrowseService {
     Map<String, Set<String>> artMap = new HashMap<String, Set<String>>();
 
     for (String uri : artUris) {
+      Set<String> jks = log.isDebugEnabled() ? new HashSet<String>() : null;
       for (Journal j : journalService.getJournalsForObject(URI.create(uri))) {
         Set<String> artList = artMap.get(j.getKey());
         if (artList == null) {
           artMap.put(j.getKey(), artList = new HashSet<String>());
         }
         artList.add(uri);
+        if (log.isDebugEnabled())
+          jks.add(j.getKey());
       }
+      if (log.isDebugEnabled())
+        log.debug("Article with id <" + uri + "> is part of journals " + jks);
     }
 
     return artMap;