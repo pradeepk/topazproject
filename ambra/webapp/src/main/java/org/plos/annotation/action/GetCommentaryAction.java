1d594f6a17c54151a3f23eeb4f674948ce6ed8b0
jkirton <jkirton@48088309-b40f-0410-b149-b14953253675>
2008-06-10 17:36:58 +0000

    fixes #950 Merged GetCommentaryAction and GetCorrectionsCommentaryAction into GetCommentaryAction replacing execute() with 2 new public metods that list the comments and corrections respectively.  The circumvents cglib's apparent inability to retain the 'target' member property when calling overridden abstract methods.  The target property was being set ok but wasn't being transferred to the cglib proxies.  Maybe this had something to do with the fact we are now intercepting methods for transactionality.  Either way, this approach better conforms to convention.
    git-svn-id: http://svn.topazproject.org/svn/head@5897 48088309-b40f-0410-b149-b14953253675

--- ambra/webapp/src/main/java/org/plos/annotation/action/GetCommentaryAction.java
+++ ambra/webapp/src/main/java/org/plos/annotation/action/GetCommentaryAction.java
@@ -1,5 +1,5 @@
-/* $$HeadURL::                                                                            $$
- * $$Id$$
+/* $$HeadURL:: http://gandalf.topazproject.org/svn/branches/0.8.2.1/plos/webapp/src/main/#$$
+ * $$Id: GetCommentaryAction.java 4629 2008-02-04 06:07:05Z alex $$
  *
  * Copyright (c) 2006-2007 by Topaz, Inc.
  * http://topazproject.org
@@ -19,30 +19,147 @@
 
 package org.plos.annotation.action;
 
+import java.util.Arrays;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.transaction.annotation.Transactional;
+
 import org.plos.ApplicationException;
+import org.plos.annotation.Commentary;
 import org.plos.annotation.service.WebAnnotation;
+import org.plos.article.service.FetchArticleService;
+import org.plos.article.service.NoSuchArticleIdException;
+import org.plos.models.Article;
+
+import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
 
 /**
- * Action class to get a list of all commentary for an article and the threads
- * associated with each base comment.
- * 
+ * Action class to get a list of all commentary for an article and the threads associated
+ * with each base comment.
+ *
  * @author Stephen Cheng
  * @author jkirton
+ * @author Alex Worden
  */
 @SuppressWarnings("serial")
-public class GetCommentaryAction extends AbstractCommentaryAction {
+public class GetCommentaryAction extends AnnotationActionSupport {
+
+  protected final Log log = LogFactory.getLog(this.getClass());
+
+  protected String target;
+  private Commentary[] commentary;
+  private Article article;
+  private FetchArticleService fetchArticleService;
 
   /**
-   * For this use case, we provide only comment (non-correction) related
-   * annotations
+   * Provides a list of comments for the target.
+   * @return Array of {@link WebAnnotation}s representing the target's comments
+   * @throws ApplicationException
    */
-  @Override
-  protected WebAnnotation[] getAnnotations() throws ApplicationException {
-    return getAnnotationService().listComments(getTarget());
+  @Transactional(readOnly = true)
+  public final String listComments() throws Exception {
+    return list(false);
   }
 
-  @Override
-  protected String useCaseDescriptor() {
-    return "all Commentary";
+  /**
+   * Provides a list of corrections for the target.
+   * @return Array of {@link WebAnnotation}s representing the target's corrections
+   * @throws ApplicationException
+   */
+  @Transactional(readOnly = true)
+  public final String listCorrections() throws Exception {
+    return list(true);
   }
+
+  /**
+   * Pulls either all corrections or all non-correction comments for the given target.
+   * @param isCorrections Pull corrections?
+   * @return status
+   * @throws Exception
+   */
+  private String list(boolean isCorrections) throws Exception {
+    try {
+      article = fetchArticleService.getArticleInfo(target);
+      WebAnnotation[] annotations = isCorrections? getAnnotationService().listCorrections(target) : getAnnotationService().listComments(target);
+      commentary = new Commentary[annotations.length];
+      Commentary com = null;
+      if (annotations.length > 0) {
+        for (int i = 0; i < annotations.length; i++) {
+          com = new Commentary();
+          com.setAnnotation(annotations[i]);
+          try {
+            getAnnotationService().listAllReplies(annotations[i].getId(), annotations[i].getId(), com);
+          } catch (ApplicationException ae) {
+            Throwable t = ae.getCause();
+            if ((t instanceof NoSuchArticleIdException) || (t instanceof java.lang.SecurityException)) {
+              // don't error if that id is gone or if you can't list the replies
+              com.setNumReplies(0);
+              com.setReplies(null);
+            } else {
+              throw ae;
+            }
+          }
+          commentary[i] = com;
+        }
+        Arrays.sort(commentary, commentary[0]);
+      }
+    } catch (final ApplicationException e) {
+      if(log.isErrorEnabled()) log.error("Could not get " + (isCorrections ? "corrections" : "comments") + " for articleID: " + target, e);
+      addActionError("Annotation fetching failed with error message: " + e.getMessage());
+      return ERROR;
+    }
+    return SUCCESS;
+  }
+
+  @Transactional(readOnly = true)
+  public final String getArticleMetaInfo () throws Exception {
+    article = fetchArticleService.getArticleInfo(target);
+    return SUCCESS;
+  }
+
+  /**
+   * Set the target that it annotates.
+   * @param target target
+   */
+  public final void setTarget(final String target) {
+    this.target = target;
+  }
+
+  /**
+   * @return the target of the annotation
+   */
+  @RequiredStringValidator(message="You must specify the target that you want to list the annotations for")
+  public final String getTarget() {
+    return target;
+  }
+
+  /**
+   * @param articleOtmService The articleOtmService to set.
+   */
+  public final void setFetchArticleService(FetchArticleService service) {
+    this.fetchArticleService = service;
+  }
+
+  /**
+   * @return Returns the articleInfo.
+   */
+  public final Article getArticleInfo() {
+    return article;
+  }
+
+  /**
+   * @param articleInfo The articleInfo to set.
+   */
+  public final void setArticleInfo(Article articleInfo) {
+    this.article = articleInfo;
+  }
+
+  /**
+   * @return The commentary array
+   */
+  public final Commentary[] getCommentary() {
+    return commentary;
+  }
+
 }
