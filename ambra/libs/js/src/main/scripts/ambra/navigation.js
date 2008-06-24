pradeepk/topazproject

commit 8c875e0ce5dc2b542450211286692f65236c607b

fixes #734
Centralized the handling of TOC building in navigation.js merging in functionality from init_navigation.js.  init_navigation.js is now entirely removed from the app.

navigation.js will check the dom for certain nodes of prescribed names upon page load.  If they are present, the TOC is built.  Note the Floating menu (floatMenu.js) works in the same manner and is centralized as well.

git-svn-id: http://svn.topazproject.org/svn/head@6026 48088309-b40f-0410-b149-b14953253675

ambra/libs/js/src/main/scripts/ambra/navigation.js
@@ -26,11 +26,14 @@
 dojo.provide("ambra.navigation");
 dojo.require("ambra.general");
 ambra.navigation = {
- buildTOC: function(tocObj){
+ buildTOC: function(tocParentNode){
+   var ul = dojo.byId('tocUl');
+   if(ul) return; // presume we already have the list built
    var tocEl = document.getElementsByTagAndAttributeName(null, 'toc');
-   if(!tocObj || !tocEl) return;
+   if(!tocParentNode || !tocEl) return;
    
    var ul = document.createElement('ul');
+   ul.setAttribute('id','tocUl');
    
    for (var i=0; i<tocEl.length; i++) {
      var li = document.createElement('li');
@@ -46,6 +49,9 @@ ambra.navigation = {
      ul.appendChild(li);
    }
    
-   tocObj.appendChild(ul);
- } 
-}  
+   tocParentNode.appendChild(ul);
+ }
+}
+dojo.addOnLoad(function() {
+  ambra.navigation.buildTOC(dojo.byId('sectionNavTop'));
+});

ambra/journals/journal-clinicalTrials/src/main/resources/ambra/configuration/defaults.xml
@@ -74,7 +74,6 @@
             <file>/javascript/dojo/dojo/dojo.js</file>
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
           </javascript>
         </page>
         <page>

ambra/journals/journal-compbiol/src/main/resources/ambra/configuration/defaults.xml
@@ -74,7 +74,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_home.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_home.js</file>
           </javascript>
         </page>

ambra/journals/journal-genetics/src/main/resources/ambra/configuration/defaults.xml
@@ -74,7 +74,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_home.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_home.js</file>
           </javascript>
         </page>

ambra/journals/journal-ntd/src/main/resources/ambra/configuration/defaults.xml
@@ -74,7 +74,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_home.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_home.js</file>
           </javascript>
         </page>

ambra/journals/journal-overlay/src/main/resources/ambra/configuration/defaults.xml
@@ -66,7 +66,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_home.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_home.js</file>
           </javascript>
         </page>

ambra/journals/journal-pathogens/src/main/resources/ambra/configuration/defaults.xml
@@ -73,7 +73,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_home.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_home.js</file>
           </javascript>
         </page>

ambra/journals/journal-plosone/src/main/resources/ambra/configuration/defaults.xml
@@ -67,7 +67,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_default.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
           </javascript>
         </default>
         <page>
@@ -83,7 +82,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_home.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
           </javascript>
         </page>
         <page>
@@ -100,7 +98,6 @@
             <file>/javascript/dojo/dojo/dojo.js</file>
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_article.js</file>
           </javascript>
         </page>
@@ -114,7 +111,6 @@
             <file>/javascript/dojo/dojo/dojo.js</file>
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
           </javascript>
         </page>
         <page>
@@ -129,7 +125,6 @@
             <file>/javascript/dojo/dojo/dojo.js</file>
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
           </javascript>
         </page>
         <page>
@@ -139,7 +134,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_discussion.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_discussion.js</file>
           </javascript>
         </page>
@@ -150,7 +144,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_discussion.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_newDiscussion.js</file>
           </javascript>
         </page>
@@ -168,7 +161,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/config_discussion.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_discussion.js</file>
           </javascript>
         </page>
@@ -249,7 +241,6 @@
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/advsearch.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
           </javascript>
         </page>
         <page>
@@ -264,7 +255,6 @@
             <file>/javascript/dojo/dojo/dojo.js</file>
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
           </javascript>
         </page>
         <page>
@@ -273,7 +263,6 @@
             <file>/javascript/dojo/dojo/dojo.js</file>
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_newProfile.js</file>
           </javascript>
         </page>
@@ -289,7 +278,6 @@
             <file>/javascript/dojo/dojo/dojo.js</file>
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/init_ambraAlerts.js</file>
           </javascript>
         </page>
@@ -305,7 +293,6 @@
             <file>/javascript/dojo/dojo/dojo.js</file>
             <file>/javascript/dojo/dojo/ambra.js</file>
             <file>/javascript/init_global.js</file>
-            <file>/javascript/init_navigation.js</file>
             <file>/javascript/user_profile.js</file>
           </javascript>
         </page>

commit 99e001cb770045da066130900778e07d79556425

Restored navigation menu functionality for IE which was inadvertantly broken with r6026 since IE needs the help of js for CSS driven menus to work.
git-svn-id: http://svn.topazproject.org/svn/head@6051 48088309-b40f-0410-b149-b14953253675

ambra/libs/js/src/main/scripts/ambra/navigation.js
@@ -54,4 +54,20 @@ ambra.navigation = {
 }
 dojo.addOnLoad(function() {
   ambra.navigation.buildTOC(dojo.byId('sectionNavTop'));
+  if(dojo.isIE) {
+    var navContainer = dojo.byId("nav");
+    if(!navContainer) return;
+    for (var i=0; i<navContainer.childNodes.length; i++) {
+      if (navContainer.childNodes[i].nodeName == "LI") {
+        var navLi = navContainer.childNodes[i];
+        navLi.onmouseover = function() {
+         this.className = this.className.concat(" over");
+        }
+        navLi.onmouseout = function() {
+          this.className = this.className.replace(/\sover/, "");
+          this.className = this.className.replace(/over/, "");
+        }
+      }
+    }
+  }
 });