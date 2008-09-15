pradeepk/topazproject

commit bc960dfb1f662c6b416af2bea90ca76ae628f268

Added reference to @Predicate annotation. Closes #989.
Note that there is a bit of ugliness in 'tri-stating'
the boolean attributes in @Predicate annotation. If
this is too much of a pain, override of reference
can be excluded for boolean attributes.

Also removed the hack in #6189, and introduced another
(*sigh*) for rdfs:range comparison. This will only
be fixed after the 2-pass configuration support in
SessionFactory to resolve forward references.


git-svn-id: http://svn.topazproject.org/svn/head@6410 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/java/org/topazproject/otm/metadata/RdfDefinition.java
@@ -36,6 +36,10 @@ import org.topazproject.otm.id.IdentifierGenerator;
  * @author Pradeep Krishnan
  */
 public class RdfDefinition extends PropertyDefinition {
+  /**
+   * To specify un-typed literal fields in configuration only.
+   */
+  public static final String UNTYPED = "__untyped__";
   private String              uri;
   private Boolean             inverse;
   private Boolean             objectProperty;
@@ -72,6 +76,17 @@ public class RdfDefinition extends PropertyDefinition {
                        IdentifierGenerator generator, CascadeType[] cascade, FetchType fetchType,
                        String associatedEntity, Boolean objectProperty) {
     super(name, reference);
+
+    // Normalize undefines
+    if (colType == CollectionType.UNDEFINED)
+      colType = (reference == null) ? CollectionType.PREDICATE : null;
+
+    if ((cascade != null) && (cascade.length == 1) && (cascade[0] == CascadeType.undefined))
+      cascade = (reference == null) ? new CascadeType[] { CascadeType.peer } : null;
+
+    if (fetchType == FetchType.undefined)
+      fetchType = (reference == null) ? FetchType.lazy : null;
+
     this.uri                = uri;
     this.dataType           = dataType;
     this.inverse            = inverse;
@@ -140,6 +155,12 @@ public class RdfDefinition extends PropertyDefinition {
       fetchType          = (fetchType == null) ? ref.fetchType : fetchType;
     }
 
+    if (objectProperty == null)
+      objectProperty = (associatedEntity != null) || inverse;
+
+    if (objectProperty)
+      dataType = null;
+
     // validate stuff
 
     /* Disabled till we split the resolve() as a second pass
@@ -170,8 +191,9 @@ public class RdfDefinition extends PropertyDefinition {
    */
   public boolean refersSameRange(RdfDefinition o) {
     return same(dataType, o.dataType) && same(colType, o.colType)
-            && same(objectProperty, o.objectProperty)
-            && same(associatedEntity, o.associatedEntity);
+            && same(objectProperty, o.objectProperty);
+     // TODO: Add equivalence comparison on associated entities rather than name compare
+     //       && same(associatedEntity, o.associatedEntity);
   }
 
   private boolean same(Object o1, Object o2) {

topaz/core/src/test/java/org/topazproject/otm/metadata/RdfDefTest.java
@@ -1,0 +1,155 @@
+/* $HeadURL::                                                                            $
+ * $Id$
+ *
+ * Copyright (c) 2007-2008 by Topaz, Inc.
+ * http://topazproject.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.topazproject.otm.metadata;
+
+import java.net.URI;
+import java.util.Collections;
+
+import junit.framework.TestCase;
+
+import org.topazproject.otm.CollectionType;
+import org.topazproject.otm.FetchType;
+import org.topazproject.otm.CascadeType;
+import org.topazproject.otm.OtmException;
+import org.topazproject.otm.SessionFactory;
+import org.topazproject.otm.impl.SessionFactoryImpl;
+import org.topazproject.otm.annotations.Entity;
+import org.topazproject.otm.annotations.Id;
+import org.topazproject.otm.annotations.Predicate;
+import org.topazproject.otm.annotations.UriPrefix;
+
+public class RdfDefTest extends TestCase {
+  private SessionFactory sf = new SessionFactoryImpl();
+  private RdfDefinition ap1,ap2,ap3,bp1,bp2,bp3;
+
+  public void setUp() throws OtmException {
+    sf.preload(A.class);
+    sf.preload(B.class);
+    sf.preload(Assoc2.class);
+
+    ap1 = (RdfDefinition)sf.getDefinition("A:p1");
+    ap2 = (RdfDefinition)sf.getDefinition("A:p2");
+    ap3 = (RdfDefinition)sf.getDefinition("A:p3");
+    bp1 = (RdfDefinition)sf.getDefinition("B:p1");
+    bp2 = (RdfDefinition)sf.getDefinition("B:p2");
+    bp3 = (RdfDefinition)sf.getDefinition("B:p3");
+
+    bp1.resolveReference(sf);
+    bp2.resolveReference(sf);
+    bp3.resolveReference(sf);
+  }
+
+  private void compare(RdfDefinition def, Object[] vals) {
+    String message = "Testing '" + def.getName() + "': ";
+    assertEquals(message, vals[0], def.getUri());
+    assertEquals(message, vals[1], def.isEntityOwned());
+    assertEquals(message, vals[2], def.getModel());
+    assertEquals(message, vals[3], def.hasInverseUri());
+    assertEquals(message, vals[4], def.typeIsUri());
+    assertEquals(message, vals[5], def.getDataType());
+    assertEquals(message, vals[6], def.getAssociatedEntity());
+    assertEquals(message, vals[7], def.getColType());
+    assertEquals(message, vals[8], def.getFetchType());
+    assertEquals(message, vals[9], def.getCascade());
+    assertEquals(message, vals[10], def.isAssociation());
+  }
+
+  public void test01() {
+    Object[] vals = new Object[] {"a:p1", true, "test", false, false, null, null,
+                               CollectionType.PREDICATE, FetchType.lazy,
+                               Collections.singleton(CascadeType.peer), false};
+
+    compare(ap1, vals);
+    compare(bp1, vals);
+
+    assertTrue(ap1.refersSameGraphNodes(bp1));
+    assertTrue(ap1.refersSameRange(bp1));
+  }
+
+  public void test02() {
+    Object[] vals = new Object[] {"a:p2", true, "test", false, true, null, null,
+                               CollectionType.PREDICATE, FetchType.lazy,
+                               Collections.singleton(CascadeType.peer), false};
+
+    compare(ap2, vals);
+    vals[6] = "Assoc1";
+    vals[10] = true;
+    compare(bp2, vals);
+
+    assertTrue(ap2.refersSameGraphNodes(bp2));
+    // FIXME assertFalse(ap2.refersSameRange(bp2));
+  }
+
+  public void test03() {
+    Object[] vals = new Object[] {"a:p3", true, "test", false, true, null, "Assoc1",
+                               CollectionType.PREDICATE, FetchType.lazy,
+                               Collections.singleton(CascadeType.peer), true};
+
+    compare(ap3, vals);
+    vals[6] = "Assoc2";
+    compare(bp3, vals);
+
+    assertTrue(ap3.refersSameGraphNodes(bp3));
+    // FIXME assertFalse(ap3.refersSameRange(bp3));
+  }
+
+  @Entity(model="test", name="Base")
+  public static class Base {
+    public String getId() {return null;}
+    @Id
+    public void setId(String s) {}
+  }
+
+  @UriPrefix("a:")
+  @Entity(name="A")
+  public static class A extends Base {
+    public String getP1() {return null;}
+    @Predicate(model="test")
+    public void setP1(String s) {}
+    public URI getP2() {return null;}
+    @Predicate(model="test")
+    public void setP2(URI u) {}
+    public Assoc1 getP3() {return null;}
+    @Predicate(model="test")
+    public void setP3(Assoc1 a) {}
+  }
+
+  @UriPrefix("b:")
+  @Entity(name="B")
+  public static class B extends Base {
+    public String getP1() {return null;}
+    @Predicate(ref="A:p1")
+    public void setP1(String s) {}
+    public Assoc1 getP2() {return null;}
+    @Predicate(ref="A:p2")
+    public void setP2(Assoc1 u) {}
+    public Assoc2 getP3() {return null;}
+    @Predicate(ref="A:p3")
+    public void setP3(Assoc2 a) {}
+  }
+
+  @Entity(type="t:a1", name="Assoc1")
+  public static class Assoc1 extends Base {
+  }
+
+  @Entity(type="t:a2", name="Assoc2")
+  public static class Assoc2 extends Assoc1 {
+  }
+}

ambra/libs/otm-models/src/main/java/org/topazproject/ambra/models/ReplyThread.java
@@ -60,7 +60,8 @@ public class ReplyThread extends Reply {
    *
    * @param replies the thread of replies
    */
-  @Predicate(uri="r:inReplyTo", inverse=true, notOwned=true, cascade = { CascadeType.child })
+  @Predicate(uri="r:inReplyTo", inverse=Predicate.BT.TRUE, notOwned=Predicate.BT.TRUE, 
+             cascade = { CascadeType.child })
   public void setReplies(List<ReplyThread> replies) {
     this.replies = replies;
   }

ambra/libs/otm-models/src/main/java/org/topazproject/ambra/models/Representation.java
@@ -97,7 +97,8 @@ public class Representation extends Blob implements PostLoadEventListener, PreIn
    *
    * @param object the object
    */
-  @Predicate(uri = "topaz:hasRepresentation", inverse=true, notOwned=true)
+  @Predicate(uri = "topaz:hasRepresentation", inverse=Predicate.BT.TRUE, 
+             notOwned=Predicate.BT.TRUE)
   public void setObject(ObjectInfo object) {
     this.object = object;
     modified = true;

ambra/libs/otm-models/src/main/java/org/topazproject/ambra/models/UserAccount.java
@@ -169,7 +169,7 @@ public class UserAccount implements Serializable {
    *
    * @param profile the profile.
    */
-  @Predicate(uri = "foaf:holdsAccount", inverse = true, model="profiles",
+  @Predicate(uri = "foaf:holdsAccount", inverse = Predicate.BT.TRUE, model="profiles",
              cascade = {CascadeType.child})
   public void setProfile(UserProfile profile) {
     this.profile = profile;

ambra/webapp/src/main/java/org/topazproject/ambra/cache/OtmInterceptor.java
@@ -32,8 +32,6 @@ import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-
-
 import org.topazproject.ambra.journal.JournalService;
 import org.topazproject.ambra.models.Journal;
 import org.topazproject.otm.ClassMetadata;
@@ -45,6 +43,7 @@ import org.topazproject.otm.mapping.BlobMapper;
 import org.topazproject.otm.mapping.Mapper;
 import org.topazproject.otm.mapping.RdfMapper;
 import org.topazproject.otm.metadata.Definition;
+import org.topazproject.otm.metadata.RdfDefinition;
 
 /**
  * An interceptor that listens to changes from OTM and maintains an object cache  that acts as
@@ -59,13 +58,6 @@ public class OtmInterceptor implements Interceptor {
   private final Cache          objCache;
   private final Cache          repCache;
   private final JournalService journalService;
-
-  // FIXME: remove once reference annotations are added in OTM
-  private static final Map<String, String> aliases = new HashMap<String, String>();
-  static {
-    aliases.put("CitationInfo:authors", "Citation:authors");
-    aliases.put("UserProfileInfo:realName", "FoafPerson:realName");
-  }
 
   /**
    * Creates a new OtmInterceptor object.
@@ -421,10 +413,16 @@ public class OtmInterceptor implements Interceptor {
     }
 
     private String getName(Definition def, Session sess) {
-      // FIXME: remove once reference annotations are added in OTM
-      String name = def.getName();
-      String alias = aliases.get(name);
-      return (alias != null) ? alias : name;
+      if ((def.getReference() == null) || !(def instanceof RdfDefinition))
+        return def.getName();
+
+      RdfDefinition ref = (RdfDefinition)sess.getSessionFactory().getDefinition(def.getReference());
+
+      if (((RdfDefinition)def).refersSameGraphNodes(ref) 
+          && ((RdfDefinition)def).refersSameRange(ref))
+        return getName(ref, sess);
+
+      return def.getName();
     }
 
     public String toString() {

ambra/webapp/src/main/java/org/topazproject/ambra/model/UserProfileInfo.java
@@ -68,7 +68,7 @@ public class UserProfileInfo implements Serializable {
    *
    * @param realName the value to set.
    */
-  @Predicate(uri = "foaf:name")
+  @Predicate(ref="FoafPerson:realName")
   public void setRealName(String realName) {
     this.realName = realName;
   }

ambra/webapp/src/main/java/org/topazproject/ambra/model/article/CitationInfo.java
@@ -61,7 +61,7 @@ public class CitationInfo implements Serializable {
     return authors;
   }
 
-  @Predicate(uri = "plos:hasAuthorList", collectionType = CollectionType.RDFSEQ)
+  @Predicate(ref = "Citation:authors")
   public void setAuthors(List<UserProfileInfo> authors) {
     this.authors = authors;
   }

commit b82b2bbc1bec2c0ed5dd6139a220aa5982795f8e

Fix data-type for explicitly configured UNTYPED literal. This error was introduced in r6410.
git-svn-id: http://svn.topazproject.org/svn/head@6420 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/java/org/topazproject/otm/metadata/RdfDefinition.java
@@ -162,6 +162,9 @@ public class RdfDefinition extends PropertyDefinition {
     if (objectProperty)
       dataType = null;
 
+    if (UNTYPED.equals(dataType))
+      dataType = null;
+
     // validate stuff
 
     /* Disabled till we split the resolve() as a second pass

topaz/core/src/test/java/org/topazproject/otm/metadata/RdfDefTest.java
@@ -125,7 +125,7 @@ public class RdfDefTest extends TestCase {
   @Entity(name="A")
   public static class A extends Base {
     public String getP1() {return null;}
-    @Predicate(model="test")
+    @Predicate(model="test", dataType=Predicate.UNTYPED)
     public void setP1(String s) {}
     public URI getP2() {return null;}
     @Predicate(model="test")