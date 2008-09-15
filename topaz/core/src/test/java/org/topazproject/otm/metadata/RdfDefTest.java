pradeepk/topazproject

commit b9f1027f708863fc3d0056bb7c29b219657fcd62

Handle covariant return types in annotation processing. Closes #1028.
Definitions now can be superseded by other definitions.
eg. in ClassMetadata the superseded definitions
are suppressed so that there aren't any duplicate
properties (ie. properties with the same name, uri, etc.).


git-svn-id: http://svn.topazproject.org/svn/head@6411 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/java/org/topazproject/otm/metadata/AnnotationClassMetaFactory.java
@@ -18,8 +18,6 @@
  */
 package org.topazproject.otm.metadata;
 
-import java.beans.BeanInfo;
-import java.beans.IndexedPropertyDescriptor;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
@@ -34,6 +32,7 @@ import java.net.URI;
 import java.net.URL;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -185,67 +184,35 @@ public class AnnotationClassMetaFactory {
     ClassBindings bin = sf.getClassBindings(def.getName());
     bin.bind(EntityMode.POJO, new ClassBinder(clazz));
 
-    Set<Method> annotated = new HashSet<Method>();
-    String      ours      = Id.class.getPackage().getName();
+    String                  ours       = Id.class.getPackage().getName();
+
+    Set<PropertyDescriptor> properties = new HashSet<PropertyDescriptor>();
 
     for (Method method : clazz.getDeclaredMethods()) {
       // We only care about annotated methods. Ignore everything else.
       for (Annotation a : method.getAnnotations()) {
         if (a.annotationType().getPackage().getName().equals(ours)) {
-          annotated.add(method);
+          PropertyDescriptor property = toProperty(method);
+
+          if (property == null)
+            throw new OtmException("'" + method.toGenericString()
+                                   + "' is not a valid getter or setter");
+
+          validate(property, def, method);
+          properties.add(property);
 
           break;
         }
       }
     }
 
-    BeanInfo                beanInfo;
-    Set<PropertyDescriptor> properties = new HashSet<PropertyDescriptor>();
+    Map<String, String> supersedes = new HashMap<String, String>(); // localName --> superseded def
 
-    try {
-      beanInfo = Introspector.getBeanInfo(clazz);
-    } catch (IntrospectionException e) {
-      throw new OtmException("Failed to introspect " + clazz, e);
-    }
-
-    for (PropertyDescriptor property : beanInfo.getPropertyDescriptors()) {
-      if (property instanceof IndexedPropertyDescriptor)
-        continue;
-
-      Method setter = property.getWriteMethod();
-      Method getter = property.getReadMethod();
-
-      if (!annotated.contains(setter) && !annotated.contains(getter))
-        continue;
-
-      if (setter == null)
-        throw new OtmException("Missing setter for property '" + property.getName() + "' in "
-                               + clazz);
-
-      if ((getter == null) && !(def instanceof ViewDefinition))
-        throw new OtmException("Missing getter for property '" + property.getName() + "' in "
-                               + clazz);
-
-      if (Modifier.isFinal(setter.getModifiers()))
-        throw new OtmException("Setter can't be 'final' for  '" + property.getName() + "' in "
-                               + clazz);
-
-      if ((getter != null) && Modifier.isFinal(setter.getModifiers()))
-        throw new OtmException("Getter can't be 'final' for  '" + property.getName() + "' in "
-                               + clazz);
-
-      annotated.remove(getter);
-      annotated.remove(setter);
-      properties.add(property);
-    }
-
-    if (annotated.size() > 0)
-      throw new OtmException("Following annotated methods are not valid getters or setters: "
-                             + annotated);
+    if (def instanceof EntityDefinition)
+      buildSupersedes((EntityDefinition) def, supersedes);
 
     for (PropertyDescriptor property : properties) {
-      PropertyInfo       fi = new PropertyInfo(def, property);
-
+      PropertyInfo       fi = new PropertyInfo(def, property, supersedes.get(property.getName()));
       PropertyDefinition d  = fi.getDefinition(sf, uriPrefix);
 
       if (d == null) {
@@ -266,6 +233,79 @@ public class AnnotationClassMetaFactory {
     }
 
     return def.buildClassMetadata(sf);
+  }
+
+  private static PropertyDescriptor toProperty(Method m)
+                                        throws OtmException {
+    String propName = getPropertyName(m);
+
+    if ((propName == null) || Modifier.isStatic(m.getModifiers()))
+      return null;
+
+    try {
+      return new PropertyDescriptor(propName, m.getDeclaringClass());
+    } catch (IntrospectionException e) {
+      throw new OtmException("Failed to create a PropertyDescriptor for '" + propName + "'", e);
+    }
+  }
+
+  private static String getPropertyName(Method m) {
+    if (m.getName().startsWith("set") && (m.getParameterTypes().length == 1))
+      return Introspector.decapitalize(m.getName().substring(3));
+
+    if (m.getName().startsWith("get") && (m.getParameterTypes().length == 0))
+      return Introspector.decapitalize(m.getName().substring(3));
+
+    if (m.getName().startsWith("is") && (m.getParameterTypes().length == 0))
+      return Introspector.decapitalize(m.getName().substring(2));
+
+    return null;
+  }
+
+  private static void validate(PropertyDescriptor property, ClassDefinition def, Method m)
+                        throws OtmException {
+    Method setter = property.getWriteMethod();
+    Method getter = property.getReadMethod();
+
+    if (!m.equals(setter) && !m.equals(getter))
+      throw new OtmException("'" + m.toGenericString() + "' is not a getter or setter for '"
+                             + property.getName() + "'");
+
+    if (setter == null)
+      throw new OtmException("Missing setter for property '" + property.getName() + "' in "
+                             + m.getDeclaringClass());
+
+    if ((getter == null) && !(def instanceof ViewDefinition))
+      throw new OtmException("Missing getter for property '" + property.getName() + "' in "
+                             + m.getDeclaringClass());
+
+    if (Modifier.isFinal(setter.getModifiers()))
+      throw new OtmException("Setter can't be 'final' for  '" + property.getName() + "' in "
+                             + m.getDeclaringClass());
+
+    if ((getter != null) && Modifier.isFinal(setter.getModifiers()))
+      throw new OtmException("Getter can't be 'final' for  '" + property.getName() + "' in "
+                             + m.getDeclaringClass());
+  }
+
+  private void buildSupersedes(EntityDefinition def, Map<String, String> supersedes) {
+    if (def.getSuper() != null) {
+      EntityDefinition sdef = (EntityDefinition) sf.getDefinition(def.getSuper());
+
+      if (sdef != null)
+        buildSupersedes(sdef, supersedes);
+    }
+
+    ClassBindings b = sf.getClassBindings(def.getName());
+
+    if (b != null) {
+      for (String prop : b.getProperties()) {
+        PropertyDefinition pd = (PropertyDefinition) sf.getDefinition(prop);
+
+        if ((pd != null) && def.getName().equals(pd.getNamespace()))
+          supersedes.put(pd.getLocalName(), prop); // overwrite super-class
+      }
+    }
   }
 
   private static String getName(Class<?> clazz) {
@@ -309,17 +349,19 @@ public class AnnotationClassMetaFactory {
     final Class<?>           type;
     final boolean            isArray;
     final boolean            isCollection;
+    final String             supersedes;
 
-    public PropertyInfo(ClassDefinition cd, PropertyDescriptor property)
+    public PropertyInfo(ClassDefinition cd, PropertyDescriptor property, String supersedes)
                  throws OtmException {
-      this.cd         = cd;
-      this.property   = property;
-      this.name       = cd.getName() + ":" + property.getName();
+      this.cd           = cd;
+      this.property     = property;
+      this.name         = cd.getName() + ":" + property.getName();
+      this.supersedes   = supersedes;
 
-      Class<?> ptype  = property.getPropertyType();
+      Class<?> ptype    = property.getPropertyType();
 
-      isArray         = ptype.isArray();
-      isCollection    = Collection.class.isAssignableFrom(ptype);
+      isArray           = ptype.isArray();
+      isCollection      = Collection.class.isAssignableFrom(ptype);
 
       if (isArray)
         type = ptype.getComponentType();
@@ -458,6 +500,10 @@ public class AnnotationClassMetaFactory {
                                           IdentifierGenerator generator)
                                    throws OtmException {
       String ref = ((rdf != null) && !"".equals(rdf.ref())) ? rdf.ref() : null;
+
+      if (ref == null)
+        ref = supersedes;
+
       String uri =
         ((rdf != null) && !"".equals(rdf.uri())) ? sf.expandAlias(rdf.uri())
         : (((ref == null) && (ns != null)) ? (ns + property.getName()) : null);
@@ -518,8 +564,8 @@ public class AnnotationClassMetaFactory {
       if (ft == FetchType.undefined)
         ft = (ref == null) ? FetchType.lazy : null;
 
-      return new RdfDefinition(getName(), ref, uri, dt, inverse, model, mt, owned, generator, ct,
-                               ft, assoc, objectProperty);
+      return new RdfDefinition(getName(), ref, supersedes, uri, dt, inverse, model, mt, owned,
+                               generator, ct, ft, assoc, objectProperty);
     }
 
     private Boolean getBooleanProperty(Predicate.BT raw, String ref, Boolean dflt) {

topaz/core/src/main/groovy/org/topazproject/otm/metadata/FieldDef.groovy
@@ -209,7 +209,7 @@ public class FieldDef {
       if (isId)
         m = [new IdMapperImpl(new IdDefinition(dn, idGen), [(EntityMode.POJO) : l])];
       else
-        m = [new RdfMapperImpl(new RdfDefinition(dn, null, pred, dtype, inverse, model, mt, owned, idGen,
+        m = [new RdfMapperImpl(new RdfDefinition(dn, null, null, pred, dtype, inverse, model, mt, owned, idGen,
 ct, ft, cm?.getName(), (ser == null) || inverse || "OBJECT".equals(propType)), [(EntityMode.POJO) : l])]
     } else {
       String     collType = colType ? colType : rdf.defColType
@@ -224,7 +224,7 @@ ct, ft, cm?.getName(), (ser == null) || inverse || "OBJECT".equals(propType)), [
       else
         l = new CollectionFieldBinder(get, set, ser, compType);
 
-      m = [new RdfMapperImpl(new RdfDefinition(dn, null, pred, dtype, inverse, model, mt, owned,
+      m = [new RdfMapperImpl(new RdfDefinition(dn, null, null, pred, dtype, inverse, model, mt, owned,
                              idGen, ct, ft, cm?.getName(),
                   (ser == null) || inverse || "OBJECT".equals(propType)), [(EntityMode.POJO) : l])]
     }

topaz/core/src/main/java/org/topazproject/otm/metadata/Definition.java
@@ -40,6 +40,7 @@ public class Definition {
   private final String ns;
   private final String specific;
   private final String reference;
+  private final String supersedes;
   private State        state = State.UNRESOLVED;
 
   /**
@@ -48,7 +49,7 @@ public class Definition {
    * @param name   The unique name of this definition.
    */
   public Definition(String name) {
-    this(name, null);
+    this(name, null, null);
   }
 
   /**
@@ -56,10 +57,12 @@ public class Definition {
    *
    * @param name   The unique name of this definition.
    * @param reference The definition to refer to resolve undefined attribiutes or null.
+   * @param supersedes The definition that this supersedes or null.
    */
-  public Definition(String name, String reference) {
+  public Definition(String name, String reference, String supersedes) {
     this.name        = name;
     this.reference   = reference;
+    this.supersedes  = supersedes;
 
     String[] sp      = name.split(":", 2);
     specific         = sp[sp.length - 1];
@@ -100,6 +103,15 @@ public class Definition {
    */
   public String getReference() {
     return reference;
+  }
+
+  /**
+   * Gets the superseded definition.
+   *
+   * @return the definition this supersedes or null
+   */
+  public String getSupersedes() {
+    return supersedes;
   }
 
   /**

topaz/core/src/main/java/org/topazproject/otm/metadata/EntityDefinition.java
@@ -142,9 +142,17 @@ public class EntityDefinition extends ClassDefinition {
 
       def.resolveReference(sf);
 
-      if (def instanceof RdfDefinition)
+      if (def instanceof RdfDefinition) {
+        if (def.getSupersedes() != null) {
+          for (RdfMapper m : fields) {
+            if (def.getSupersedes().equals(m.getDefinition().getName())) {
+              fields.remove(m);
+              break;
+            }
+          }
+        }
         fields.add(new RdfMapperImpl((RdfDefinition) def, binders));
-      else if (def instanceof IdDefinition) {
+      } else if (def instanceof IdDefinition) {
         if (idField != null)
           throw new OtmException("Duplicate Id field " + def.getName() + " in " + ref.getName());
 

topaz/core/src/main/java/org/topazproject/otm/metadata/PropertyDefinition.java
@@ -30,7 +30,7 @@ public class PropertyDefinition extends Definition {
    * @param name   The name of this definition.
    */
   public PropertyDefinition(String name) {
-    this(name, null);
+    this(name, null, null);
   }
 
   /**
@@ -38,8 +38,9 @@ public class PropertyDefinition extends Definition {
    *
    * @param name   The name of this definition.
    * @param reference The definition to refer to resolve undefined attribiutes or null.
+   * @param supersedes The definition that this supersedes or null.
    */
-  public PropertyDefinition(String name, String reference) {
-    super(name, reference);
+  public PropertyDefinition(String name, String reference, String supersedes) {
+    super(name, reference, supersedes);
   }
 }

topaz/core/src/main/java/org/topazproject/otm/metadata/RdfDefinition.java
@@ -58,6 +58,7 @@ public class RdfDefinition extends PropertyDefinition {
    *
    * @param name   The name of this definition.
    * @param reference The definition to refer to resolve undefined attribiutes or null.
+   * @param supersedes The definition that this supersedes or null.
    * @param uri the rdf predicate. Null to resolve from reference
    * @param dataType of literals or null for un-typed or to resolve from reference
    * @param inverse if this field is persisted with an inverse predicate. Null to resolve from reference
@@ -71,11 +72,11 @@ public class RdfDefinition extends PropertyDefinition {
    * @param associatedEntity the entity name for associations. Null to resolve from reference.
    * @param objectProperty if this is an object property. Null to resolve from reference.
    */
-  public RdfDefinition(String name, String reference, String uri, String dataType, Boolean inverse,
-                       String model, CollectionType colType, Boolean entityOwned,
-                       IdentifierGenerator generator, CascadeType[] cascade, FetchType fetchType,
-                       String associatedEntity, Boolean objectProperty) {
-    super(name, reference);
+  public RdfDefinition(String name, String reference, String supersedes, String uri, 
+                       String dataType, Boolean inverse, String model, CollectionType colType,
+                       Boolean entityOwned, IdentifierGenerator generator, CascadeType[] cascade,
+                       FetchType fetchType, String associatedEntity, Boolean objectProperty) {
+    super(name, reference, supersedes);
 
     // Normalize undefines
     if (colType == CollectionType.UNDEFINED)

topaz/core/src/test/java/org/topazproject/otm/metadata/SupersedesTest.java
@@ -1,0 +1,124 @@
+/* $HeadURL::                                                                            $
+ * $Id$
+ *
+ * Copyright (c) 2007-2008 by Topaz, Inc.
+ * http://topazproject.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.topazproject.otm.metadata;
+
+import java.net.URI;
+import java.util.Collections;
+
+import junit.framework.TestCase;
+
+import org.topazproject.otm.CascadeType;
+import org.topazproject.otm.ClassMetadata;
+import org.topazproject.otm.CollectionType;
+import org.topazproject.otm.FetchType;
+import org.topazproject.otm.OtmException;
+import org.topazproject.otm.SessionFactory;
+import org.topazproject.otm.impl.SessionFactoryImpl;
+import org.topazproject.otm.annotations.Entity;
+import org.topazproject.otm.annotations.Id;
+import org.topazproject.otm.annotations.Predicate;
+import org.topazproject.otm.annotations.UriPrefix;
+
+public class SupersedesTest extends TestCase {
+  private SessionFactory sf = new SessionFactoryImpl();
+  private RdfDefinition assoc, extended;
+
+  public void setUp() throws OtmException {
+    sf.preload(B.class);
+    sf.preload(Extended.class);
+
+    assoc = (RdfDefinition)sf.getDefinition("A:assoc");
+    extended = (RdfDefinition)sf.getDefinition("B:assoc");
+
+    extended.resolveReference(sf);
+  }
+
+  private void compare(RdfDefinition def, Object[] vals) {
+    String message = "Testing '" + def.getName() + "': ";
+    assertEquals(message, vals[0], def.getUri());
+    assertEquals(message, vals[1], def.isEntityOwned());
+    assertEquals(message, vals[2], def.getModel());
+    assertEquals(message, vals[3], def.hasInverseUri());
+    assertEquals(message, vals[4], def.typeIsUri());
+    assertEquals(message, vals[5], def.getDataType());
+    assertEquals(message, vals[6], def.getAssociatedEntity());
+    assertEquals(message, vals[7], def.getColType());
+    assertEquals(message, vals[8], def.getFetchType());
+    assertEquals(message, vals[9], def.getCascade());
+    assertEquals(message, vals[10], def.isAssociation());
+  }
+
+  public void test01() {
+    Object[] vals = new Object[] {"a:assoc", true, "test", false, true, null, "Assoc",
+                               CollectionType.PREDICATE, FetchType.lazy,
+                               Collections.singleton(CascadeType.peer), true};
+
+    compare(assoc, vals);
+    vals[6] = "Extended";
+    compare(extended, vals);
+
+    assertTrue(extended.refersSameGraphNodes(assoc));
+    assertTrue(extended.refersSameRange(assoc));
+  }
+
+  public void test02() {
+    ClassMetadata cm = sf.getClassMetadata("A");
+    assertEquals(1, cm.getRdfMappers().size());
+    assertEquals("assoc", cm.getRdfMappers().iterator().next().getName());
+    assertEquals("Assoc", cm.getRdfMappers().iterator().next().getAssociatedEntity());
+
+    cm = sf.getClassMetadata("B");
+    assertEquals(1, cm.getRdfMappers().size());
+    assertEquals("assoc", cm.getRdfMappers().iterator().next().getName());
+    assertEquals("Extended", cm.getRdfMappers().iterator().next().getAssociatedEntity());
+  }
+
+  @Entity(model="test", name="Base")
+  public static class Base {
+    public String getId() {return null;}
+    @Id
+    public void setId(String s) {}
+  }
+
+  @UriPrefix("a:")
+  @Entity(name="A")
+  public static class A extends Base {
+    public Assoc getAssoc() {return null;}
+    @Predicate(model="test")
+    public void setAssoc(Assoc a) {}
+  }
+
+  @UriPrefix("b:")
+  @Entity(name="B")
+  public static class B extends A {
+    @Override
+    public Extended getAssoc() {return null;}
+    @Predicate
+    public void setAssoc(Extended a) {}
+  }
+
+  @Entity(type="t:assoc", name="Assoc")
+  public static class Assoc extends Base {
+  }
+
+  @Entity(type="t:extended", name="Extended")
+  public static class Extended extends Assoc {
+  }
+}

commit 2d99a8d2b197a25b018b609cbf5db10d38294179

The java.beans.PropertyDescriptor(propName, class) constructor is buggy. So figure out getter and setter methods ourselves.
This fixes the broken build from r6411.


git-svn-id: http://svn.topazproject.org/svn/head@6418 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/java/org/topazproject/otm/metadata/AnnotationClassMetaFactory.java
@@ -237,27 +237,59 @@ public class AnnotationClassMetaFactory {
 
   private static PropertyDescriptor toProperty(Method m)
                                         throws OtmException {
-    String propName = getPropertyName(m);
+    String capitalized = getCapitalizedPropertyName(m);
 
-    if ((propName == null) || Modifier.isStatic(m.getModifiers()))
+    if (capitalized == null)
       return null;
 
+    String propName = Introspector.decapitalize(capitalized);
+    Method setter   = null;
+    Method getter   = null;
+
+    if (m.getName().startsWith("set")) {
+      setter = m;
+
+      try {
+        getter = m.getDeclaringClass().getMethod("is" + capitalized);
+      } catch (Throwable t) {
+      }
+
+      try {
+        if ((getter == null) || !getter.getReturnType().equals(setter.getParameterTypes()[0]))
+          getter = m.getDeclaringClass().getMethod("get" + capitalized);
+      } catch (Throwable t) {
+      }
+    } else {
+      getter = m;
+
+      try {
+        setter = m.getDeclaringClass().getMethod("set" + capitalized, getter.getReturnType());
+      } catch (Throwable t) {
+      }
+    }
+
     try {
-      return new PropertyDescriptor(propName, m.getDeclaringClass());
+      // NOTE: PropertyDescriptor(propertyName, class) is buggy. 
+      // Hence all the work above to figure out getter and setter.
+      return new PropertyDescriptor(propName, getter, setter);
     } catch (IntrospectionException e) {
-      throw new OtmException("Failed to create a PropertyDescriptor for '" + propName + "'", e);
+      throw new OtmException("Failed to create a PropertyDescriptor for '" + propName + "' from '"
+                             + m.toGenericString() + "'", e);
     }
   }
 
-  private static String getPropertyName(Method m) {
+  private static String getCapitalizedPropertyName(Method m) {
+    if (Modifier.isStatic(m.getModifiers()))
+      return null;
+
     if (m.getName().startsWith("set") && (m.getParameterTypes().length == 1))
-      return Introspector.decapitalize(m.getName().substring(3));
+      return m.getName().substring(3);
 
     if (m.getName().startsWith("get") && (m.getParameterTypes().length == 0))
-      return Introspector.decapitalize(m.getName().substring(3));
+      return m.getName().substring(3);
 
     if (m.getName().startsWith("is") && (m.getParameterTypes().length == 0))
-      return Introspector.decapitalize(m.getName().substring(2));
+      return m.getName().substring(2);
 
     return null;
   }

topaz/core/src/test/java/org/topazproject/otm/metadata/RdfDefTest.java
@@ -110,14 +110,18 @@ public class RdfDefTest extends TestCase {
     // FIXME assertFalse(ap3.refersSameRange(bp3));
   }
 
+  public void test04() {
+    sf.preload(Journal.class);
+  }
+
   @Entity(model="test", name="Base")
+  @UriPrefix("a:")
   public static class Base {
     public String getId() {return null;}
     @Id
     public void setId(String s) {}
   }
 
-  @UriPrefix("a:")
   @Entity(name="A")
   public static class A extends Base {
     public String getP1() {return null;}
@@ -151,5 +155,12 @@ public class RdfDefTest extends TestCase {
 
   @Entity(type="t:a2", name="Assoc2")
   public static class Assoc2 extends Assoc1 {
+  }
+
+  @Entity(name="Journal")
+  public static class Journal extends Base {
+    public String geteIssn() {return null;}
+    @Predicate
+    public void seteIssn(String s) {}
   }
 }