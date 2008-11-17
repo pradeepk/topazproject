pradeepk/topazproject

commit 6850faaef76b959423cafa033e420d4592d5750d

Removed createGraphInTx/dropGraphInTx, replacing with generic test methods where possible. Also changed createGraph/dropGraph to use graph names rather than GraphConfig definitions. This needed some updates in Ambra to deal with the new dependency that createGraph/dropGraph now has on graph registrations in SessionFactory
git-svn-id: http://svn.topazproject.org/svn/head@6683 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/java/org/topazproject/ambra/bootstrap/WebAppListenerInitModels.java
@@ -77,11 +77,6 @@ public class WebAppListenerInitModels implements ServletContextListener {
 
       SessionFactory sessionFactory = new SessionFactoryImpl();
       sessionFactory.setTripleStore(store);
-      session = sessionFactory.openSession();
-
-      dropObsoleteGraphs(conf, session);
-
-      txn = session.beginTransaction();
       conf                  = conf.subset("ambra.graphs");
 
       Iterator it           = conf.getKeys();
@@ -95,8 +90,15 @@ public class WebAppListenerInitModels implements ServletContextListener {
         String graph  = conf.getString(key);
         String type   = conf.getString(key + "[@type]", "mulgara:Model");
 
-        session.createGraph(new GraphConfig("", new URI(graph), new URI(type)));
+        sessionFactory.addGraph(new GraphConfig(key, new URI(graph), new URI(type)));
       }
+
+
+      session = sessionFactory.openSession();
+      dropObsoleteGraphs(conf, session);
+      txn = session.beginTransaction();
+      for (GraphConfig graph: sessionFactory.listGraphs())
+        session.createGraph(graph.getId());
       txn.commit();
       log.info("Successfully created all configured ITQL Graphs.");
     } catch (Exception e) {
@@ -119,9 +121,13 @@ public class WebAppListenerInitModels implements ServletContextListener {
    */
   private void dropObsoleteGraphs(Configuration conf, Session session) throws URISyntaxException {
     String graphPrefix = conf.getString("ambra.topaz.tripleStore.mulgara.graphPrefix");
+    Transaction txn = null;
     try {
-      session.dropGraphInTx(new GraphConfig("", new URI(graphPrefix + "str"), null));
+      txn = session.beginTransaction();
+      session.doNativeUpdate("drop <" + graphPrefix + "str> ;");
+      txn.commit();
     } catch (OtmException e) {
+      if (txn != null) txn.rollback();
       log.warn("Could not drop graph " + graphPrefix + "str", e);
     }
   }

ambra/doi-resolver/src/test/java/org/topazproject/ambra/doi/DOITypeResolverTest.java
@@ -87,12 +87,15 @@ public class DOITypeResolverTest {
     try {
       session = factory.openSession();
 
-      try {
-        session.dropGraphInTx(mc);
-      } catch (OtmException e) {
-      }
       Transaction tx = session.beginTransaction();
-      session.createGraph(mc);
+      try {
+        session.dropGraph(mc.getId());
+        tx.commit();
+      } catch (OtmException e) {
+        tx.rollback();
+      }
+      tx = session.beginTransaction();
+      session.createGraph(mc.getId());
 
       try {
         for (String doi : types.keySet())

ambra/webapp/src/main/java/org/topazproject/ambra/configuration/OtmConfiguration.java
@@ -128,7 +128,7 @@ public class OtmConfiguration {
       txn = session.beginTransaction();
       for (GraphConfig graph : graphs) {
         factory.addGraph(graph);
-        session.createGraph(graph);
+        session.createGraph(graph.getId());
       }
       txn.commit();
     } catch (Exception e) {

ambra/webapp/src/main/java/org/topazproject/ambra/permission/service/PermissionsService.java
@@ -40,6 +40,7 @@ import org.topazproject.otm.OtmException;
 import org.topazproject.otm.Rdf;
 import org.topazproject.otm.RdfUtil;
 import org.topazproject.otm.Session;
+import org.topazproject.otm.SessionFactory;
 import org.topazproject.otm.Transaction;
 import org.topazproject.otm.TripleStore;
 import org.topazproject.otm.query.Results;
@@ -156,9 +157,16 @@ public class PermissionsService implements Permissions {
       txn = s.beginTransaction();
     }
     try {
-      s.createGraph(new GraphConfig("grants",  toURI(GRANTS_GRAPH),  toURI(GRANTS_GRAPH_TYPE)));
-      s.createGraph(new GraphConfig("revokes", toURI(REVOKES_GRAPH), toURI(REVOKES_GRAPH_TYPE)));
-      s.createGraph(new GraphConfig("pp",      toURI(PP_GRAPH),      toURI(PP_GRAPH_TYPE)));
+      SessionFactory sf   = s.getSessionFactory();
+      GraphConfig grants  = new GraphConfig("grants",  toURI(GRANTS_GRAPH),  toURI(GRANTS_GRAPH_TYPE));
+      GraphConfig revokes = new GraphConfig("revokes", toURI(REVOKES_GRAPH), toURI(REVOKES_GRAPH_TYPE));
+      GraphConfig pp      = new GraphConfig("pp",      toURI(PP_GRAPH),      toURI(PP_GRAPH_TYPE));
+      sf.addGraph(grants);
+      sf.addGraph(revokes);
+      sf.addGraph(pp);
+      s.createGraph(grants.getId());
+      s.createGraph(revokes.getId());
+      s.createGraph(pp.getId());
       if (localTxn) txn.commit();
     } catch (OtmException e) {
       if (localTxn) txn.rollback();

fedora/fedora-blobstore/src/it/java/org/topazproject/fedora/otm/FedoraBlobTest.java
@@ -150,22 +150,15 @@ public class FedoraBlobTest {
 
     String[] names = new String[] { "ri", "grants", "revokes", "criteria" };
 
-    Session session = factory.openSession();
-    try {
-      for (String name : names) {
-        GraphConfig graph = factory.getGraph(name);
-
-        try {
-          session.dropGraphInTx(graph);
-        } catch (Throwable t) {
-          if (log.isDebugEnabled())
-            log.debug("Failed to drop graph '" + name + "'", t);
-        }
-
-        session.createGraphInTx(graph);
+    for (final String name : names) {
+      try {
+        doInSession(new Action() { public void run(Session s) throws OtmException { s.dropGraph(name); } });
+      } catch (Throwable t) {
+        if (log.isDebugEnabled())
+          log.debug("Failed to drop graph '" + name + "'", t);
       }
-    } finally {
-      session.close();
+
+      doInSession(new Action() { public void run(Session s) throws OtmException { s.createGraph(name); } });
     }
 
   }

topaz/core/src/it/groovy/org/topazproject/otm/AbstractTest.groovy
@@ -53,10 +53,8 @@ public abstract class AbstractTest extends GroovyTestCase {
     for (c in graphs) {
       def g = new GraphConfig(c[0], "local:///topazproject#${c[1]}".toURI(), c[2])
       rdf.sessFactory.addGraph(g)
-      Session sess = rdf.sessFactory.openSession();
-      try { sess.dropGraphInTx(g); } catch (Throwable t) { }
-      sess.createGraphInTx(g);
-      sess.close();
+      try { doInTx { s -> s.dropGraph(g.getId()) } } catch (OtmException e) { }
+      doInTx { s -> s.createGraph(g.getId()) }
     }
   }
 

topaz/core/src/it/groovy/org/topazproject/otm/metadata/BuilderIntegrationTest.groovy
@@ -43,18 +43,10 @@ public class BuilderIntegrationTest extends GroovyTestCase {
     def m2 = new GraphConfig("m2", "local:///topazproject#otmtest2".toURI(), null)
     rdf.sessFactory.addGraph(m2);
 
-    Session sess = rdf.sessFactory.openSession();
-    try {
-      sess.dropGraphInTx(ri)
-    } catch (Throwable t) {
-    }
-    try {
-      sess.dropGraphInTx(m2)
-    } catch (Throwable t) {
-    }
-    sess.createGraphInTx(ri)
-    sess.createGraphInTx(m2)
-    sess.close();
+    try { doInTx{ s -> s.dropGraph(ri.getId()) } } catch (OtmException e) { }
+    try { doInTx{ s -> s.dropGraph(m2.getId()) } } catch (OtmException e) { }
+    doInTx{ s -> s.createGraph(ri.getId()) }
+    doInTx{ s -> s.createGraph(m2.getId()) }
   }
 
   void testSimple() {
@@ -204,10 +196,8 @@ public class BuilderIntegrationTest extends GroovyTestCase {
       def m = new GraphConfig("m${num}", "local:///topazproject#otmtest_m${num}".toURI(), null)
       rdf.sessFactory.addGraph(m);
 
-      Session session = rdf.sessFactory.openSession();
-      try { session.dropGraphInTx(m); } catch (OtmException oe) { }
-      session.createGraphInTx(m);
-      session.close();
+      try { doInTx{ s -> s.dropGraph(m.getId()) } } catch (OtmException oe) { }
+      doInTx{ s -> s.createGraph(m.getId()) }
     }
 
     // predicate stored with child graph

topaz/core/src/it/groovy/org/topazproject/otm/query/OqlTest.groovy
@@ -1018,10 +1018,8 @@ public class OqlTest extends AbstractTest {
       def m = new GraphConfig("m${num}", "local:///topazproject#otmtest_m${num}".toURI(), null)
       rdf.sessFactory.addGraph(m);
 
-      Session session = rdf.sessFactory.openSession();
-      try { session.dropGraphInTx(m); } catch (OtmException oe) { }
-      session.createGraphInTx(m);
-      session.close();
+      try { doInTx{ s -> s.dropGraph(m.getId()) } } catch (OtmException oe) { }
+      doInTx{ s -> s.createGraph(m.getId()) }
     }
 
     // predicate stored with child graph

commit a2cc8ecf098802180c400cff91fa1319857d219f

Fixed obsolete-graph removal broken in r6683 due to using a subsetted config.
git-svn-id: http://svn.topazproject.org/svn/head@6690 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/java/org/topazproject/ambra/bootstrap/WebAppListenerInitModels.java
@@ -76,9 +76,9 @@ public class WebAppListenerInitModels implements ServletContextListener {
 
       SessionFactory sessionFactory = new SessionFactoryImpl();
       sessionFactory.setTripleStore(store);
-      conf                  = conf.subset("ambra.graphs");
 
-      Iterator it           = conf.getKeys();
+      Configuration gconf = conf.subset("ambra.graphs");
+      Iterator      it    = gconf.getKeys();
 
       while (it.hasNext()) {
         String key = (String) it.next();
@@ -86,8 +86,8 @@ public class WebAppListenerInitModels implements ServletContextListener {
         if ((key.indexOf("[") >= 0) || (key.indexOf(".") >= 0))
           continue;
 
-        String graph  = conf.getString(key);
-        String type   = conf.getString(key + "[@type]", "mulgara:Model");
+        String graph  = gconf.getString(key);
+        String type   = gconf.getString(key + "[@type]", "mulgara:Model");
 
         sessionFactory.addGraph(new GraphConfig(key, new URI(graph), new URI(type)));
       }