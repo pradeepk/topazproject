4e78e6191df83021fed2b044920b246a8597559d
dragisak <dragisak@48088309-b40f-0410-b149-b14953253675>
2008-11-19 17:02:57 +0000

    toArray needs to be atomic and allFactories can change between calls. You can't guarantee size of array to stay constant during this operation.
    git-svn-id: http://svn.topazproject.org/svn/head@6708 48088309-b40f-0410-b149-b14953253675

--- mulgara/mulgara-client/src/main/java/org/topazproject/mulgara/itql/EmbeddedClient.java
+++ mulgara/mulgara-client/src/main/java/org/topazproject/mulgara/itql/EmbeddedClient.java
@@ -60,9 +60,11 @@ public class EmbeddedClient extends TIClient {
       public void run() {
         // close all session factories
         synchronized (EmbeddedClient.class) {
-          // Note: need to make a copy of the values here so we don't get values removed
-          // from out under us; we also need to make sure the copy is "atomic"
-          for (Map<URI, SessionFactory> fl : allFactories.values().toArray(new Map[allFactories.values().size()])) {
+          /*
+            Note: need to make a copy of the values here so we don't get values removed
+            from out under us; we also need to make sure the copy is "atomic"
+          */
+          for (Map<URI, SessionFactory> fl : allFactories.values().toArray(new Map[0])) {
             for (SessionFactory sf : fl.values()) {
               try {
                 sf.close();
