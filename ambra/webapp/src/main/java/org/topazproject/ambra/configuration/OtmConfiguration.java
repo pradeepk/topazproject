pradeepk/topazproject

commit 7c160081a24bf91b0ec4d6cccbc6e30cedecdd54

Migrate Ambra to using automated detection of Topaz annotated classes. This is a new feature in Topaz which looks for topaz.xml marker file in jars to parse class files in that jar to auto-load all annotated classes there instead of having to manually preload each one.
Please note: I have left some files in applicationContext.xml. These files should never
have been created here and need to be moved/removed in the future. For now as long as you
are creating classes in otm-models, they will be auto-loaded.


git-svn-id: http://svn.topazproject.org/svn/head@6786 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/java/org/topazproject/ambra/configuration/OtmConfiguration.java
@@ -42,7 +42,7 @@ import org.topazproject.fedora.otm.FedoraBlobFactory;
  */
 public class OtmConfiguration {
   private SessionFactory   factory;
-  private String[]         preloadClasses;
+  private String[]         preloadClasses = new String[0];
   private GraphConfig[]    graphs;
   private static final Log log = LogFactory.getLog(OtmConfiguration.class);
 
@@ -66,6 +66,8 @@ public class OtmConfiguration {
       log.debug("Added blobstore : " + blobStore.getClass());
 
     factory.setBlobStore(blobStore);
+    factory.preloadFromClasspath();
+    factory.validate();
   }
 
   /**

ambra/libs/otm-models/src/main/resources/topaz.xml
@@ -1,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+  $HeadURL$
+  $Id$
+  
+  Copyright (c) 2006-2008 by Topaz, Inc.
+  http://topazproject.org
+  
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+  http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+<topaz>
+</topaz>

ambra/webapp/src/main/webapp/WEB-INF/applicationContext.xml
@@ -256,68 +256,11 @@
 
     <property name="preloadClasses">
       <list>
-        <value>org.topazproject.ambra.models.Aggregation</value>
-        <value>org.topazproject.ambra.models.Annotation</value>
-        <value>org.topazproject.ambra.models.Annotea</value>
-        <value>org.topazproject.ambra.models.Article</value>
-        <value>org.topazproject.ambra.models.AuthenticationId</value>
-        <value>org.topazproject.ambra.models.Category</value>
-        <value>org.topazproject.ambra.models.Citation</value>
-        <value>org.topazproject.ambra.models.Comment</value>
-        <value>org.topazproject.ambra.models.Correction</value>
-        <value>org.topazproject.ambra.models.FormalCorrection</value>
-        <value>org.topazproject.ambra.models.MinorCorrection</value>
-        <value>org.topazproject.ambra.models.DublinCore</value>
-        <value>org.topazproject.ambra.models.EditorialBoard</value>
-        <value>org.topazproject.ambra.models.FoafPerson</value>
-        <value>org.topazproject.ambra.models.Issue</value>
-        <value>org.topazproject.ambra.models.Journal</value>
-        <value>org.topazproject.ambra.models.License</value>
-        <value>org.topazproject.ambra.models.ObjectInfo</value>
-        <value>org.topazproject.ambra.models.PermissionsPropagator</value>
-        <value>org.topazproject.ambra.models.Rating</value>
-        <value>org.topazproject.ambra.models.RatingContent</value>
-        <value>org.topazproject.ambra.models.RatingSummary</value>
-        <value>org.topazproject.ambra.models.RatingSummaryContent</value>
-        <value>org.topazproject.ambra.models.RelatedArticle</value>
-        <value>org.topazproject.ambra.models.Reply</value>
-        <value>org.topazproject.ambra.models.ReplyThread</value>
-        <value>org.topazproject.ambra.models.Representation</value>
-        <value>org.topazproject.ambra.models.Trackback</value>
-        <value>org.topazproject.ambra.models.TrackbackContent</value>
-        <value>org.topazproject.ambra.models.TextRepresentation</value>
-        <value>org.topazproject.ambra.models.UserAccount</value>
-        <value>org.topazproject.ambra.models.UserPreference</value>
-        <value>org.topazproject.ambra.models.UserPreferences</value>
-        <value>org.topazproject.ambra.models.UserProfile</value>
-        <value>org.topazproject.ambra.models.UserRole</value>
-        <value>org.topazproject.ambra.models.Volume</value>
-        <value>org.topazproject.ambra.models.Blob</value>
-        <value>org.topazproject.ambra.models.AnnotationBlob</value>
-        <value>org.topazproject.ambra.models.ReplyBlob</value>
         <value>org.topazproject.ambra.model.article.CitationInfo</value>
         <value>org.topazproject.ambra.model.UserProfileInfo</value>
         <value>org.topazproject.ambra.model.article.ArticleInfo</value>
         <value>org.topazproject.ambra.model.article.NewArtInfo</value>
         <value>org.topazproject.ambra.model.article.RelatedArticleInfo</value>
-        <value>org.topazproject.otm.criterion.Conjunction</value>
-        <value>org.topazproject.otm.criterion.Criterion</value>
-        <value>org.topazproject.otm.criterion.DetachedCriteria</value>
-        <value>org.topazproject.otm.criterion.Disjunction</value>
-        <value>org.topazproject.otm.criterion.EQCriterion</value>
-        <value>org.topazproject.otm.criterion.ExistsCriterion</value>
-        <value>org.topazproject.otm.criterion.GECriterion</value>
-        <value>org.topazproject.otm.criterion.GTCriterion</value>
-        <value>org.topazproject.otm.criterion.LECriterion</value>
-        <value>org.topazproject.otm.criterion.LTCriterion</value>
-        <value>org.topazproject.otm.criterion.MinusCriterion</value>
-        <value>org.topazproject.otm.criterion.NECriterion</value>
-        <value>org.topazproject.otm.criterion.NotCriterion</value>
-        <value>org.topazproject.otm.criterion.NotExistsCriterion</value>
-        <value>org.topazproject.otm.criterion.Order</value>
-        <value>org.topazproject.otm.criterion.Parameter</value>
-        <value>org.topazproject.otm.criterion.TransCriterion</value>
-        <value>org.topazproject.otm.criterion.WalkCriterion</value>
       </list>
     </property>
   </bean>

commit d98b7da8f1a1f69cde98036defbf3f73e0a64aea

Fix bug introduced in r6786. Aliases needs to be defined before preloading classes. Reworked the configurator to build the SessionFactory only when all config items are set and when the factory is first asked for (which is at init now because of the current spring config).
git-svn-id: http://svn.topazproject.org/svn/head@6796 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/java/org/topazproject/ambra/configuration/OtmConfiguration.java
@@ -19,13 +19,14 @@
 package org.topazproject.ambra.configuration;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.topazproject.otm.GraphConfig;
-import org.topazproject.otm.Session;
 import org.topazproject.otm.SessionFactory;
 import org.topazproject.otm.Transaction;
 import org.topazproject.otm.impl.SessionFactoryImpl;
@@ -41,9 +42,12 @@ import org.topazproject.fedora.otm.FedoraBlobFactory;
  * @author Stephen Cheng
  */
 public class OtmConfiguration {
-  private SessionFactory   factory;
-  private String[]         preloadClasses = new String[0];
-  private GraphConfig[]    graphs;
+  private final String        tripleStoreUrl;
+  private final BlobStore     blobStore;
+  private SessionFactory      factory = null;
+  private String[]            preloadClasses = new String[0];
+  private GraphConfig[]       graphs = new GraphConfig[0];
+  private Map<String, String> aliases = new HashMap<String, String>();
   private static final Log log = LogFactory.getLog(OtmConfiguration.class);
 
   /**
@@ -53,21 +57,8 @@ public class OtmConfiguration {
    * @param blobStore the blob-store to use
    */
   public OtmConfiguration(String tripleStoreUrl, BlobStore blobStore) {
-    factory = new SessionFactoryImpl();
-
-    if (log.isDebugEnabled()) {
-      log.debug("Creating new triplestore: " + tripleStoreUrl);
-    }
-
-    factory.setTripleStore(new ItqlStore(URI.create(tripleStoreUrl),
-                           WebappItqlClientFactory.getInstance()));
-
-    if (log.isDebugEnabled())
-      log.debug("Added blobstore : " + blobStore.getClass());
-
-    factory.setBlobStore(blobStore);
-    factory.preloadFromClasspath();
-    factory.validate();
+    this.tripleStoreUrl = tripleStoreUrl;
+    this.blobStore = blobStore;
   }
 
   /**
@@ -85,17 +76,11 @@ public class OtmConfiguration {
    * @param preloadClasses The preloadClasses to set.
    */
   public void setPreloadClasses(String[] preloadClasses) {
-    this.preloadClasses = preloadClasses;
-
-    for (String className : preloadClasses) {
-      try {
-        factory.preload(Class.forName(className));
-      } catch (ClassNotFoundException ce) {
-        log.info("Could not preload class: " + className, ce);
-      }
+    if ((factory != null) && !Arrays.equals(preloadClasses, this.preloadClasses)) {
+      factory = null;
+      log.warn("Removed old factory because new set of preloadClasses specified");
     }
-
-    factory.validate();
+    this.preloadClasses = preloadClasses;
   }
 
   /**
@@ -104,6 +89,8 @@ public class OtmConfiguration {
    * @return Returns the factory.
    */
   public SessionFactory getFactory() {
+    if (factory == null)
+      factory = createFactory();
     return factory;
   }
 
@@ -122,18 +109,11 @@ public class OtmConfiguration {
    * @param graphs The graphs to set.
    */
   public void setGraphs(final GraphConfig[] graphs) {
+    if ((factory != null) && !Arrays.equals(graphs, this.graphs)) {
+      factory = null;
+      log.warn("Removed old factory because new set of graph configs specified");
+    }
     this.graphs = graphs;
-
-    for (GraphConfig graph : graphs)
-      factory.addGraph(graph);
-
-    TransactionHelper.doInTx(factory, new TransactionHelper.Action<Void>() {
-      public Void run(Transaction tx) {
-        for (GraphConfig graph : graphs)
-          tx.getSession().createGraph(graph.getId());
-        return null;
-      }
-    });
   }
 
   /**
@@ -142,7 +122,7 @@ public class OtmConfiguration {
    * @return the aliases
    */
   public Map<String, String> getAliases() {
-    return factory.listAliases();
+    return aliases;
   }
 
   /**
@@ -151,8 +131,11 @@ public class OtmConfiguration {
    * @param aliases the aliases to set.
    */
   public void setAliases(Map<String, String> aliases) {
-    for (Map.Entry<String, String> alias : aliases.entrySet())
-      factory.addAlias(alias.getKey(), alias.getValue());
+    if ((factory != null) && !aliases.equals(this.aliases)) {
+      factory = null;
+      log.warn("Removed old factory because new set of aliases specified");
+    }
+    this.aliases = aliases;
   }
 
   /**
@@ -161,13 +144,79 @@ public class OtmConfiguration {
    * @param fbfs array of fedora blob factories to add
    */
   public void setFedoraBlobFactories(FedoraBlobFactory[] fbfs) {
-    if (factory.getBlobStore() instanceof FedoraBlobStore) {
-      FedoraBlobStore fbs = (FedoraBlobStore)factory.getBlobStore();
+    if (blobStore instanceof FedoraBlobStore) {
+      FedoraBlobStore fbs = (FedoraBlobStore)blobStore;
       for (FedoraBlobFactory fbf : fbfs) {
         fbs.addBlobFactory(fbf);
         if (log.isDebugEnabled())
           log.debug("Added BlobFactory for " + fbf.getSupportedUriPrefixes());
       }
     }
+  }
+
+  private SessionFactory createFactory() {
+    if (log.isDebugEnabled())
+      log.debug("Creating new SessionFactory instance ...");
+
+    SessionFactory factory = new SessionFactoryImpl();
+
+    if (log.isDebugEnabled())
+      log.debug("Adding aliases: " + aliases);
+
+    for (Map.Entry<String, String> alias : aliases.entrySet())
+      factory.addAlias(alias.getKey(), alias.getValue());
+
+    if (log.isDebugEnabled())
+      log.debug("Creating new triplestore: " + tripleStoreUrl);
+
+    factory.setTripleStore(new ItqlStore(URI.create(tripleStoreUrl),
+                           WebappItqlClientFactory.getInstance()));
+
+    if (log.isDebugEnabled())
+      log.debug("Setting blobstore : " + blobStore.getClass());
+
+    factory.setBlobStore(blobStore);
+
+    if (log.isDebugEnabled())
+      log.debug("Adding graph configs : " + Arrays.asList(graphs));
+
+    for (GraphConfig graph : graphs)
+      factory.addGraph(graph);
+
+    if (log.isDebugEnabled())
+      log.debug("Creating graphs : " + Arrays.asList(graphs));
+
+    TransactionHelper.doInTx(factory, new TransactionHelper.Action<Void>() {
+      public Void run(Transaction tx) {
+        for (GraphConfig graph : graphs)
+          tx.getSession().createGraph(graph.getId());
+        return null;
+      }
+    });
+
+    if (log.isDebugEnabled())
+      log.debug("Pre-loading classes from class-path ...");
+
+    factory.preloadFromClasspath();
+
+    if (log.isDebugEnabled())
+      log.debug("Pre-loading classes : " + Arrays.asList(preloadClasses));
+
+    for (String className : preloadClasses) {
+      try {
+        factory.preload(Class.forName(className));
+      } catch (ClassNotFoundException ce) {
+        log.warn("Could not preload class: " + className, ce);
+      }
+    }
+
+    if (log.isDebugEnabled())
+      log.debug("Validating factory config...");
+
+    factory.validate();
+
+    log.info("Initialized OTM SessionFactory instance.");
+
+    return factory;
   }
 }