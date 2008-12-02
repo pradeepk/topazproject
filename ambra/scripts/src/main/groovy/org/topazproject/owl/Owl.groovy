78b36206e2b8c66e017a48afdb20cdced3fdd47a
amit <amit@48088309-b40f-0410-b149-b14953253675>
2008-12-02 07:34:27 +0000

    Cleaned up script in a big way. Topaz's ability to find and load annotated classes help in removing majority of the code here.
    I have added a function generate for now. This is a hack to allow use of mvn
    -Pgenerate-owl within a development tree. You still will have to patch
    AnnotationClassMetaFactory.java to disable use of context class loader to make
    it work.
    
    git-svn-id: http://svn.topazproject.org/svn/head@6817 48088309-b40f-0410-b149-b14953253675

--- ambra/scripts/src/main/groovy/org/topazproject/owl/Owl.groovy
+++ ambra/scripts/src/main/groovy/org/topazproject/owl/Owl.groovy
@@ -20,14 +20,10 @@ package org.topazproject.owl
 
 import java.util.Collections
 import java.util.HashMap
-import java.util.jar.JarFile
 
 import org.topazproject.otm.ClassMetadata
 import org.topazproject.otm.EntityMode
-import org.topazproject.otm.OtmException
 import org.topazproject.otm.SessionFactory
-
-import org.topazproject.otm.criterion.DetachedCriteria
 
 import org.topazproject.otm.impl.SessionFactoryImpl
 
@@ -46,127 +42,45 @@ import org.topazproject.ambra.util.ToolHelper
  * @author Amit Kapoor
  */
 class Owl {
-  static GroovyClassLoader gcl = new GroovyClassLoader()
   static SessionFactory factory = new SessionFactoryImpl()
 
   /**
    * Given a list of directories and/or jar files, generate metadata.
    */
   public static void main(String[] args) {
-    addClasses(ToolHelper.fixArgs(args))
+    factory.preloadFromClasspath()
     generateOwl()
   }
 
   /**
-   * Extract otm annotated meatadata from classes found in directories and/or jar files.
-   *
-   * @param classPaths a collection of jar filenames and/or directories
+   * This is a temporary solution to the problem associated with the gmaven
+   * plugin. This function accepts the classloader from the calle to ensure
+   * classes are loaded correctly. This allows use of mvn -Pgenerate-owl within
+   * a development tree. Please note: you will need to temporarily patch
+   * AnnotationClassMetaFactory.java to not use the default class loader
+   * instead of the context class loader.
    */
-  static void addClasses(classPaths) {
-    // Update gcl classpath first
-    classPaths.each() { fname ->
-      def file = expandFilename(fname)
-      if (file.isDirectory() || fname =~ /\.jar$/) {
-        gcl.addClasspath(file.getAbsolutePath())
-      } else
-        println "$fname not a jar file or directory"
-    }
-
-    // Process classes
-    classPaths.each() { fname ->
-      def file = expandFilename(fname)
-      if (fname =~ /\.jar$/)
-        addJar(file)
-      else if (file.isDirectory())
-        addDirectory(file)
-    }
-
-    // Add Object to class meta-data
-    Map<EntityMode, EntityBinder> binders = new HashMap<EntityMode, EntityBinder>();
-    binders.put(EntityMode.POJO, new ClassBinder(Object.class));
-    factory.setClassMetadata(new ClassMetadata(binders, "Object", Collections.EMPTY_SET,
-                                               Collections.EMPTY_SET, "", null, Collections.EMPTY_SET,
-                                               null, Collections.EMPTY_SET, Collections.EMPTY_SET));
-    
-    // DetachedCriteria
-    binders = new HashMap<EntityMode, EntityBinder>();
-    binders.put(EntityMode.POJO, new ClassBinder(DetachedCriteria.class));
-    factory.setClassMetadata(new ClassMetadata(binders, "DetachedCriteria", Collections.EMPTY_SET,
-                                               Collections.EMPTY_SET, "", null, Collections.EMPTY_SET,
-                                               null, Collections.EMPTY_SET, Collections.EMPTY_SET));
-
-    factory.validate()
-  }
+   public static generate(ClassLoader cl, String[] args) {
+     factory.preloadFromClasspath(cl)
+     generateOwl()
+   }
 
   /**
    * Generate the OWL statements
    */
   static void generateOwl() {
+    // Add Object to class meta-data
+    Map<EntityMode, EntityBinder> binders = new HashMap<EntityMode, EntityBinder>()
+    binders.put(EntityMode.POJO, new ClassBinder(Object.class))
+    factory.setClassMetadata(new ClassMetadata(binders, "Object", Collections.EMPTY_SET,
+                                               Collections.EMPTY_SET, "", null, Collections.EMPTY_SET,
+                                               null, Collections.EMPTY_SET, Collections.EMPTY_SET))
+    factory.validate()
+
     OwlGenerator owlGen= new OwlGenerator("http://www.plos.org/content_model#", (SessionFactory)factory)
     owlGen.addNamespaces(null, factory.listAliases())
     owlGen.generateClasses()
     owlGen.generateProperties()
     owlGen.save("file:" + System.properties['user.home'] + File.separator + "ambra.owl")
-  }
-
-  /**
-   * Extract class files from jar
-   */
-  static void addJar(File file) {
-    def jarfile = new JarFile(file)
-    jarfile.entries().each() {
-      def name = it.toString()
-      if (name =~ /\.class$/) {
-        def clazz = getClass(name)
-        if (clazz)
-          processClass(clazz)
-      }
-    }
-  }
-
-  /** 
-   * Iterate over all the files in a directory and find any otm classes.
-   */
-  static void addDirectory(File dir) {
-    dir.eachFileRecurse() { fname ->
-      if (fname =~ /\.class$/) {
-        def name = fname.getAbsolutePath() - dir.getAbsolutePath()
-        name = name.replaceFirst(/^\//, "")
-        def clazz = getClass(name)
-        if (clazz)
-          processClass(clazz)
-      }
-    }
-  }
-
-  /** 
-   * See if a class is otm annotated and add it to our factory.
-   */
-  static void processClass(Class clazz) {
-    try {
-      factory.preload(clazz)
-    } catch (OtmException o) {
-      println "OTM Exception loading " + clazz.getName() + " : $o"
-    }
-  }
-
-  /**
-   * Convert a filename to a classname and load it via our GroovyClassLoader
-   */
-  static Class getClass(String name) {
-    def cName = name.replaceAll(/\//, ".") - ".class"
-    try {
-      return gcl.loadClass(cName)
-    } catch (NoClassDefFoundError ncdfe) {
-      println "$ncdfe (loading $cName)"
-      return null
-    }
-  }
-
-  /**
-   * Deal with ~ at the beginning of a file or directory
-   */
-  static File expandFilename(String name) {
-    return new File(name.replaceFirst(/^~/, System.getProperty("user.home")))
   }
 }
