b83af6636b5dd9d811aa18de4e0de594798f524d
dragisak <dragisak@48088309-b40f-0410-b149-b14953253675>
2009-01-27 20:28:01 +0000

    Simplify unit test.
    git-svn-id: http://svn.topazproject.org/svn/head@7213 48088309-b40f-0410-b149-b14953253675

--- ambra/webapp/src/test/java/org/topazproject/ambra/util/ArticleFormattingDirectiveTest.java
+++ ambra/webapp/src/test/java/org/topazproject/ambra/util/ArticleFormattingDirectiveTest.java
@@ -1,7 +1,7 @@
 /* $HeadURL$
  * $Id$
  *
- * Copyright (c) 2006-2008 by Topaz, Inc.
+ * Copyright (c) 2006-2009 by Topaz, Inc.
  * http://topazproject.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,64 +20,34 @@
 package org.topazproject.ambra.util;
 
 import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import static org.testng.Assert.assertEquals;
 
 /**
  * @author Dragisa Krsmanovic
  */
 public class ArticleFormattingDirectiveTest {
 
-  @Test
-  public void testItalic() throws Exception {
-    Assert.assertEquals(ArticleFormattingDirective.format("foo<italic>bar</italic>"),
-        "foo<i>bar</i>");
+  @DataProvider(name = "content")
+  public String[][] createContent() {
+    return new String[][]{
+        {"foo<italic>bar</italic>", "foo<i>bar</i>"},
+        {"foo&lt;italic&gt;bar&lt;/italic&gt;", "foo<i>bar</i>"},
+        {"foo<bold>bar</bold>", "foo<b>bar</b>"},
+        {"foo<monospace>bar</monospace>", "foo<span class=\"monospace\">bar</span>"},
+        {"foo<overline>bar</overline>", "foo<span class=\"overline\">bar</span>"},
+        {"foo<sc>bar</sc>", "foo<small>bar</small>"},
+        {"foo<strike>bar</strike>", "foo<s>bar</s>"},
+        {"foo<underline>bar</underline>", "foo<u>bar</u>"},
+        {"foo<named-content xmlns:xlink= \"http://www.w3.org/1999/xlink\" " +
+            "content-type=\"genus-species\" xlink:type=\"simple\">bar</named-content>",
+            "foo<i>bar</i>"}
+    };
   }
 
-  @Test
-  public void testHtmlEncodedItalic() throws Exception {
-    Assert.assertEquals(ArticleFormattingDirective.format("foo&lt;italic&gt;bar&lt;/italic&gt;"),
-        "foo<i>bar</i>");
+  @Test(dataProvider = "content")
+  public void testItalic(String content, String expected) throws Exception {
+    assertEquals(ArticleFormattingDirective.format(content), expected);
   }
 
-  @Test
-  public void testBold() throws Exception {
-    Assert.assertEquals(ArticleFormattingDirective.format("foo<bold>bar</bold>"), "foo<b>bar</b>");
-  }
-
-  @Test
-  public void testMonospace() throws Exception {
-    Assert.assertEquals(ArticleFormattingDirective.format("foo<monospace>bar</monospace>"),
-        "foo<span class=\"monospace\">bar</span>");
-  }
-
-  @Test
-  public void testOverline() throws Exception {
-    Assert.assertEquals(ArticleFormattingDirective.format("foo<overline>bar</overline>"),
-        "foo<span class=\"overline\">bar</span>");
-  }
-
-  @Test
-  public void testSmall() throws Exception {
-    Assert.assertEquals(ArticleFormattingDirective.format("foo<sc>bar</sc>"),
-        "foo<small>bar</small>");
-  }
-
-  @Test
-  public void testStrike() throws Exception {
-    Assert.assertEquals(ArticleFormattingDirective.format("foo<strike>bar</strike>"),
-        "foo<s>bar</s>");
-  }
-
-  @Test
-  public void testUnderline() throws Exception {
-    Assert.assertEquals(ArticleFormattingDirective.format("foo<underline>bar</underline>"),
-        "foo<u>bar</u>");
-  }
-
-  @Test
-  public void testNamedContent() throws Exception {
-    Assert.assertEquals(ArticleFormattingDirective.format("foo<named-content xmlns:xlink= " +
-        "\"http://www.w3.org/1999/xlink\" content-type=\"genus-species\" xlink:type=\"simple\">" +
-        "bar</named-content>"), "foo<i>bar</i>");
-  }
 }
