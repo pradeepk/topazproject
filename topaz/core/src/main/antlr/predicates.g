pradeepk/topazproject

commit a04f499bcdee306e5edfbdf3a6ad3cc85c6a703a

Fix type propagation in casts: the resulting type needs to be propagated to the last id of the casted expression; e.g. in 'cast(a.b, Foo)' the AST representing '.b' should now have type Foo.
git-svn-id: http://svn.topazproject.org/svn/head@6943 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/antlr/predicates.g
@@ -274,6 +274,21 @@ options {
       return type.getMeta().getGraph();
     }
 
+    private AST findLastId(AST factor) {
+      switch (factor.getType()) {
+        case PARAM:
+          return factor.getFirstChild();
+
+        case REF:
+          for (factor = factor.getFirstChild(); factor.getNextSibling() != null;
+               factor = factor.getNextSibling())
+            ;
+
+        default:
+          return factor;
+      }
+    }
+
     private void addVar(AST var, AST clazz) throws RecognitionException {
       ExprType type = (clazz != null) ? getTypeForClass(clazz, "from clause") : null;
       addVar(var, type);
@@ -439,7 +454,8 @@ factor[boolean isProj, boolean isComp] returns [ExprType type = null]
     | #(PARAM ID)
     | type=fcall[isProj, isComp]
     | #(REF (   v:ID                 { updateAST(#v, null, type = getTypeForVar(#v), null, true); }
-              | type=c:cast[isProj]  { updateAST(#c, null, type, null, ((OqlAST) #c).isVar()); }
+              | type=c:cast[isProj]  { updateAST(findLastId(#c), null, type, null,
+                                                 ((OqlAST) #c).isVar()); }
             )
             (   ! ID         { type = resolveField(currentAST, type, #ID); }
               | ! URIREF     { type = handlePredicate(currentAST, type, #URIREF); }

commit 4f5d00acee37ead22be42e990262432a1436834a

Fixed problem with cast(foo, Foo) where 'foo' wasn't being marked as a variable, thereby leading to invalid resulting tql. This bug was introduced in r6943.
git-svn-id: http://svn.topazproject.org/svn/head@7265 48088309-b40f-0410-b149-b14953253675

topaz/core/src/main/antlr/predicates.g
@@ -454,8 +454,8 @@ factor[boolean isProj, boolean isComp] returns [ExprType type = null]
     | #(PARAM ID)
     | type=fcall[isProj, isComp]
     | #(REF (   v:ID                 { updateAST(#v, null, type = getTypeForVar(#v), null, true); }
-              | type=c:cast[isProj]  { updateAST(findLastId(#c), null, type, null,
-                                                 ((OqlAST) #c).isVar()); }
+              | type=c:cast[isProj]  { AST last = findLastId(#c);
+                                       updateAST(last, null, type, null, ((OqlAST) last).isVar()); }
             )
             (   ! ID         { type = resolveField(currentAST, type, #ID); }
               | ! URIREF     { type = handlePredicate(currentAST, type, #URIREF); }