pradeepk/topazproject

commit 3fb918d36ddece59ef42666e50ecf938b4dc7cb1

avoid calling articleInfoX variable without first checking that it exists.
create new variable, docTitle, in article_variables.ftl, and remove all references to articleInfoX in various templates.

often, when caches are out of whack or other errors occur, articleInfoX is empty. however, we can still display most of the article using the xsl transform, and it's nicer to display what we can than to fail completely with a freemarker error.

git-svn-id: http://svn.topazproject.org/svn/head@7379 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/webapp/article/article_content.ftl
@@ -25,7 +25,7 @@
   <@s.url id="feedbackURL" includeParams="none" namespace="/" action="feedbackCreate"
     page="${thisPageURL?url}"/>
   <#include "article_blurb.ftl">
-  <h1 xpathLocation="noSelect">${articleInfoX.title}</h1>
+  <h1 xpathLocation="noSelect">${docTitle}</h1>
   <#assign tab="article" />
   <#include "article_tabs.ftl">
     <div id="retractionHtmlId" class="retractionHtmlId" style="display:none;" xpathLocation="noSelect">

ambra/webapp/src/main/webapp/journals/plosJournals/article/article_content.ftl
@@ -23,7 +23,7 @@
   <@s.url id="thisPageURL" includeParams="get" includeContext="true" encode="false"/>
   <@s.url id="feedbackURL" includeParams="none" namespace="/" action="feedbackCreate" page="${thisPageURL?url}"/>
   <#include "article_blurb.ftl">
-  <h1 xpathLocation="noSelect">${articleInfoX.title}</h1>
+  <h1 xpathLocation="noSelect">${docTitle}</h1>
   <#assign tab="article" />
   <#include "article_tabs.ftl">
     <div id="retractionHtmlId" class="retractionHtmlId" style="display:none;" xpathLocation="noSelect">

ambra/webapp/src/main/webapp/article/article_comments.ftl
@@ -42,7 +42,7 @@
    </#if>
       <a id="top" name="top" toc="top" title="Top"></a>
       <#include "article_blurb.ftl">
-      <h1>${articleInfoX.title}</h1>
+      <h1>${docTitle}</h1>
       <#assign tab="comments" />
       <#include "article_tabs.ftl">
       <div class="rss"><a href="#">Comments RSS</a></div>

ambra/webapp/src/main/webapp/article/article_related.ftl
@@ -30,7 +30,7 @@
     <div id="researchArticle" class="content related">
       <a id="top" name="top" toc="top" title="Top"></a>
       <#include "article_blurb.ftl">
-      <h1>${articleInfoX.title}</h1>
+      <h1>${docTitle}</h1>
       <#assign tab="related" />
       <#include "article_tabs.ftl">
 

ambra/webapp/src/main/webapp/article/article_rhc.ftl
@@ -79,7 +79,7 @@
           <a href="http://www.stumbleupon.com/submit?url=${jDocURL}" target="_new"> <img border=0 src="http://cdn.stumble-upon.com/images/16x16_su_solid.gif" alt="StumbleUpon" title="Add to StumbleUpon"></a>
           <#-- for more info, see http://www.stumbleupon.com/buttons.php -->
           <#-- Facebook -->
-          <script>function fbs_click() {u='${docURL}';t='${articleInfoX.title?url}';window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;}</script><a href="http://www.facebook.com/share.php?u=${docURL?url}" onclick="return fbs_click()"><img src="http://static.ak.fbcdn.net/images/share/facebook_share_icon.gif" alt="Facebook" title="Add to Facebook" /></a>       <!-- for mor info, see http://www.facebook.com/share_partners.php -->
+          <script>function fbs_click() {u='${docURL}';t='${docTitle?url}';window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;}</script><a href="http://www.facebook.com/share.php?u=${docURL?url}" onclick="return fbs_click()"><img src="http://static.ak.fbcdn.net/images/share/facebook_share_icon.gif" alt="Facebook" title="Add to Facebook" /></a>       <!-- for mor info, see http://www.facebook.com/share_partners.php -->
           <#-- Connotea -->
           <script type="text/javascript">
             function bookmark_in_connotea(u) {
@@ -92,7 +92,7 @@
           <a style='cursor: pointer;' onclick='javascript:bookmark_in_connotea("${docURL}");'><img src='${freemarker_config.getContext()}/images/icon_connotea_16x16.gif' alt="Connotea" title="Add to Connotea"/></a>
           <#-- See: http://www.connotea.org/wiki/AddToConnoteaButton -->
           <#-- Citeulike -->
-          <a href="http://www.citeulike.org/posturl?url=${docURL?url}&title=${articleInfoX.title?url}" target="_new"><img src='${freemarker_config.getContext()}/images/icon_citeulike_16x16.gif' alt="CiteULike" title="Add to CiteULike" /></a>
+          <a href="http://www.citeulike.org/posturl?url=${docURL?url}&title=${docTitle?url}" target="_new"><img src='${freemarker_config.getContext()}/images/icon_citeulike_16x16.gif' alt="CiteULike" title="Add to CiteULike" /></a>
           <#-- For more info see http://www.citeulike.org/faq/all.adp -->
           <#-- Digg
             TODO:Eventually we should be passing the abstract as the bodytext to digg.  We could also switch up and
@@ -101,7 +101,7 @@
           <script type="text/javascript">
           digg_url = '${jDocURL}';
           digg_skin = 'icon';
-          digg_title = '${articleInfoX.title?replace("'","\\'")}';
+          digg_title = '${docTitle?replace("'","\\'")}';
           digg_bodytext = '';
           digg_topic = '';
           digg_media = 'news';

ambra/webapp/src/main/webapp/article/article_variables.ftl
@@ -17,14 +17,22 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 -->
+
 <#--
   The digg URL has to be different as digg appears to be picking up the redirect
   from our internal DOI resolver and messing up the formating.
  -->
-<#assign shortDOI = "${articleInfoX.id?replace('info:doi/','')}" />
-<#assign docURL = "http://dx.plos.org/" + shortDOI />
-<#assign jDocURL = freemarker_config.getJournalUrl(journalContext) + "/article/" +
-  articleInfoX.id?url />
+<#if articleInfoX??>
+  <#assign shortDOI = "${articleInfoX.id?replace('info:doi/','')}" />
+  <#assign docURL = "http://dx.plos.org/" + shortDOI />
+  <#assign jDocURL = freemarker_config.getJournalUrl(journalContext) + "/article/" + articleInfoX.id?url />
+  <#assign docTitle = articleInfoX.title />
+<#else>
+  <#assign shortDOI = "" />
+  <#assign docURL = "" />
+  <#assign jDocURL = "" />
+  <#assign docTitle = "" />
+</#if>
 
 <#assign publisher=""/>
 <#if Request[freemarker_config.journalContextAttributeKey]?exists>

ambra/webapp/src/main/webapp/journals/plosJournals/article/article_body.ftl
@@ -17,7 +17,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 -->
-<#include "article_variables.ftl">
+<#include "/article/article_variables.ftl">
 <div id="content" class="article" style="visibility:visible;">
 <#include "/article/article_rhc.ftl">
 <form name="articleInfo" id="articleInfo" method="" action="">
@@ -28,7 +28,7 @@
 <input type="hidden" name="annotationId" value="${annotationId}" />
 <input type="hidden" name="isResearchArticle" value="${isResearchArticle?string}" />
 </form>
-<div id="articleContainer"><#include "article_content.ftl"></div>
+<div id="articleContainer"><#include "/journals/plosJournals/article/article_content.ftl"></div>
 <div style="visibility:hidden">
 <#include "/widget/annotation_add.ftl">
 <#include "/widget/contextAction.ftl">

commit 994a83dbc4afaf49379bb49f2f88233b90ba5b39

redefine docTitle variable in article_content.ftl, fixes a bug introduced in r7379
docTitle is defined in article_variables, however article_variables is not processed when an annotation is added.

inelegant solution for now is to redefine docTitle in article_content.ftl.

git-svn-id: http://svn.topazproject.org/svn/head@7390 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/webapp/article/article_content.ftl
@@ -17,6 +17,13 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 -->
+
+<#if articleInfoX??>
+  <#assign docTitle = articleInfoX.title />
+<#else>
+  <#assign docTitle = "" />
+</#if>
+
 <@s.url id="createDiscussionURL" namespace="/annotation/secure" action="startDiscussion"
   includeParams="none" target="${articleURI}" />
 <div id="researchArticle" class="content">

ambra/webapp/src/main/webapp/journals/plosJournals/article/article_content.ftl
@@ -17,6 +17,13 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 -->
+
+<#if articleInfoX??>
+  <#assign docTitle = articleInfoX.title />
+<#else>
+  <#assign docTitle = "" />
+</#if>
+
 <@s.url id="createDiscussionURL" namespace="/annotation/secure" action="startDiscussion" includeParams="none" target="${articleURI}" />
 <div id="researchArticle" class="content">
   <a id="top" name="top" toc="top" title="Top"></a>