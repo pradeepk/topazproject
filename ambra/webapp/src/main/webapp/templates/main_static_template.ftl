pradeepk/topazproject

commit 6a072e290915a4f771574379be6606f7b8922f5f

Removed struts.xml from journal jars.
{{{/static/foo}}} automatically tries to use Freemarker template {{{foo.ftl}}}. Note that pageNotFound will not fire for static package because there is a default "wildcard" action.

Freemarker templates for all actions are searched in this order:

 1. Journal jar: {{{/struts}}}
 2. PLoS defaults: {{{/journals/plosJournals}}}
 3. Ambra defaults: {{{/}}}

AmbraActionMapper doesn't search for actions any more. Inheritance is done only for Freemarker templates.

Fixes #1022

git-svn-id: http://svn.topazproject.org/svn/head@6553 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/resources/struts.xml
@@ -116,8 +116,6 @@
 
     <default-interceptor-ref name="ambraUserStack" />
 
-    <default-action-ref name="pageNotFound" />
-
     <global-results>
       <result name="new-profile" type="redirect-action">
         <param name="actionName">prePopulateNewUser</param>
@@ -212,6 +210,9 @@
   </package>
 
   <package name="search" extends="default" namespace="/search">
+
+    <default-action-ref name="pageNotFound" />
+
     <action name="searchStart" class="org.topazproject.ambra.action.NoOpAction">
       <result name="input" type="ambraFreemarker">
         <param name="templateFile">/search/searchResults.ftl</param>
@@ -238,59 +239,17 @@
   </package>
 
   <package name="static" extends="default" namespace="/static">
-    <action name="browse" class="org.topazproject.ambra.action.NoOpAction">
+    <action name="*" class="org.topazproject.ambra.action.NoOpAction">
       <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/browse.ftl</param>
-      </result>
-    </action>
-    <action name="commentGuidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/commentGuidelines.ftl</param>
-      </result>
-    </action>
-    <action name="help" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/help.ftl</param>
-      </result>
-    </action>
-    <action name="ratingGuidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/ratingGuidelines.ftl</param>
-      </result>
-    </action>
-    <action name="releaseNotes" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/releaseNotes.ftl</param>
-      </result>
-    </action>
-    <action name="rssFeeds" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/rssFeeds.ftl</param>
-      </result>
-    </action>
-    <action name="rssInfo" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/rssInfo.ftl</param>
-      </result>
-    </action>
-    <action name="error" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/serverError.ftl</param>
-      </result>
-    </action>
-    <action name="sitemap" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/sitemap.ftl</param>
-      </result>
-    </action>
-    <action name="users" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/static/users.ftl</param>
+        <param name="templateFile">/static/{1}.ftl</param>
       </result>
     </action>
   </package>
 
   <package name="rate" extends="default" namespace="/rate">
+
+    <default-action-ref name="pageNotFound" />
+
     <action name="getAverageRatingsForArticle" class="org.topazproject.ambra.rating.action.GetAverageRatingsAction">
       <interceptor-ref name="chain">
         <param name="excludes">targetSource, callbacks, exposeProxy</param>
@@ -319,6 +278,9 @@
   </package>
 
   <package name="rate-secure" extends="default" namespace="/rate/secure">
+
+    <default-action-ref name="pageNotFound" />
+
     <action name="rateArticle" class="org.topazproject.ambra.rating.action.RateAction" method="rateArticle">
       <result name="input" type="json"/>
       <result name="success" type="json"/>
@@ -339,6 +301,9 @@
   </package>
 
   <package name="article" extends="default" namespace="/article">
+
+    <default-action-ref name="pageNotFound" />
+
     <action name="listTrackbacks" class="org.topazproject.ambra.action.TrackbackAction" method="getTrackbacksForArticle">
       <result name="success" type="ambraFreemarker">
         <param name="templateFile">/article/trackbackList.ftl</param>
@@ -480,6 +445,9 @@
   </package>
 
   <package name="article-feed" extends="default" namespace="/article/feed">
+
+    <default-action-ref name="pageNotFound" />
+
     <action name="getFeed" class="org.topazproject.ambra.article.action.ArticleFeed" method="execute">
       <result name="success" type="feed">
         <param name="feedName">wireFeed</param>
@@ -488,6 +456,9 @@
   </package>
 
   <package name="annotation" extends="default" namespace="/annotation">
+
+    <default-action-ref name="pageNotFound" />
+
     <action name="listAnnotation" class="org.topazproject.ambra.annotation.action.ListAnnotationAction">
       <result name="success">/WEB-INF/pages/annotation/listAnnotation.ftl</result>
     </action>
@@ -558,6 +529,9 @@
   </package>
 
   <package name="annotation-secure" extends="default" namespace="/annotation/secure">
+
+    <default-action-ref name="pageNotFound" />
+
     <action name="createAnnotation">
       <result>/WEB-INF/pages/annotation/createAnnotation.ftl</result>
     </action>
@@ -636,6 +610,9 @@
   </package>
 
   <package name="user-secure" extends="default" namespace="/user/secure">
+
+    <default-action-ref name="pageNotFound" />
+
     <action name="secureRedirect" class="org.topazproject.ambra.action.RedirectAction">
       <result type="redirect">
         <param name="location">${goTo}</param>
@@ -754,6 +731,8 @@
   <package name="user" extends="default" namespace="/user">
     <default-interceptor-ref name="ambraBaseStack" />
 
+    <default-action-ref name="pageNotFound" />
+
     <action name="prePopulateNewUser" class="org.topazproject.ambra.user.action.MemberUserProfileAction" method="prePopulateUserDetails">
       <result name="new-profile" type="user">
         <param name="templateFile">/journals/plosJournals/user/createNewUser.ftl</param>
@@ -794,6 +773,8 @@
 
   <package name="admin" extends="default" namespace="/admin">
     <default-interceptor-ref name="ambraAdminStack" />
+
+    <default-action-ref name="pageNotFound" />
 
     <action name="adminTop" class="org.topazproject.ambra.admin.action.AdminTopAction">
       <result name="success" type="ambraFreemarker">

ambra/journals/journal-clinicalTrials/src/main/resources/struts.xml
@@ -1,137 +1,0 @@
-<!DOCTYPE struts PUBLIC
-    "-//Apache Software Foundation//DTD Struts Configuration 2.0//EN"
-    "http://struts.apache.org/dtds/struts-2.0.dtd">
-
-<!--
-  Copyright (c) 2007-2008 by Topaz, Inc.
-  http://topazproject.org
-  
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
--->
-<struts>
-  <package name="default-clinicalTrials" extends="default" namespace="/journals/clinicalTrials">
-    <action name="home" class="org.topazproject.ambra.action.HomePageAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/index.ftl</param>
-      </result>
-    </action>
-  </package>
-
-  <package name="article-clinicalTrials" extends="article" namespace="/journals/clinicalTrials/article">
-    <action name="browse" class="org.topazproject.ambra.article.action.BrowseArticlesAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/article/browse.ftl</param>
-      </result>
-    </action>
-  </package>
-
-  <package name="static-clinicalTrials" extends="default-clinicalTrials" namespace="/journals/clinicalTrials/static">
-    <action name="about" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/about.ftl</param>
-      </result>
-    </action>
-    <action name="authors" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/authors.ftl</param>
-      </result>
-    </action>
-    <action name="browse" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/browse.ftl</param>
-      </result>
-    </action>
-    <action name="checklist" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/checklist.ftl</param>
-      </result>
-    </action>
-    <action name="contact" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/contact.ftl</param>
-      </result>
-    </action>
-    <action name="faq" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/faq.ftl</param>
-      </result>
-    </action>
-    <action name="information" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/information.ftl</param>
-      </result>
-    </action>
-    <action name="rssFeeds" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/rssFeeds.ftl</param>
-      </result>
-    </action>
-    <action name="sitemap" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/sitemap.ftl</param>
-      </result>
-    </action>
-    <action name="users" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/clinicalTrials/static/users.ftl</param>
-      </result>
-    </action>
-
-    <!-- shared -->
-    <action name="commentGuidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/commentGuidelines.ftl</param>
-      </result>
-    </action>
-    <action name="help" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/help.ftl</param>
-      </result>
-    </action>
-    <action name="license" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/license.ftl</param>
-      </result>
-    </action>
-    <action name="privacy" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/privacy.ftl</param>
-      </result>
-    </action>
-    <action name="ratingGuidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/ratingGuidelines.ftl</param>
-      </result>
-    </action>
-    <action name="rssInfo" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/rssInfo.ftl</param>
-      </result>
-    </action>
-    <action name="searchHelp" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/searchHelp.ftl</param>
-      </result>
-    </action>
-    <action name="terms" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/terms.ftl</param>
-      </result>
-    </action>
-    <action name="userFaq" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/userFaq.ftl</param>
-      </result>
-    </action>
-  </package>
-</struts>

ambra/journals/journal-compbiol/src/main/resources/struts.xml
@@ -1,179 +1,0 @@
-<!DOCTYPE struts PUBLIC
-    "-//Apache Software Foundation//DTD Struts Configuration 2.0//EN"
-    "http://struts.apache.org/dtds/struts-2.0.dtd">
-
-<!--
-  Copyright (c) 2007-2008 by Topaz, Inc.
-  http://topazproject.org
-  
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
--->
-<struts>
-
-  <package name="default-compbiol" extends="default" namespace="/journals/compbiol">
-    <action name="home" class="org.topazproject.ambra.action.HomePageAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/index.ftl</param>
-      </result>
-    </action>
-    <action name="recentArticles" class="org.topazproject.ambra.action.HomePageAction">
-      <result name="success">/journals/compbiol/article/recentArticles.ftl</result>
-    </action>
-    <action name="mostCommented" class="org.topazproject.ambra.action.HomePageAction">
-      <result name="success">/journals/compbiol/article/mostCommented.ftl</result>
-    </action>
-  </package>
-
-  <package name="article-compbiol" extends="article" namespace="/journals/compbiol/article">
-    <action name="browse" class="org.topazproject.ambra.article.action.BrowseArticlesAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/article/browse.ftl</param>
-      </result>
-    </action>
-  </package>  
-  
-  <package name="static-compbiol" extends="default-compbiol" namespace="/journals/compbiol/static">
-    <action name="about" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/about.ftl</param>
-      </result>
-    </action>
-    <action name="authors" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/authors.ftl</param>
-      </result>
-    </action>
-    <action name="browse" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/browse.ftl</param>
-      </result>
-    </action>
-    <action name="checklist" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/checklist.ftl</param>
-      </result>
-    </action>
-    <action name="contact" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/contact.ftl</param>
-      </result>
-    </action>
-    <action name="edboard" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/edboard.ftl</param>
-      </result>
-    </action>
-    <action name="eic" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/eic.ftl</param>
-      </result>
-    </action>
-    <action name="guidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/guidelines.ftl</param>
-      </result>
-    </action>
-    <action name="information" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/information.ftl</param>
-      </result>
-    </action>
-    <action name="policies" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/policies.ftl</param>
-      </result>
-    </action>
-    <action name="reviewerGuidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/reviewer_guidelines.ftl</param>
-      </result>
-    </action>
-    <action name="rssFeeds" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/rssFeeds.ftl</param>
-      </result>
-    </action>
-    <action name="sitemap" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/sitemap.ftl</param>
-      </result>
-    </action>
-    <action name="users" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/compbiol/static/users.ftl</param>
-      </result>
-    </action>
-
-    <!-- shared static pages -->
-    <action name="commentGuidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/commentGuidelines.ftl</param>
-      </result>
-    </action>
-    <action name="competing" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/competing.ftl</param>
-      </result>
-    </action>
-    <action name="figureGuidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/figure_guidelines.ftl</param>
-      </result>
-    </action>
-    <action name="help" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/help.ftl</param>
-      </result>
-    </action>
-    <action name="latexGuidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/latexGuidelines.ftl</param>
-      </result>
-    </action>
-    <action name="license" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/license.ftl</param>
-      </result>
-    </action>
-    <action name="privacy" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/privacy.ftl</param>
-      </result>
-    </action>
-    <action name="ratingGuidelines" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/ratingGuidelines.ftl</param>
-      </result>
-    </action>
-    <action name="rssInfo" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/rssInfo.ftl</param>
-      </result>
-    </action>
-    <action name="searchHelp" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/searchHelp.ftl</param>
-      </result>
-    </action>
-    <action name="terms" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/terms.ftl</param>
-      </result>
-    </action>
-    <action name="userFaq" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
-        <param name="templateFile">/journals/plosJournals/static/userFaq.ftl</param>
-      </result>
-    </action>
-  </package>
-</struts>

commit 0a1b73cefbeee0b945a3d76f8915d39be9d6fb9a

Fixes the pageNotFound issue introduced in r6553.  Now if a page fails when being viewed under static content, we assume it's a 404 error.  Freemarker offers no way to determine if a template exists or not.  We may want to add a method to this in the action class eventually.
References #1266

git-svn-id: http://svn.topazproject.org/svn/head@7615 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/resources/struts.xml
@@ -44,6 +44,9 @@
       <result-type name="ambraFreemarker" class="org.topazproject.ambra.struts2.AmbraFreemarkerResult">
         <param name="location">/templates/main_template.ftl</param>
       </result-type>
+      <result-type name="ambraStaticFreemarker" class="org.topazproject.ambra.struts2.AmbraFreemarkerResult">
+        <param name="location">/templates/main_static_template.ftl</param>
+      </result-type>
       <result-type name="headless" class="org.topazproject.ambra.struts2.AmbraFreemarkerResult">
         <param name="location">/templates/headless_template.ftl</param>
       </result-type>
@@ -240,7 +243,7 @@
 
   <package name="static" extends="default" namespace="/static">
     <action name="*" class="org.topazproject.ambra.action.NoOpAction">
-      <result name="success" type="ambraFreemarker">
+      <result name="success" type="ambraStaticFreemarker">
         <param name="templateFile">/static/{1}.ftl</param>
       </result>
     </action>

ambra/webapp/src/main/webapp/templates/main_static_template.ftl
@@ -1,0 +1,44 @@
+<#--
+  $HeadURL::                                                                            $
+  $Id$
+
+  Copyright (c) 2007-2008 by Topaz, Inc.
+  http://topazproject.org
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+<#if Request[freemarker_config.journalContextAttributeKey]?exists>
+  <#assign journalContext = Request[freemarker_config.journalContextAttributeKey].journal>
+<#else>
+  <#assign journalContext = "">
+</#if>
+
+<#include "/global/global_config.ftl">
+
+<#include "/global/global_top.ftl">
+
+<!-- begin : main content -->
+
+<#--
+  This template is called only for static content.  If something fails, assume it's a 404
+  and handle it gracefully
+-->
+<#attempt>
+  <#include "${templateFile}">
+<#recover>
+  <#include "/static/pageNotFound.ftl">
+</#attempt>
+<!-- end : main contents -->
+
+<#include "/global/global_bottom.ftl">
+