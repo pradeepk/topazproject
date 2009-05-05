pradeepk/topazproject

commit 9a6f5be2c6f8474b09fa1c1beb8ee430821296f7

Special characters used in Lucene need to be escaped in query strings. Use method provided in Lucene instead of writing our own. Addresses #1283
git-svn-id: http://svn.topazproject.org/svn/head@7651 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/java/org/topazproject/ambra/search/action/SearchAction.java
@@ -34,6 +34,7 @@ import java.net.URI;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.lucene.queryParser.QueryParser;
 
 import org.springframework.transaction.annotation.Transactional;
 
@@ -89,7 +90,7 @@ public class SearchAction extends BaseSessionAwareActionSupport {
   public String executeSimpleSearch() {
     // the simple search text field correlates to advanced search's "for all the words" field
     this.textSearchAll = query;
-    return executeSearch(query);
+    return executeSearch(escape(query));
   }
 
   /**
@@ -99,7 +100,7 @@ public class SearchAction extends BaseSessionAwareActionSupport {
   public String executeAdvancedSearch() {
     if(doSearch()) {
       query = buildAdvancedQuery();
-      return executeSearch(query);
+      return executeSearch(QueryParser.escape(query));
     }
 
     categoryInfos = browseService.getArticlesByCategory();
@@ -329,16 +330,8 @@ public class SearchAction extends BaseSessionAwareActionSupport {
   /**
    * Static helper method to escape special characters in a Lucene search string with a \
    */
-  protected static final String escapeChars = "+-&|!(){}[]^\"~*?:\\";
   protected static String escape(String in) {
-    StringBuilder buf = new StringBuilder();
-    for (int i=0; i < in.length(); i++) {
-      if (escapeChars.indexOf(in.charAt(i)) != -1) {
-        buf.append("\\");
-      }
-      buf.append(in.charAt(i));
-    }
-    return buf.toString();
+    return QueryParser.escape(in);
   }
 
   protected static boolean isDigit(String str) {

commit 3178979351d69e304fe9c57915004c8cae395f19

Fix bug introduced in r7651. Escaped a wrong string in
git-svn-id: http://svn.topazproject.org/svn/head@7652 48088309-b40f-0410-b149-b14953253675

ambra/webapp/src/main/java/org/topazproject/ambra/search/action/SearchAction.java
@@ -100,7 +100,7 @@ public class SearchAction extends BaseSessionAwareActionSupport {
   public String executeAdvancedSearch() {
     if(doSearch()) {
       query = buildAdvancedQuery();
-      return executeSearch(QueryParser.escape(query));
+      return executeSearch(query);
     }
 
     categoryInfos = browseService.getArticlesByCategory();