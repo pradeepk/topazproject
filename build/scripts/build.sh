c448a486fd77fab6df954685c2282c3e4105800a
dragisak <dragisak@48088309-b40f-0410-b149-b14953253675>
2009-08-08 00:38:41 +0000

    Add mulgara-service and otm-extras to deployment.
    git-svn-id: http://svn.topazproject.org/svn/head@7681 48088309-b40f-0410-b149-b14953253675

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -84,7 +84,7 @@ echo ${SVNVERSION} > ${MVN_LAST_BUILD}
 # Deploy the OTM jars
 if [ ${N} -eq 0 ]; then
   echo "Deploying jars"
-  for pkg in "." "mulgara" "mulgara/string-compare-resolver" "mulgara/mulgara-client" "topaz" "topaz/core" ; do
+  for pkg in "." "mulgara" "mulgara/string-compare-resolver" "mulgara/mulgara-client" "mulgara/mulgara-service" "topaz" "topaz/core" "topaz/extras" ; do
     ${MVN} ${MVNARGS} -N -f "$pkg/pom.xml" -Dmaven.test.skip=true deploy
   done
 fi
